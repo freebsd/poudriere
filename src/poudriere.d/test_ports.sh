6ce28d524a68702c9453fee5f6214a2d16f7b2e2
Baptiste Daroussin <bapt@FreeBSD.org>
2010-09-07 18:12:28 +0000

    s/jailname/JAILNAME/g for consistency

--- src/poudriere.d/test_ports.sh
+++ src/poudriere.d/test_ports.sh
@@ -17,7 +17,7 @@ cleanup() {
 	outside_portsdir ${PORTDIRECTORY} && umount ${PORTDIRECTORY}
 	umount ${MNT}/usr/ports/packages
 	umount ${MNT}/usr/ports
-	/bin/sh ${SCRIPTPREFIX}/stop_jail.sh -n ${jailname}
+	/bin/sh ${SCRIPTPREFIX}/stop_jail.sh -n ${JAILNAME}
 }
 
 sig_handler() {
@@ -35,18 +35,18 @@ build_port() {
 	do
 		if [ "${PHASE}" = "deinstall" ]; then
 			echo "===> Checking pkg_info"
-			PKG_DBDIR=${PKG_DBDIR} jexec -U root ${jailname} /usr/sbin/pkg_info ${PKGNAME}
+			PKG_DBDIR=${PKG_DBDIR} jexec -U root ${JAILNAME} /usr/sbin/pkg_info ${PKGNAME}
 			PLIST="${PKG_DBDIR}/${PKGNAME}/+CONTENTS"
 			if [ -r ${MNT}${PLIST} ]; then
 				echo "===> Checking shared library dependencies"
 				grep -v "^@" ${MNT}${PLIST} | \
 				sed -e "s,^,${PREFIX}/," | \
-				xargs jexec -U root ${jailname} ldd 2>&1 | \
+				xargs jexec -U root ${JAILNAME} ldd 2>&1 | \
 				grep -v "not a dynamic executable" | \
 				grep '=>' | awk '{ print $3;}' | sort -u
 			fi
 		fi
-		jexec -U root ${jailname} make -C ${PORTDIRECTORY} ${PORT_FLAGS} ${PHASE} PKGREPOSITORY=/tmp PACKAGES=/tmp
+		jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} ${PORT_FLAGS} ${PHASE} PKGREPOSITORY=/tmp PACKAGES=/tmp
 		if [ $? -gt 0 ]; then
 			echo "===> Error running make ${PHASE}"
 			if [ "${PHASE}" = "package" ]; then
@@ -91,21 +91,21 @@ trap sig_handler SIGINT SIGTERM SIGKILL
 
 test -z ${PORTDIRECTORY} && usage
 PORTNAME=`make -C ${PORTDIRECTORY} -VPKGNAME`
-for jailname in `zfs list -rH ${ZPOOL}/poudriere | awk '/^'${ZPOOL}'\/poudriere\// { sub(/^'${ZPOOL}'\/poudriere\//, "", $1); print $1 }'`; do
-	MNT=`zfs list -H ${ZPOOL}/poudriere/${jailname} | awk '{ print $NF}'`
-	/bin/sh ${SCRIPTPREFIX}/start_jail.sh -n ${jailname}
+for JAILNAME in `zfs list -rH ${ZPOOL}/poudriere | awk '/^'${ZPOOL}'\/poudriere\// { sub(/^'${ZPOOL}'\/poudriere\//, "", $1); print $1 }'`; do
+	MNT=`zfs list -H ${ZPOOL}/poudriere/${JAILNAME} | awk '{ print $NF}'`
+	/bin/sh ${SCRIPTPREFIX}/start_jail.sh -n ${JAILNAME}
 	STATUS=1 #injail
 	mkdir -p ${MNT}/usr/ports
 	mount -t nullfs ${PORTSDIR} ${MNT}/usr/ports
-	mkdir -p ${POUDRIERE_DATA}/packages/${jailname}/All
-	mount -t nullfs ${POUDRIERE_DATA}/packages/${jailname} ${MNT}/usr/ports/packages
+	mkdir -p ${POUDRIERE_DATA}/packages/${JAILNAME}/All
+	mount -t nullfs ${POUDRIERE_DATA}/packages/${JAILNAME} ${MNT}/usr/ports/packages
 
 	if outside_portsdir ${PORTDIRECTORY}; then
 		mkdir -p ${MNT}/${PORTDIRECTORY}
 		mount -t nullfs ${PORTDIRECTORY} ${MNT}/${PORTDIRECTORY}
 	fi
 
-	jexec -U root ${jailname} /usr/sbin/mtree -U  -f /usr/ports/Templates/BSD.local.dist -d -e -p /usr/local
+	jexec -U root ${JAILNAME} /usr/sbin/mtree -U  -f /usr/ports/Templates/BSD.local.dist -d -e -p /usr/local
 
 cat << EOF >> ${MNT}/etc/make.conf
 USE_PACKAGE_DEPENDS=yes
@@ -113,25 +113,25 @@ BATCH=yes
 EOF
 
 	(
-	jexec -U root ${jailname} make -C ${PORTDIRECTORY} clean
-	jexec -U root ${jailname} make -C ${PORTDIRECTORY} extract-depends fetch-depends patch-depends build-depends lib-depends
+	jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} clean
+	jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} extract-depends fetch-depends patch-depends build-depends lib-depends
 # Package all newly build ports
-	DEPLIST=`jexec -U root ${jailname} make -C ${PORTDIRECTORY} build-depends-list`
-	for pkg in `jexec -U root ${jailname} /usr/sbin/pkg_info | awk '{ print $1}'`; do
-		test -f ${POUDRIERE_DATA}/packages/${jailname}/All/${pkg}.tbz || jexec -U root ${jailname} /usr/sbin/pkg_create -b ${pkg} /usr/ports/packages/All/${pkg}.tbz
+	DEPLIST=`jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} build-depends-list`
+	for pkg in `jexec -U root ${JAILNAME} /usr/sbin/pkg_info | awk '{ print $1}'`; do
+		test -f ${POUDRIERE_DATA}/packages/${JAILNAME}/All/${pkg}.tbz || jexec -U root ${JAILNAME} /usr/sbin/pkg_create -b ${pkg} /usr/ports/packages/All/${pkg}.tbz
 	done
-	) | tee ${LOGS}/${PORTNAME}-${jailname}.depends.log
+	) | tee ${LOGS}/${PORTNAME}-${JAILNAME}.depends.log
 
 	(
-	PKGNAME=`jexec -U root ${jailname} make -C ${PORTDIRECTORY} -VPKGNAME`
-	PKG_DBDIR=`jexec -U root ${jailname} mktemp -d -t pkg_db`
-	LOCALBASE=`jexec -U root ${jailname} make -C ${PORTDIRECTORY} -VLOCALBASE`
+	PKGNAME=`jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} -VPKGNAME`
+	PKG_DBDIR=`jexec -U root ${JAILNAME} mktemp -d -t pkg_db`
+	LOCALBASE=`jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} -VLOCALBASE`
 	PREFIX="${BUILDROOT:-/tmp}/`echo ${PKGNAME} | tr '[,+]' _`"
 	PORT_FLAGS="PREFIX=${PREFIX} PKG_DBDIR=${PKG_DBDIR} NO_DEPENDS=yes"
 	echo "===> Building with flags: ${PORT_FLAGS}"
 	echo "===> Cleaning workspace"
-	jexec -U root ${jailname} make -C ${PORTDIRECTORY} clean
-	[ $CONFIGSTR -eq 1 ] && jexec -U root ${jailname} make -C ${PORTDIRECTORY} config
+	jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} clean
+	[ $CONFIGSTR -eq 1 ] && jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} config
 
 	if [ -d ${MNT}${PREFIX} ]; then
 		echo "===> Removing existing ${PREFIX}"
@@ -151,13 +151,13 @@ EOF
 	fi
 
 	echo "===> Cleaning up"
-	jexec -U root ${jailname} make -C ${PORTDIRECTORY} clean
+	jexec -U root ${JAILNAME} make -C ${PORTDIRECTORY} clean
 
 	echo "===> Removing existing ${PREFIX} dir"
 	[ "${PREFIX}" != "${LOCALBASE}" ] && rm -rf ${MNT}${PREFIX} ${MNT}${PREFIX}.PLIST_DIRS.before ${MNT}${PREFIX}.PLIST_DIRS.after
 	rm -rf ${MNT}${PKG_DBDIR}
 
-	) | tee  ${LOGS}/${PORTNAME}-${jailname}.build.log
+	) | tee  ${LOGS}/${PORTNAME}-${JAILNAME}.build.log
 
 	cleanup
 	STATUS=0 #injail
