5a5550ac0216febb9e7e0c670b2964cc8de5f568
Baptiste Daroussin <bapt@FreeBSD.org>
2011-03-29 13:18:28 +0000

    Stop running jail before erasing it

--- src/poudriere.d/remove_jail.sh
+++ src/poudriere.d/remove_jail.sh
@@ -39,7 +39,12 @@ done
 test -z ${NAME} && usage
 
 zfs list ${ZPOOL}/poudriere/${NAME} >/dev/null 2>&1 || err 1 "No such jail: ${NAME}"
-JAILBASE=`zfs list -H ${ZPOOL}/poudriere/${NAME} | awk '{ print $NF}'`
+JAILBASE=`zfs list -H -o mountpoint ${ZPOOL}/poudriere/${NAME}`
+
+if /usr/sbin/jls host.hostname | egrep "^${NAME}$" > /dev/null;then
+	msg "Found jail in running state. Stoping it."
+	/usr/local/bin/poudriere stopjail -n ${NAME}
+fi
 
 msg_n "Removing ${NAME} jail..."
 zfs destroy -r ${ZPOOL}/poudriere/${NAME}
