33576a8887b0147ee03230bbd36650d3ab17508c
Baptiste Daroussin <bapt@FreeBSD.org>
2011-03-30 10:24:28 +0000

    Cleanup a bit list_jails.sh, be more generic

--- src/poudriere.d/list_jails.sh
+++ src/poudriere.d/list_jails.sh
@@ -13,9 +13,8 @@ SCRIPTPATH=`realpath $0`
 SCRIPTPREFIX=`dirname ${SCRIPTPATH}`
 . ${SCRIPTPREFIX}/common.sh
 
-JAILSPATH=/usr/local/poudriere/jails
 
-[ -d ${JAILSPATH} ] || err 1 "No jails directory found in /usr/local/poudriere."
+JAILNAMES=`zfs list -rH ${ZPOOL}/poudriere | awk '/^'${ZPOOL}'\/poudriere\// { sub(/^'${ZPOOL}'\/poudriere\//, "", $1); print $1 }'`
 
 while getopts "n:q" FLAG; do
         case "${FLAG}" in
@@ -31,19 +30,16 @@ while getopts "n:q" FLAG; do
 	esac
 done
 
+[ "${JAILNAMES}X" = "X" ] && err 1 "No jails found."
 [ "${NOHEADER}X" = "1X" ] || printf '%-20s %-13s %s\n' "JAILNAME" "VERSION" "ARCH"
 
-JAILLIST=`ls ${JAILSPATH}`
-[ "${NAME}X" = "X" ] || JAILLIST=${NAME}
+for JAILNAME in ${JAILNAMES};do
+	MNT=`zfs list -H -o mountpoint ${ZPOOL}/poudriere/${JAILNAME}`
 
-for DIR in ${JAILLIST};do
-
-	if [ -d ${JAILSPATH}/${DIR} -a -d ${JAILSPATH}/${DIR}/boot/kernel ];then
-		JAILNAME=${DIR}
-		VERSION=`jail -c path=${JAILSPATH}/${JAILNAME} host.hostname=${JAILNAME} command=uname -v | awk '{print $2}'`
-		ARCH=`jail -c path=${JAILSPATH}/${JAILNAME} host.hostname=${JAILNAME} command=uname -p`
+	if [ -d ${MNT}${DIR} -a -d ${MNT}/boot/kernel ];then
+		VERSION=`jail -U root -c path=${MNT} command=uname -r`
+		ARCH=`jail -u root -c path=${MNT} command=uname -p`
 		
 		printf '%-20s %-13s %s\n' ${JAILNAME} ${VERSION} ${ARCH}
 	fi
-
 done
