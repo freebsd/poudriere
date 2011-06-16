b5aca0ea5f78ba026dc05b5bd305392a136dc930
Freddy Dissaux <freddy.dxs@free.fr>
2011-06-16 19:17:18 +0000

    don't mix echo and cat style use uname -m only once

--- src/poudriere.d/create_jail.sh
+++ src/poudriere.d/create_jail.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 usage() {
-	echo "poudriere createjail parameters [options]"
-cat <<EOF
+	echo "poudriere createjail parameters [options]
 
 Parameters:
     -j jailname -- Specifies the jailname
@@ -17,13 +16,13 @@ Options:
     -s          -- Installs the whole source tree, some ports may need it
                    (Default: install only kernel sources)
     -n server   -- Use server as namserver from the jail (Default: same
-                   /etc/resolv.conf as the host system)
-EOF
+                   /etc/resolv.conf as the host system)"
 	exit 1
 }
 
 
 ARCH=`uname -m`
+REALARCH=${ARCH}
 METHOD="FTP"
 NAMESERVER=""
 
@@ -45,30 +44,30 @@ SRCSNAME="ssys"
 while getopts "j:v:a:z:m:sn:" FLAG; do
 	case "${FLAG}" in
 		j)
-		NAME=${OPTARG}
-		;;
+			NAME=${OPTARG}
+			;;
 		v)
-		VERSION=${OPTARG}
-		;;
+			VERSION=${OPTARG}
+			;;
 		a)
-		if [ `uname -m` != "amd64" ]; then
-			err 1 "Only amd64 host can choose another architecture"
-		fi
-		ARCH=${OPTARG}
-		;;
+			if [ "${REALARCH}" != "amd64" ]; then
+				err 1 "Only amd64 host can choose another architecture"
+			fi
+			ARCH=${OPTARG}
+			;;
 		m)
-		METHOD=${OPTARG}
-		;;
+			METHOD=${OPTARG}
+			;;
 		s)
-		SRCS="s*"
-		SRCSNAME="sources"
-		;;
+			SRCS="s*"
+			SRCSNAME="sources"
+			;;
 		n)
-		NAMESERVER=${OPTARG}
-		;;
+			NAMESERVER=${OPTARG}
+			;;
 		*)
 			usage
-		;;
+			;;
 	esac
 done
 
@@ -90,17 +89,19 @@ JAILBASE=${BASEFS:=/usr/local/poudriere}/jails/${NAME}
 msg_n "Creating ${NAME} fs..."
 zfs create -o mountpoint=${JAILBASE} ${ZPOOL}/poudriere/${NAME} >/dev/null 2>&1 || err 1 " Fail" && echo " done"
 
-
 #We need to fetch base and src (for drivers)
 msg_n "Fetching base sets for FreeBSD $VERSION $ARCH"
 PKGS=`echo "ls base*"| ftp -aV ftp://${FTPHOST:=ftp.freebsd.org}/pub/FreeBSD/releases/${ARCH}/${VERSION}/base/ | awk '/-r.*/ {print $NF}'`
 mkdir ${JAILBASE}/fromftp
+
 for pkg in ${PKGS}; do
-# Let's retry at least one time
+	# Let's retry at least one time
 	fetch -o ${JAILBASE}/fromftp/${pkg} ftp://${FTPHOST}/pub/FreeBSD/releases/${ARCH}/${VERSION}/base/${pkg} || fetch -o ${JAILBASE}/fromftp/${pkg} ftp://${FTPHOST}/pub/FreeBSD/releases/${ARCH}/${VERSION}/base/${pkg}
 done
+
 msg_n "Extracting base..."
 cat ${JAILBASE}/fromftp/base.* | tar --unlink -xpzf - -C ${JAILBASE}/ || err 1 " Fail" && echo " done"
+
 msg_n "Cleaning Up base sets..."
 rm ${JAILBASE}/fromftp/*
 echo " done"
@@ -108,15 +109,17 @@ echo " done"
 msg "Fetching ${SRCSNAME} sets..."
 PKGS=`echo "ls ${SRCS}"| ftp -aV ftp://${FTPHOST:=ftp.freebsd.org}/pub/FreeBSD/releases/${ARCH}/${VERSION}/src/ | awk '/-r.*/ {print $NF}'`
 for pkg in ${PKGS}; do
-# Let's retry at least one time
+	# Let's retry at least one time
 	fetch -o ${JAILBASE}/fromftp/${pkg} ftp://${FTPHOST}/pub/FreeBSD/releases/${ARCH}/${VERSION}/src/${pkg} || fetch -o ${JAILBASE}/fromftp/${pkg} ftp://${FTPHOST}/pub/FreeBSD/releases/${ARCH}/${VERSION}/src/${pkg}
 done
+
 msg "Extracting ${SRCSNAME}:"
 for SETS in ${JAILBASE}/fromftp/*.aa; do
 	SET=`basename $SETS .aa`
 	echo -e "\t- $SET...\c"
 	cat ${JAILBASE}/fromftp/${SET}.* | tar --unlink -xpzf - -C ${JAILBASE}/usr/src || err 1 " Fail" && echo " done"
 done
+
 msg_n "Cleaning Up ${SRCSNAME} sets..."
 rm ${JAILBASE}/fromftp/*
 echo " done"
@@ -127,9 +130,9 @@ OSVERSION=`awk '/\#define __FreeBSD_version/ { print $3 }' ${JAILBASE}/usr/inclu
 
 LOGIN_ENV=",UNAME_r=${VERSION},UNAME_v=FreeBSD ${VERSION},OSVERSION=${OSVERSION}"
 
-if [ "${ARCH}" = "i386" -a `uname -m` = "amd64" ];then
-LOGIN_ENV="${LOGIN_ENV},UNAME_p=i386,UNAME_m=i386"
-cat > ${JAILBASE}/etc/make.conf << EOF
+if [ "${ARCH}" = "i386" -a "${REALARCH}" = "amd64" ];then
+	LOGIN_ENV="${LOGIN_ENV},UNAME_p=i386,UNAME_m=i386"
+	cat > ${JAILBASE}/etc/make.conf << EOF
 MACHINE=i386
 MACHINE_ARCH=i386
 EOF
