da1a10c8ba8df0b1395e7902986db9cc36ecc4cc
romain@blogreen.org <romain@blogreen.org>
2011-07-28 09:14:00 +0000

    Fix $[POUDRIERE_]PORTSDIR.
    Pointy hat to:	bapt

--- src/poudriere.d/bulk.sh
+++ src/poudriere.d/bulk.sh
@@ -126,7 +126,7 @@ for JAILNAME in ${JAILNAMES}; do
 		for pkg_file in `ls ${POUDRIERE_DATA}/packages/bulk-${JAILNAME}/All/*.tbz`; do
 			msg_n "extracting description from `basename ${pkg_file}`"
 			ORIGIN=`/usr/sbin/pkg_info -qo ${pkg_file}`
-			[ -d ${PORTSDIR}/${ORIGIN} ] && jexec -U root ${JAILNAME} make -C /usr/ports/${ORIGIN} describe >> ${INDEXF}.1
+			[ -d ${POUDRIERE_PORTSDIR}/${ORIGIN} ] && jexec -U root ${JAILNAME} make -C /usr/ports/${ORIGIN} describe >> ${INDEXF}.1
 			echo " done"
 		done
 
