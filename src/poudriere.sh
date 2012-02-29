f740c4df7e29e404c57f662e0290a8969faae4ec
Baptiste Daroussin <bapt@FreeBSD.org>
2012-02-29 22:57:01 +0000

    Add cron/queue usage add new help subcommand

--- src/poudriere.sh
+++ src/poudriere.sh
@@ -8,8 +8,11 @@ usage() {
 
 Commands:
     bulk        -- generate packages for given ports
+    cron        -- run poudriere from the crontab
+    help        -- show usage informations
     jail        -- manage the jails used by poudriere
     ports       -- create, update or delete the portstrees used by poudriere
+    queue       -- queue a build request (through cron)
     testport    -- launch a test on a given port"
 	exit 1
 }
@@ -41,6 +44,9 @@ case ${CMD} in
 		;;
 	cron)
 		/bin/sh ${POUDRIEREPREFIX}/cron.sh
+		;;
+	help)
+		usage
 		;;
 	*)
 		echo "Unknown command ${CMD}"
