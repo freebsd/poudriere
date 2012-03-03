6e81705a4d4552a31710952f6a9d208d4e8d6e32
Baptiste Daroussin <bapt@FreeBSD.org>
2012-03-03 20:37:46 +0000

    Remove useless use of awk

--- src/poudriere.d/queue.sh
+++ src/poudriere.d/queue.sh
@@ -11,9 +11,8 @@ SCRIPTPREFIX=`dirname ${SCRIPTPATH}`
 
 [ -z ${CRONDIR} ] && err 1 "Please provide a CRONDIR variable in your poudriere.conf"
 perms=`stat ${CRONDIR} | awk '{print $3}'`
-[ $perms != "drwxrwxrwt" ] && err 1 "Please ask your admin to fix permissions on ${CRONDIR} (see poudriere.conf)"
+[ `stat -f '%Sp' ${CRONDIR}` != "drwxrwxrwt" ] && err 1 "Please fix permissions on ${CRONDIR} (see poudriere.conf)"
 
-for ARG in $@
-do
+for ARG in $@; do
 	echo -n "$ARG " >> ${CRONDIR}/poudriere-`date +%s`
 done
