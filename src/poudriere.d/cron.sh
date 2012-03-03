6db977aba9937e380943ae19575e52e41d0ce5c8
Baptiste Daroussin <bapt@FreeBSD.org>
2012-03-03 20:35:13 +0000

    Simplify cron.sh
    no need to loop over the files in crondir
    while here removed hard coded poudriere-data
    remove useless use of awk

--- src/poudriere.d/cron.sh
+++ src/poudriere.d/cron.sh
@@ -4,21 +4,14 @@ set -e
 SCRIPTPATH=`realpath $0`
 SCRIPTPREFIX=`dirname ${SCRIPTPATH}`
 . ${SCRIPTPREFIX}/common.sh
-OLD=`date +%s`
 
 [ -z ${CRONDIR} ] && err 1 "Please provide a CRONDIR variable in your poudriere.conf"
-perms=`stat ${CRONDIR} | awk '{print $3}'`
-[ $perms != "drwxrwxrwt" ] && err 1 "Please fix permissions on ${CRONDIR} (see poudriere.conf)"
+[ `stat -f '%Sp' ${CRONDIR}` != "drwxrwxrwt" ] && err 1 "Please fix permissions on ${CRONDIR} (see poudriere.conf)"
 
-
-if [ $(find /tmp/poudriere-data/ -name poudriere-* | wc -l) -ne 0 ]; then
-	for i in `ls ${CRONDIR}/poudriere-*`; do
-		AGE=`echo $i | awk -F - '{ print $3 }'`;
-		if [ $AGE -lt $OLD ]; then
-			OLD=$AGE	
-		fi 
-	done
-
-	COMMAND=`cat ${CRONDIR}/poudriere-${OLD}`
-	poudriere ${COMMAND} && rm ${CRONDIR}/poudriere-${OLD}
+if [ -d ${CRONDIR} ]; then
+	CMDFILE=`ls -t ${CRONDIR}/poudriere-* 2>/dev/null | tail -1`
+	if [ -z "${CMDFILE}" ]; then
+		COMMAND=`cat ${CMDFILE}`
+		poudriere ${COMMAND} && rm ${CMDFILE}
+	fi
 fi
