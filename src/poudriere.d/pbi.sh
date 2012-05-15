bf9219e6e718bfa564dc20ea611d6694c3408a19
Baptiste Daroussin <bapt@FreeBSD.org>
2012-05-15 15:00:12 +0000

    Create pbi out of pcbsd svn

--- src/poudriere.d/pbi.sh
+++ src/poudriere.d/pbi.sh
@@ -5,13 +5,12 @@ usage() {
 	echo "poudriere pbi parameters [options]
 
 Parameters:
-    -o origin   -- Specify an origin in the portstree
+    -d pbidir   -- Directory for the pbi metadata files
 
 Options:
     -j name     -- Run only inside the given jail
-    -l:         -- Localbase
+    -l path     -- Localbase
     -p tree     -- Specify on which portstree we work
-    -m mail     -- Mail address of the maintainer
     -r          -- Create real pbi"
 	exit 1
 }
@@ -22,11 +21,8 @@ SCRIPTPREFIX=`dirname ${SCRIPTPATH}`
 REAL=0
 PTNAME="default"
 
-while getopts "o:l:j:p:rm:" FLAG; do
+while getopts "l:j:p:rd:" FLAG; do
 	case "${FLAG}" in
-		o)
-			ORIGIN=${OPTARG}
-			;;
 		j)
 			jail_exists ${OPTARG} || err 1 "No such jail: ${OPTARG}"
 			JAILNAMES="${JAILNAMES} ${OPTARG}"
@@ -40,8 +36,8 @@ while getopts "o:l:j:p:rm:" FLAG; do
 		r)
 			REAL=1
 			;;
-		m)
-			MAINTAINER=${OPTARG}
+		d)
+			PBIDIRECTORY=${OPTARG}
 			;;
 		*)
 			usage
@@ -49,6 +45,9 @@ while getopts "o:l:j:p:rm:" FLAG; do
 	esac
 done
 
+test -f ${PBIDIRECTORY}/pbi.conf || usage
+. ${PBIDIRECTORY}/pbi.conf
+ORIGIN=${PBI_MAKEPORT}
 test -z ${ORIGIN} && usage
 test -z ${MYBASE} && usage
 
@@ -62,6 +61,9 @@ for JAILNAME in ${JAILNAMES}; do
 	ZVERSION=`jail_get_zpool_version ${JAILNAME}`
 
 	prepare_jail
+	mkdir -p ${JAILBASE}/pbi
+	mount -t nullfs ${PBIDIRECTORY} ${JAILBASE}/pbi
+
 	echo "LOCALBASE=${MYBASE}" >> ${JAILBASE}/etc/make.conf
 	echo "WITH_PKGNG=yes" >> ${JAILBASE}/etc/make.conf
 
@@ -85,7 +87,7 @@ for JAILNAME in ${JAILNAMES}; do
 	echo pkg -j ${JAILNAME} add /usr/ports/packages/All/${PKGNAME}.${EXT}
 	/usr/local/sbin/pkg -j ${JAILNAME} add /usr/ports/packages/All/${PKGNAME}.${EXT}
 	if [ $REAL -eq 0 ]; then
-		ABI=`/usr/local/sbin/pkg -j ${JAILNAME} -v | awk '/^abi/ { print $2 }'`
+		ABI=`/usr/local/sbin/pkg -j ${JAILNAME} -vv | awk '/^abi/ { print $2 }'`
 	else
 		ABI=`injail uname -m`
 	fi
@@ -118,18 +120,19 @@ for JAILNAME in ${JAILNAMES}; do
 		done
 	if [ $REAL -eq 0 ]; then
 		echo "
-name:  ${PKGNAME%-*}
+name:  ${PBI_PROGNAME}
 version: ${PKGNAME##*-}
 prefix: ${MYBASE}
 origin: pbi/${ORIGIN}
-www: ${WWW:-unknown}
-maintainer: ${MAINTAINER:-unknown}
+www: ${PBI_PROGWEB:-unknown}
+maintainer: ${PBI_PROGAUTHOR:-unknown}
 comment: ${COMMENT:-none}
 arch: ${ABI}
 desc: |-
   This is a test
 files:
 " >> ${JAILBASE}/+MANIFEST
+		cat  ${JAILBASE}/+MANIFEST
 		sort ${JAILBASE}/files >> ${JAILBASE}/+MANIFEST
 		echo "directories:" >> ${JAILBASE}/+MANIFEST
 		sort -r ${JAILBASE}/dirs >> ${JAILBASE}/+MANIFEST
@@ -138,22 +141,25 @@ files:
 	else
 		injail tar cfJ /data.txz -s ",${MYBASE},,g" `awk '{ printf("%s ", $0) } END { printf("\n") }' ${JAILBASE}/files`
 		mkdir ${JAILBASE}/head
-		echo ${PKGNAME%-*} > ${JAILBASE}/head/pbi_name
+		echo ${PBI_PROGNAME} > ${JAILBASE}/head/pbi_name
 		echo ${PKGNAME##*-} > ${JAILBASE}/head/pbi_version
-		echo ${MAINTAINER:-unknown} > ${JAILBASE}/head/pbi_author
-		echo ${WWW:-unkown} > ${JAILBASE}/head/pbi_web
+		echo ${PBI_PROGAUTHOR:-unknown} > ${JAILBASE}/head/pbi_author
+		echo ${PBI_PROGWEB:-unknown} > ${JAILBASE}/head/pbi_web
 		echo ${ABI} > ${JAILBASE}/head/pbi_arch
 		wc -l ${JAILBASE}/files | awk '{ print $1 }' > ${JAILBASE}/head/pbi_archive_count
 		injail uname -r > ${JAILBASE}/head/pbi_fbsdver
 		echo ${MYBASE} > ${JAILBASE}/head/pbi_defaultpath
 		date "+%Y%m%d %H%M%S" > ${JAILBASE}/head/pbi_mdate
 		sha256 -q ${JAILBASE}/data.txz > ${JAILBASE}/head/pbi_archivesum
+		[ -e "${JAILBASE}/pbi/resources/gui_banner.png" ] && cp ${JAILBASE}/pbi/resources/gui_banner.png ${JAILBASE}/head/top_banner.png
+		[ -e "${JAILBASE}/pbi/resources/gui_sidebanner.png" ] && cp ${JAILBASE}/pbi/resources/gui_banner.png ${JAILBASE}/head/side-banner.png
+		if [ -n "${PBI_PROGICON}" -a -e "${JAILBASE}/pbi/resources/${PBI_PROGICON}" ]; then
+			cp "${JAILBASE}/pbi/resources/${PBI_PROGICON}" "${PBI_HEADERDIR}/pbi_icon.${PBI_PROGICON##*.}"
+		fi
 		injail tar cjf /head.tbz -C head .
 		injail cat /head.tbz > ${JAILBASE}/${PKGNAME}.pbi
 		echo -e "\n_PBI_ICON_" >> ${JAILBASE}/${PKGNAME}.pbi
-		# to fix
-		touch ${JAILBASE}/icon.png
-		injail cat /icon.png >> ${JAILBASE}/${PKGNAME}.pbi
+		injail cat /pbi/resources/${PBI_PROGICON} >> ${JAILBASE}/${PKGNAME}.pbi
 		echo -e "\n_PBI_ARCHIVE_" >> ${JAILBASE}/${PKGNAME}.pbi
 		injail cat /data.txz >> ${JAILBASE}/${PKGNAME}.pbi
 		mv ${JAILBASE}/${PKGNAME}.pbi .
