7ec6182e794385488ff55f5ec688e1bddf597d38
Bryan Drewery <bryan@shatow.net>
2012-07-23 18:29:53 +0000

    Quote pkg being deleted

--- src/poudriere.d/clean.sh
+++ src/poudriere.d/clean.sh
@@ -5,4 +5,4 @@ port=$2
 
 name=$(awk -v n=${port} '$1 == n { print $2 }' "${JAILMNT}/cache")
 rm -rf "${JAILMNT}/pool/${name}"
-find ${JAILMNT}/pool -name ${name} -type f -delete
+find ${JAILMNT}/pool -name "${name}" -type f -delete
