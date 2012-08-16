a408191086e1795c111ba03a73fb1af3c0f86894
Bryan Drewery <bryan@shatow.net>
2012-08-16 16:13:20 +0000

    Avoid basename here

--- src/poudriere.d/bulk.sh
+++ src/poudriere.d/bulk.sh
@@ -209,7 +209,7 @@ else
 	OSMAJ=`injail uname -r | awk -F. '{ print $1 }'`
 	INDEXF=${PKGDIR}/INDEX-${OSMAJ}
 	for pkg_file in `ls ${PKGDIR}/All/*.tbz`; do
-		msg_n "extracting description from `basename ${pkg_file}`"
+		msg_n "extracting description from ${pkg_file##*/}"
 		ORIGIN=`/usr/sbin/pkg_info -qo "${pkg_file}"`
 		[ -d ${PORTSDIR}/${ORIGIN} ] && injail make -C /usr/ports/${ORIGIN} describe >> ${INDEXF}.1
 		echo " done"
