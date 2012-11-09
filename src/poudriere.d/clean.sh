e9323c7a6ff2bb7d733eb0cfef9994afa3a67a3e
Bryan Drewery <bryan@shatow.net>
2012-11-09 19:09:16 +0000

    Refactor the -d rpool/ check to only be called once

--- src/poudriere.d/clean.sh
+++ src/poudriere.d/clean.sh
@@ -23,10 +23,10 @@ clean_pool() {
 	local clean_rdepends=$2
 	local dep_dir dep_pkgname
 
-	# Determine everything that depends on the given package
-	# Recursively cleanup anything that depends on this port.
-	if [ ${clean_rdepends} -eq 1 ]; then
-		if [ -d "${JAILMNT}/poudriere/rpool/${pkgname}" ]; then
+	if [ -d "${JAILMNT}/poudriere/rpool/${pkgname}" ]; then
+		# Determine everything that depends on the given package
+		# Recursively cleanup anything that depends on this port.
+		if [ ${clean_rdepends} -eq 1 ]; then
 			for dep_pkgname in $(ls "${JAILMNT}/poudriere/rpool/${pkgname}/"); do
 
 				# clean_pool() in common.sh will pick this up and add to SKIPPED
@@ -35,22 +35,22 @@ clean_pool() {
 				clean_pool ${dep_pkgname} ${clean_rdepends}
 			done
 		fi
+
+		# Determine which packages are ready-to-build,
+		# and move from deps/ to pool/
+		if [ -z "$(find "${JAILMNT}/poudriere/rpool/${pkgname}" -type d -maxdepth 0 -empty)" ]; then
+			for dep_dir in ${JAILMNT}/poudriere/rpool/${pkgname}/*; do
+				dep_pkgname=${dep_dir##*/}
+				rm -f "${JAILMNT}/poudriere/deps/${dep_pkgname}/${pkgname}"
+				# If that packages was just waiting on my package, and
+				# is now ready-to-build, move it to pool/
+				find "${JAILMNT}/poudriere/deps/${dep_pkgname}" \
+					-type d -maxdepth 0 -empty \
+					-exec mv {} "${JAILMNT}/poudriere/pool" \;
+			done
+		fi
 	fi
 
-	# Determine which packages are ready-to-build,
-	# and move from deps/ to pool/
-	if [ -d "${JAILMNT}/poudriere/rpool/${pkgname}" ] && \
-		[ -z "$(find "${JAILMNT}/poudriere/rpool/${pkgname}" -type d -maxdepth 0 -empty)" ]; then
-		for dep_dir in ${JAILMNT}/poudriere/rpool/${pkgname}/*; do
-			dep_pkgname=${dep_dir##*/}
-			rm -f "${JAILMNT}/poudriere/deps/${dep_pkgname}/${pkgname}"
-			# If that packages was just waiting on my package, and
-			# is now ready-to-build, move it to pool/
-			find "${JAILMNT}/poudriere/deps/${dep_pkgname}" \
-				-type d -maxdepth 0 -empty \
-				-exec mv {} "${JAILMNT}/poudriere/pool" \;
-		done
-	fi
 	rm -rf "${JAILMNT}/poudriere/pool/${pkgname}" \
 		"${JAILMNT}/poudriere/deps/${pkgname}" \
 		"${JAILMNT}/poudriere/rpool/${pkgname}" 2>/dev/null || :
