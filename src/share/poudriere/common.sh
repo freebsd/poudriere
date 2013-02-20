45cb15721c2469833059a95f3196d99e94087529
Bryan Drewery <bryan@shatow.net>
2013-02-20 02:58:10 +0000

    Run log_stop in exit_handler before killing children. Not doing so causes the children kill to immediately exit the parent process (even though it is NOT being killed)

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -136,6 +136,8 @@ exit_handler() {
 	# Ignore SIGINT while cleaning up
 	trap '' SIGINT
 
+	log_stop
+
 	# Kill all children - this does NOT recurse, so orphans can still
 	# occur. This is just to avoid requiring pid files for parallel_run
 	for pid in $(jobs -p); do
@@ -520,7 +522,6 @@ cleanup() {
 	fi
 	export CLEANING_UP=1
 	[ -z "${JAILNAME%-job-*}" ] && err 2 "Fail: Missing JAILNAME"
-	log_stop
 
 	if [ -d ${MASTERMNT:-${JAILMNT}}/poudriere/var/run ]; then
 		for pid in ${MASTERMNT:-${JAILMNT}}/poudriere/var/run/*.pid; do
