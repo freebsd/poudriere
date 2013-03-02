46e6055e2ee5672b72387d2ea101a1d142a327ee
Bryan Drewery <bryan@shatow.net>
2013-03-02 03:41:30 +0000

    Sort the ready-to-build pool by the number of packages needing each package This allows libraries and large dependencies to be built first, thus allowing the build to avoid a bottleneck later if it finds it needs to stop and build dependencies before moving on.
    Submitted by:	Matthew Dillon <dillon@apollo.backplane.com>

--- src/share/poudriere/clean.sh
+++ src/share/poudriere/clean.sh
@@ -22,6 +22,7 @@ clean_pool() {
 	local pkgname=$1
 	local clean_rdepends=$2
 	local dep_dir dep_pkgname
+	local xdep xcount
 
 	if [ -d "${JAILMNT}/poudriere/rdeps/${pkgname}" ]; then
 		# Determine which packages are ready-to-build and
@@ -43,9 +44,18 @@ clean_pool() {
 					rm -f "${JAILMNT}/poudriere/deps/${dep_pkgname}/${pkgname}"
 					# If that packages was just waiting on my package, and
 					# is now ready-to-build, move it to pool/
-					find "${JAILMNT}/poudriere/deps/${dep_pkgname}" \
-						-type d -maxdepth 0 -empty \
-						-exec mv {} "${JAILMNT}/poudriere/pool" \;
+					for xdep in `find ${JAILMNT}/poudriere/deps/${dep_pkgname} -type d -maxdepth 0 -empty 2>/dev/null || :`; do
+						xdep=${xdep##*/}
+						xcount=`find "${JAILMNT}/poudriere/rdeps/${xdep}" | wc -l`
+						xcount=$(($xcount - 1))
+						if [ ${xcount} -lt 0 ]; then
+							xcount=0
+						fi
+						if [ ${xcount} -gt 9 ]; then
+							xcount=9
+						fi
+						mv ${JAILMNT}/poudriere/deps/${xdep} ${JAILMNT}/poudriere/pool${xcount}/
+					done
 				fi
 			done
 		fi
