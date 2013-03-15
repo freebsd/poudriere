339f0a4047d436d10549b411056373016ef13a7f
Bryan Drewery <bryan@shatow.net>
2013-03-15 00:28:52 +0000

    Fix detection of IPv4 support. net.inet is not a good check. kern.features.inet[6] is supported on all supported FreeBSD branches.
    Reported by:	flo

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -2,7 +2,7 @@
 
 # zfs namespace
 NS="poudriere"
-IPS="$(sysctl -n kern.features.inet 2>/dev/null || (sysctl -n net.inet 1>/dev/null 2>&1 && echo 1) || echo 0)$(sysctl -n kern.features.inet6 2>/dev/null || (sysctl -n net.inet6 1>/dev/null 2>&1 && echo 1) || echo 0)"
+IPS="$(sysctl -n kern.features.inet 2>/dev/null || echo 0)$(sysctl -n kern.features.inet6 2>/dev/null || echo 0)"
 RELDATE=$(sysctl -n kern.osreldate)
 JAILED=$(sysctl -n security.jail.jailed)
 
