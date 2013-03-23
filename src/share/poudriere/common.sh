freebsd/poudriere

commit e123cc62075eaf62f6369e80b10bbc1419620460

When a build is done, detect+log+kill leftover processes
This fixes ports such as multimedia/totem which start dbus during
build and then cause the build to hang on completion. This also
fixes these processes potentially sticking around for the next
build in that jail.

src/share/poudriere/common.sh
@@ -1467,9 +1467,27 @@ build_pkg() {
 	clean_pool ${PKGNAME} ${clean_rdepends}
 
 	bset ${MY_JOBID} status "done:${port}"
-	buildlog_stop ${portdir}
-	log_stop ${log}/logs/${PKGNAME}.log
+
+	stop_build ${portdir} ${log}/logs/${PKGNAME}.log
+
 	echo ${MY_JOBID} >&6
+}
+
+stop_build() {
+	[ $# -eq 2 ] || eargs portdir logfile
+	local portdir="$1"
+	local logfile="$2"
+
+	# 2 = HEADER+ps itself
+	if [ $(injail ps aux | wc -l) -ne 2 ]; then
+		msg "Leftover processes:"
+		injail ps auxwwd | grep -v 'ps auxwwd'
+	fi
+	# Always kill to avoid missing anything
+	injail kill -9 -1
+
+	buildlog_stop ${portdir}
+	log_stop ${logfile}
 }
 
 # Crazy redirection is to add the portname into stderr.

src/share/poudriere/testport.sh
@@ -177,8 +177,7 @@ if ! build_port /usr/ports/${ORIGIN}; then
 	save_wrkdir ${MASTERMNT} "${PKGNAME}" "/usr/ports/${ORIGIN}" "${failed_phase}" || :
 
 	if [ ${INTERACTIVE_MODE} -eq 0 ]; then
-		buildlog_stop /usr/ports/${ORIGIN}
-		log_stop ${log}/logs/${PKGNAME}.log
+		stop_build /usr/ports/${ORIGIN} ${log}/logs/${PKGNAME}.log
 		exit 1
 	fi
 fi
@@ -222,8 +221,7 @@ msg "Deinstalling package"
 injail ${PKG_DELETE} ${PKGNAME}
 
 msg "Removing existing ${PREFIX} dir"
-buildlog_stop /usr/ports/${ORIGIN}
-log_stop ${log}/logs/${PKGNAME}.log
+stop_build /usr/ports/${ORIGIN} ${log}/logs/${PKGNAME}.log
 
 cleanup
 set +e

commit 94cd3fe66a2b250c1831356cb56fb77d08ded7f5

Fix crash when no leftover processes exists (broken by [e123cc6])

src/share/poudriere/common.sh
@@ -1484,7 +1484,7 @@ stop_build() {
 		injail ps auxwwd | grep -v 'ps auxwwd'
 	fi
 	# Always kill to avoid missing anything
-	injail kill -9 -1
+	injail kill -9 -1 2>/dev/null || :
 
 	buildlog_stop ${portdir}
 	log_stop ${logfile}