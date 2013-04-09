82d0c32503b76f0f188bcd01d0c1feab9b450a04
Baptiste Daroussin <bapt@FreeBSD.org>
2013-04-09 09:44:16 +0000

    Add a small C code to determine if a directory is empty (faster then find for this)

---
+++ src/libexec/poudriere/dirempty.c
@@ -0,0 +1,65 @@
+/*-
+ * Copyright (c) 2013 Baptiste Daroussin <bapt@FreeBSD.org>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <err.h>
+ 
+bool
+dir_empty(const char *path)
+{
+	struct dirent *ent;
+	bool ret = true;
+	DIR *d;
+
+	if ((d = opendir(path)) == NULL)
+		err(EXIT_FAILURE, "%s: ", path);
+
+	while ((ent = readdir(d))) {
+		if (strcmp(ent->d_name, ".") == 0 || (strcmp(ent->d_name, "..")) == 0)
+			continue;
+		ret = false;
+		break;
+	}
+	closedir(d);
+
+	return (ret);
+}
+ 
+int
+main(int argc, char **argv)
+{
+	if (argc != 2)
+		err(EXIT_FAILURE, "Argument missing");
+ 
+	if (dir_empty(argv[1]))
+		return (EXIT_SUCCESS);
+
+	return (EXIT_FAILURE);
+}
