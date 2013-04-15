freebsd/poudriere

commit ffd3f858f017096d615341223d34c2da0273b35e

Generalize the deleted PREFIX exemption where it does not equal LOCALBASE
This is for ports that redefine PREFIX, such as misc/kdehier4 and mail/qmail
that may properly remove PREFIX on deinstall

src/share/poudriere/common.sh
@@ -917,7 +917,7 @@ build_port() {
 
 			check_leftovers ${mnt} | \
 				while read modtype path; do
-				local ppath
+				local ppath ignore_path=0
 
 				# If this is a directory, use @dirrm in output
 				if [ -d "${path}" ]; then
@@ -940,12 +940,14 @@ build_port() {
 					esac
 					;;
 				-) 
-					case "${ppath}" in
-					# This needs a better fix, but for now, install-mtree to create a prefix before 
-					# the preinst is created and the port removes it.
-					%%KDE4_PREFIX%%) ;;
-					*) echo "${ppath}" >> ${del} ;;
-					esac
+					# Skip if it is PREFIX and non-LOCALBASE. See misc/kdehier4
+					# or mail/qmail for examples
+					[ "${ppath}" = "${PREFIX}" -a \
+						"${LOCALBASE}" != "${PREFIX}" ] && ignore_path=1
+
+					if [ $ignore_path -ne 0 ]; then
+						echo "${ppath}" >> ${del}
+					fi
 					;;
 				M)
 					[ -d "${path}" ] && continue

commit 47966f39bb38568af36a0bba334d3b0008f3d8d7

Fix deleted dir detection.
Broken by:	[ffd3f85]

src/share/poudriere/common.sh
@@ -1146,10 +1146,10 @@ build_port() {
 				-)
 					# Skip if it is PREFIX and non-LOCALBASE. See misc/kdehier4
 					# or mail/qmail for examples
-					[ "${ppath}" = "${PREFIX}" -a \
+					[ "${path#${mnt}}" = "${PREFIX}" -a \
 						"${LOCALBASE}" != "${PREFIX}" ] && ignore_path=1
 
-					if [ $ignore_path -ne 0 ]; then
+					if [ $ignore_path -eq 0 ]; then
 						echo "${ppath}" >> ${del}
 					fi
 					;;