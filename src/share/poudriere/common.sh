3a299603ece5b27da22284d58e2dd6afead30c05
Baptiste Daroussin <bapt@FreeBSD.org>
2013-04-17 19:01:32 +0000

    Add bpf* to list of device to unhide to fix libnet10

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -629,7 +629,7 @@ do_jail_mounts() {
 	[ $# -ne 2 ] && eargs mnt arch
 	local mnt=$1
 	local arch=$2
-	local devfspath="null zero random urandom stdin stdout stderr fd fd/*"
+	local devfspath="null zero random urandom stdin stdout stderr fd fd/* bpf*"
 
 	# clone will inherit from the ref jail
 	if [ ${mnt##*/} = "ref" ]; then
