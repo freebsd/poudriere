37c7a36ff5def4960d6a1777ae27f664944e5990
Bryan Drewery <bryan@shatow.net>
2013-06-29 11:30:03 +0000

    MFT: Change default build name to YYYY-MM-DD_HHhMMmSSs to remove ':' causing encoding/tab-completion issues

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -2666,6 +2666,6 @@ esac
 : ${WATCHDIR:=${POUDRIERE_DATA}/queue}
 : ${PIDFILE:=${POUDRIERE_DATA}/daemon.pid}
 
-BUILDNAME=$(date +%Y-%m-%d_%H:%M:%S)
+BUILDNAME=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
 [ -d ${WATCHDIR} ] || mkdir -p ${WATCHDIR}
