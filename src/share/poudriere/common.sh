freebsd/poudriere

commit f43a788e0af32bd8a5681f640e04145f66ca3358

Use a wrapper to reset JUSER from build_port

src/share/poudriere/common.sh
@@ -1260,7 +1260,7 @@ gather_distfiles() {
 }
 
 # Build+test port and return on first failure
-build_port() {
+_real_build_port() {
 	[ $# -ne 1 ] && eargs portdir
 	local portdir=$1
 	local port=${portdir##/usr/ports/}
@@ -1582,6 +1582,15 @@ Try testport with -n to use PREFIX=LOCALBASE"
 
 	bset ${MY_JOBID} status "idle:"
 	return 0
+}
+
+# Wrapper to ensure JUSER is reset and any other cleanup needed
+build_port() {
+	local ret
+	_real_build_port "$@" || :
+	ret=$?
+	JUSER=root
+	return ${ret}
 }
 
 # Save wrkdir and return path to file
@@ -1988,7 +1997,6 @@ build_pkg() {
 			save_wrkdir ${mnt} "${port}" "${portdir}" "noneed" ||:
 		fi
 
-		JUSER=root
 		injail make -C ${portdir} clean
 
 		if [ ${build_failed} -eq 0 ]; then

src/share/poudriere/testport.sh
@@ -188,8 +188,6 @@ elif [ -f ${MASTERMNT}/usr/ports/${ORIGIN}/.keep ]; then
 	save_wrkdir ${MASTERMNT} "${PKGNAME}" "/usr/ports/${ORIGIN}" "noneed" ||:
 fi
 
-JUSER=root
-
 if [ -f ${MASTERMNT}/tmp/pkgs/${PKGNAME}.${PKG_EXT} ]; then
 	msg "Installing from package"
 	injail ${PKG_ADD} /tmp/pkgs/${PKGNAME}.${PKG_EXT} || :

commit ee5ca851a7380647f40893ea47533f0dd972aca6

Fix error detection in build_port(), broken in [f43a788]

src/share/poudriere/common.sh
@@ -1601,8 +1601,7 @@ Try testport with -n to use PREFIX=LOCALBASE"
 # Wrapper to ensure JUSER is reset and any other cleanup needed
 build_port() {
 	local ret
-	_real_build_port "$@" || :
-	ret=$?
+	_real_build_port "$@" || ret=$?
 	JUSER=root
 	return ${ret}
 }