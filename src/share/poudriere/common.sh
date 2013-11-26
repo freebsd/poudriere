a9f3d565b75c94375e005d3ca15f2045b080e59e
Baptiste Daroussin <bapt@FreeBSD.org>
2013-11-26 15:26:12 +0000

    if SIGNING command is defined in poudriere.conf it will be used

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -3260,7 +3260,7 @@ build_repo() {
 				/tmp/repo.key
 			rm -f ${MASTERMNT}/tmp/repo.key
 		else
-			injail /poudriere/pkg-static repo /packages
+			injail /poudriere/pkg-static repo /packages ${SIGNING_COMMAND:+signing_command: ${SIGNING_COMMAND}}
 		fi
 		# Remount ro
 		umount ${MASTERMNT}/packages
