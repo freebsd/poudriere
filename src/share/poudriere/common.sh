freebsd/poudriere

commit b93594ccbcbbdbec8bb4598f0231823b1011dbe9

Parallelize sanity_check_pkgs for a big speedup
In a test of bulk -Cf p5.ports, this improved the startup->start build
time from 452 seconds to 169 seconds.

src/share/poudriere/common.sh
@@ -1091,29 +1091,39 @@ package_dir_exists_and_has_packages() {
 	return 0
 }
 
+sanity_check_pkg() {
+	[ $# -eq 1 ] || eargs pkg
+	local pkg="$1"
+	local depfile origin
+
+	origin=$(pkg_get_origin "${pkg}")
+	port_is_needed "${origin}" || return 0
+	depfile="$(deps_file "${pkg}")"
+	while read dep; do
+		if [ ! -e "${PACKAGES}/All/${dep}.${PKG_EXT}" ]; then
+			msg_debug "${pkg} needs missing ${PACKAGES}/All/${dep}.${PKG_EXT}"
+			msg "Deleting ${pkg##*/}: missing dependency: ${dep}"
+			delete_pkg "${pkg}"
+			return 65	# Package deleted, need another pass
+		fi
+	done < "${depfile}"
+
+	return 0
+}
 
 sanity_check_pkgs() {
 	local ret=0
-	local depfile origin
 
 	package_dir_exists_and_has_packages || return 0
 
+	parallel_start
 	for pkg in ${PACKAGES}/All/*.${PKG_EXT}; do
-		origin=$(pkg_get_origin "${pkg}")
-		port_is_needed "${origin}" || continue
-		depfile="$(deps_file "${pkg}")"
-		while read dep; do
-			if [ ! -e "${PACKAGES}/All/${dep}.${PKG_EXT}" ]; then
-				ret=1
-				msg_debug "${pkg} needs missing ${PACKAGES}/All/${dep}.${PKG_EXT}"
-				msg "Deleting ${pkg##*/}: missing dependency: ${dep}"
-				delete_pkg "${pkg}"
-				break
-			fi
-		done < "${depfile}"
+		parallel_run sanity_check_pkg "${pkg}" || ret=$?
 	done
-
-	return $ret
+	parallel_stop || ret=$?
+	[ ${ret} -eq 0 ] && return 0	# Nothing deleted
+	[ ${ret} -eq 65 ] && return 1	# Packages deleted
+	err 1 "Failure during sanity check"
 }
 
 check_leftovers() {

commit 72d5e934b7c3a07dc80638352ef905e708472fc2

Fix sanity checks not properly recursively deleting packages
This was manifest as missing dependencies during package building
due to out-of-order builds caused by the queue not having
the proper dependency list because of not rebuilding some
packages when their dependencies were deleted.

This was mostly due to using || on commands which disabled the
set -e handling. By properly recording return statuses and returning
and checking on them sanity_check_pkgs again works.

This was a regression from [b93594c]

Reported by:	antoine, mat, bdrewery
With help from:	antoine

src/share/poudriere/common.sh
@@ -2947,11 +2947,13 @@ parallel_start() {
 # list.
 _reap_children() {
 	local pid
+	local ret=0
+
 	for pid in ${PARALLEL_PIDS}; do
 		# Check if this pid is still alive
 		if ! kill -0 ${pid} 2>/dev/null; then
 			# This will error out if the return status is non-zero
-			_wait ${pid}
+			_wait ${pid} || ret=$?
 			# Remove pid from PARALLEL_PIDS and strip away all
 			# spaces
 			PARALLEL_PIDS_L=${PARALLEL_PIDS%% ${pid} *}
@@ -2963,22 +2965,27 @@ _reap_children() {
 			PARALLEL_PIDS=" ${PARALLEL_PIDS_L} ${PARALLEL_PIDS_R} "
 		fi
 	done
+
+	return ${ret}
 }
 
 # Wait on all remaining running processes and clean them up. Error out if
 # any have non-zero return status.
 parallel_stop() {
+	local ret=0
+
 	pwait ${PARALLEL_PIDS} 2>/dev/null || :
 	for pid in ${PARALLEL_PIDS}; do
 		# This will read the return code of each child
-		# and properly error out if the children errored
-		wait ${pid}
+		wait ${pid} || ret=$?
 	done
 
 	exec 6<&-
 	exec 6>&-
 	unset PARALLEL_PIDS
 	unset NBPARALLEL
+
+	return ${ret}
 }
 
 parallel_shutdown() {
@@ -3000,8 +3007,8 @@ parallel_run() {
 	# of PID reuse/collision
 	_SHOULD_REAP=$((_SHOULD_REAP + 1))
 	if [ ${_SHOULD_REAP} -eq 16 ]; then
-		_reap_children
 		_SHOULD_REAP=0
+		_reap_children || return $?
 	fi
 
 	PARALLEL_CHILD=1 parallel_exec $cmd "$@" &