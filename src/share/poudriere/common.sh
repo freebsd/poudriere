cae394d6a07a2df556e33e3abb96d4afc0ef32b9
Bryan Drewery <bryan@shatow.net>
2014-01-22 20:00:29 +0000

    Fix typo of host

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1143,7 +1143,7 @@ jail_start() {
 	for kld in ${needkld}; do
 		if ! kldstat -q -m ${kld} ; then
 			if [ $JAILED -eq 0 ]; then
-				err 1 "Please load the ${kld} module on the hist using \"kldload ${kld}\""
+				err 1 "Please load the ${kld} module on the host using \"kldload ${kld}\""
 			else
 				kldload ${kld} || err 1 "Required kernel module '${kld}' not found"
 			fi
