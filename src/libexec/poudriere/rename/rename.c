9c958124f5d47bfcd8d913c06a35220538583ba2
Bryan Drewery <bryan@shatow.net>
2014-01-30 19:20:56 +0000

    ATOMIC_PACKAGE_REPOSITORY: Fix build with 9.1 and 8.x
    mv(1) supports the -h param in 10.0/head/9.2 to rename a symlink instead
    of move it. Add our own rename(1) which will just call rename(2).
    
    This also avoids overhead in mv(1) and may be beneficial in queue
    handling.

---
+++ src/libexec/poudriere/rename/rename.c
@@ -0,0 +1,47 @@
+/*-
+ * Copyright (c) 2014 Bryan Drewery <bdrewery@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <err.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sysexits.h>
+
+/**
+ * Just call rename(2) on the params. This is a replacement for mv(1)
+ * to both lower overhead and to support mv -h, which is not in 8.x or
+ * 9.1, to rename a symlink and not move it.
+ */
+int
+main(int argc, char **argv) {
+
+	if (argc != 3)
+		errx(EX_USAGE, "Usage: rename src dst");
+
+	if (rename(argv[1], argv[2]))
+		err(EXIT_FAILURE, NULL);
+
+	return 0;
+}
--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1081,7 +1081,7 @@ commit_packages() {
 	# Switch latest symlink to new build
 	PACKAGES=${PACKAGES_ROOT}/.latest
 	ln -s ${pkgdir_new} ${PACKAGES_ROOT}/.latest_new
-	mv -fh ${PACKAGES_ROOT}/.latest_new ${PACKAGES}
+	rename ${PACKAGES_ROOT}/.latest_new ${PACKAGES}
 
 	# Look for broken top-level links and remove them, if they reference
 	# the old directory
