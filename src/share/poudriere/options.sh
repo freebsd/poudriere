697e0e020be5bfd4b1f2bd6c4504681afa7fbca9
Bryan Drewery <bryan@shatow.net>
2014-02-20 02:00:29 +0000

    options: Require dialog4ports as it runs from host and jail may be for a different ARCH

--- src/share/poudriere/options.sh
+++ src/share/poudriere/options.sh
@@ -106,6 +106,7 @@ shift $((OPTIND-1))
 export PORTSDIR=`pget ${PTNAME} mnt`
 [ -d "${PORTSDIR}/ports" ] && PORTSDIR="${PORTSDIR}/ports"
 [ -z "${PORTSDIR}" ] && err 1 "No such ports tree: ${PTNAME}"
+which dialog4ports 2>/dev/null || err 1 "You must have ports-mgmt/dialog4ports installed on the host to use this command."
 
 if [ $# -eq 0 ]; then
 	[ -n "${BULK_LIST}" ] || err 1 "No packages specified"
