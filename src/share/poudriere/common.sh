f81506463ef0c9eea10483985accedf530149486
Antoine Brodin <antoine@FreeBSD.org>
2014-03-06 13:59:42 +0000

    White list /var/db/dbus/machine-id leftover, requested and tested by miwi@

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1922,6 +1922,9 @@ Try testport with -n to use PREFIX=LOCALBASE"
 					etc/gconf/gconf.xml.defaults/%gconf-tree*.xml) ;;
 					# fc-cache - skip for now
 					/var/db/fontconfig/*) ;;
+					# this leftover has to stay during dbus upgrade
+					# otherwise dbus dies horribly
+					@dirrm\ /var/db/dbus|/var/db/dbus/machine-id) ;;
 					*) echo "${ppath}" >> ${add} ;;
 					esac
 					;;
