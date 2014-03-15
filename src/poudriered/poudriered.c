d1c4cc48cb36cf18162fa68f04dc0c37590097d6
Baptiste Daroussin <bapt@FreeBSD.org>
2014-03-15 22:26:04 +0000

    Bryan also has a copyright on poudriered.c

--- src/poudriered/poudriered.c
+++ src/poudriered/poudriered.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2014 Baptiste Daroussin <bapt@FreeBSD.org>
+ * Copyright (c) 2014 Bryan Drewery <bdrewery@FreeBSD.org>
  * All rights reserved.
  *~
  * Redistribution and use in source and binary forms, with or without
