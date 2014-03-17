db3cc7ac92869ef73295c46fd7d50ee673608ad4
Bryan Drewery <bryan@shatow.net>
2014-03-17 14:06:10 +0000

    MFH: Ignore parents of home directories too during leftovers check

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1530,6 +1530,7 @@ Try testport with -n to use PREFIX=LOCALBASE"
 						# home directory of users created
 						case " ${homedirs} " in
 						*\ ${path}\ *) continue;;
+						*\ ${path}/*\ *) continue;;
 						esac
 					fi
 					case "${ppath}" in
