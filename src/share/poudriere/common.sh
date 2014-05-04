e5e4acab374b109830324fa571ec4112fd2ed628
Bryan Drewery <bryan@shatow.net>
2014-05-04 18:54:39 +0000

    check_leftovers.sh now handles removing redundant dirs

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1938,14 +1938,7 @@ _real_build_port() {
 				msg "Error: Files or directories left over:"
 				die=1
 				grep -v "^@dirrm" ${add}
-				# Remove @dirrm for parent dirs already in plist
-				grep "^@dirrm" ${add} | sort -r | while read \
-				    type dir; do
-					grep -qE \
-					    "^@(unexec rmdir \"?(%D/)?${dir}[ \"]|dirrm(try)? ${dir}\$)" \
-					    ${mnt}${tmpplist} ||
-					    echo "${type} ${dir}"
-				done
+				grep "^@dirrm" ${add} | sort -r
 			fi
 			if [ -s "${del}" ]; then
 				msg "Error: Files or directories removed:"
