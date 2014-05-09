69c480fb2335119666b6425c9d97a88816d94a04
Bryan Drewery <bryan@shatow.net>
2014-05-09 01:53:55 +0000

    HTML: Add default sorts to all tables

--- src/share/poudriere/html/assets/poudriere.js
+++ src/share/poudriere/html/assets/poudriere.js
@@ -799,6 +799,8 @@ function setup_build() {
 				"targets": '_all',
 			}
 		],
+		"stateSave": true, // Enable cookie for keeping state
+		"order": [[0, 'asc']], // Sort by Job ID
 	});
 
 	build_order_column = {
@@ -908,7 +910,6 @@ function setup_jail() {
 	];
 
 	$('#builds_table').dataTable({
-		"aaSorting": [], // No initial sorting
 		"bAutoWidth": false,
 		"processing": true, // Show processing icon
 		"deferRender": true, // Defer creating TR/TD until needed
@@ -929,6 +930,7 @@ function setup_jail() {
 				$('td', row).addClass('latest');
 			}
 		},
+		"order": [[0, 'asc']], // Sort by buildname
 	});
 
 	//applyHovering('builds_table');
@@ -1006,7 +1008,6 @@ function setup_index() {
 	];
 
 	table = $('#latest_builds_table').dataTable({
-		"aaSorting": [], // No initial sorting
 		"bAutoWidth": false,
 		"processing": true, // Show processing icon
 		"deferRender": true, // Defer creating TR/TD until needed
@@ -1014,7 +1015,7 @@ function setup_index() {
 		"stateSave": true, // Enable cookie for keeping state
 		"lengthMenu":[[5,10,25,50,100,200, -1],[5,10,25,50,100,200,"All"]],
 		"pageLength": 50,
-		"order": [[2, 'asc']],
+		"order": [[2, 'asc']], // Sort by buildname
 		"columnDefs": [
 			{
 				"data": null,
