7b603c4fdb95543aa5992d41f3a61b28ef561148
Bryan Drewery <bryan@shatow.net>
2014-05-22 06:13:34 +0000

    processonelog: Update missing_header for clang output

--- src/share/poudriere/processonelog.sh
+++ src/share/poudriere/processonelog.sh
@@ -32,7 +32,7 @@ elif bzgrep -q "Couldn't fetch it - please try" $1; then
   reason="fetch"
 elif bzgrep -q "Error: shared library \".*\" does not exist" $1; then
   reason="LIB_DEPENDS"
-elif bzgrep -qE "\.(c|cc|cxx|cpp|h|y)[1-9:]+ .+\.[hH]: No such file" $1; then
+elif bzgrep -qE "\.(c|cc|cxx|cpp|h|y)[1-9:]+ .+\.[hH](: No such file|' file not found)" $1; then
   reason="missing_header"
 elif bzgrep -qE '(nested function.*declared but never defined|warning: nested extern declaration)' $1; then
   reason="nested_declaration"
