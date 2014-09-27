freebsd/poudriere

commit a12eddf95f455a0c7863c4b65d4ce19d0f696053

Plug jexec/rexec

src/share/poudriere/common.sh
@@ -239,8 +239,11 @@ injail() {
 	local name
 
 	_my_name name
-	jexec -U ${JUSER:-root} ${name}${JNETNAME:+-${JNETNAME}} \
-	    ${MAX_MEMORY_JEXEC} "$@"
+	rexec -s ${MASTERMNT}/.p/${name}${JNETNAME:+-${JNETNAME}}.sock \
+		-u ${JUSER:-root} ${MAX_MEMORY_JEXEC} $@
+
+#	jexec -U ${JUSER:-root} ${name}${JNETNAME:+-${JNETNAME}} \
+#	    ${MAX_MEMORY_JEXEC} "$@"
 }
 
 jstart() {
@@ -256,11 +259,15 @@ jstart() {
 		host.hostname=${BUILDER_HOSTNAME-${name}} \
 		${network} \
 		allow.socket_af allow.raw_sockets allow.chflags allow.sysvipc
+	mkdir -p ${MASTERMNT}/.p/
+	jexecd -j ${name} -d ${MASTERMNT}/.p/
 	jail -c persist name=${name}-n \
 		path=${MASTERMNT}${MY_JOBID+/../${MY_JOBID}} \
 		host.hostname=${BUILDER_HOSTNAME-${name}} \
 		${ipargs} \
 		allow.socket_af allow.raw_sockets allow.chflags allow.sysvipc
+	jexecd -j ${name}-n -d ${MASTERMNT}/.p/
+	injail id ${PORTBUILD_USER} ; echo $?
 	if ! injail id ${PORTBUILD_USER} >/dev/null 2>&1 ; then
 		msg_n "Creating user/group ${PORTBUILD_USER}"
 		injail pw groupadd ${PORTBUILD_USER} -g 65532 || \
@@ -3250,7 +3257,7 @@ delete_old_pkg() {
 				dpath=${d#*:/usr/ports/}
 				case ${td} in
 				LIB)
-					[ -n "${liblist}" ] || liblist=$(injail ldconfig -r | awk '$1 ~ /:-l/ { gsub(/.*-l/, "", $1); printf("%s ",$1) } END { printf("\n") }')
+					[ -n "${liblist}" ] || liblist=$(injail ldconfig -r | awk '$1 ~ /:-l/ { gsub(/.*-l/, "", $2); printf("%s ",$1) } END { printf("\n") }')
 					case ${key} in
 					lib*)
 						unset found

src/libexec/poudriere/jexecd/jexecd.c
@@ -239,7 +239,7 @@ serve(int fd) {
 		if (ke.filter == EVFILT_PROC) {
 			killpg(cl->pid, SIGTERM);
 			nv = nvlist_create(0);
-			nvlist_add_number(nv, "return", WEXITSTATUS(cl->pid));
+			nvlist_add_number(nv, "return", WEXITSTATUS(ke.data));
 			nvlist_send(cl->fd, nv);
 			nvlist_destroy(nv);
 			client_free(cl);
@@ -287,7 +287,7 @@ main(int argc, char **argv)
 			    (intmax_t)otherpid);
 		}
 		/* If we cannot create pidfile from other reasons, only warn. */
-		warn("Cannot open or create pidfile");
+		warn("Cannot open or create pidfile: '%s'", path);
 	}
 
 	memset(&un, 0, sizeof(struct sockaddr_un));

src/libexec/poudriere/rexec/rexec.c
@@ -67,7 +67,7 @@ main(int argc, char **argv)
 	strlcpy(un.sun_path, sock, sizeof(un.sun_path));
 
 	if (connect(fd, (struct sockaddr *) &un, sizeof(struct sockaddr_un)) == -1)
-		err(EXIT_FAILURE, "connect()");
+		err(EXIT_FAILURE, "connect(%s)", sock);
 
 	nv = nvlist_create(0);
 	arguments = nvlist_create(0);

commit 5855b4c6169a07bd70dd06b6099c72372d48e50c

Fix lookup of libraries, regression from a12eddf

src/share/poudriere/common.sh
@@ -3250,7 +3250,7 @@ delete_old_pkg() {
 				dpath=${d#*:/usr/ports/}
 				case ${td} in
 				LIB)
-					[ -n "${liblist}" ] || liblist=$(injail ldconfig -r | awk '$1 ~ /:-l/ { gsub(/.*-l/, "", $2); printf("%s ",$1) } END { printf("\n") }')
+					[ -n "${liblist}" ] || liblist=$(injail ldconfig -r | awk '$1 ~ /:-l/ { gsub(/.*-l/, "", $1); printf("%s ",$1) } END { printf("\n") }')
 					case ${key} in
 					lib*)
 						unset found