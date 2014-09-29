99b9ccbcdb8007a34dc7191ff7506f1fd006307a
Bryan Drewery <bryan@shatow.net>
2014-09-29 10:56:56 -0500

    Remove the : from -s/-c to be consistent with -C
    Reported by:	marino

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -3847,7 +3847,7 @@ prepare_ports() {
 		    msg_n "Cleaning all packages due to newer version of the jail..."
 
 		[ ${CLEAN} -eq 1 ] &&
-		    msg_n "(-c): Cleaning all packages..."
+		    msg_n "(-c) Cleaning all packages..."
 
 		if [ ${JAIL_NEEDS_CLEAN} -eq 1 ] || [ ${CLEAN} -eq 1 ]; then
 			rm -rf ${PACKAGES}/* ${cache_dir}
@@ -3931,7 +3931,7 @@ prepare_ports() {
 			delete_stale_symlinks_and_empty_dirs
 		fi
 	else
-		[ ${SKIPSANITY} -eq 1 ] && sflag="(-s): "
+		[ ${SKIPSANITY} -eq 1 ] && sflag="(-s) "
 		msg "${sflag}Skipping incremental rebuild and repository sanity checks"
 	fi
 
