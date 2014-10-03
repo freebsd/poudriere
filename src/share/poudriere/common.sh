01c2345b8c43be82c1fc5fd89aff11e46106c7da
John Marino <draco@marino.st>
2014-10-04 01:42:13 +0200

    Move build summary after sig-handler builder list, stop showing log info
    Powerful boxes can have 60 builders or more.  Have the builder summary
    come before the builder list just means it scrolls off immediately on
    these setups.  To deal with this, move the builder summary to the end,
    after the builder list is displayed.
    
    For aesthetic reasons (and debatable utility), remove the display of the
    log with the sig-handler.  This line is often greater than 80 characters
    and wraps around on consoles.

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -817,8 +817,6 @@ siginfo_handler() {
 	[ "${status}" = "index:" -o "${status#stopped:}" = "crashed:" ] && \
 	    return 0
 
-	show_build_summary
-
 	now=$(date +%s)
 
 	# Skip if stopping or starting jobs or stopped.
@@ -857,7 +855,7 @@ siginfo_handler() {
 		done
 	fi
 
-	show_log_info
+	show_build_summary
 }
 
 jail_exists() {
