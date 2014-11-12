freebsd/poudriere

commit 1b5a26f6c5153e831be59bea1bc925bebe21749b

HTML: Always install assets into log dirs using a hardlinked base copy.
Timestamps are preserved such that the base copy in
/usr/local/poudriere/data/logs/bulk/.html is only touched when files are
changed. This directory is then hardlink copied into the log
directories. The goal is to be efficient with space usage in the log
dirs, compared to the first implementation of this that duplicated all
HTML files in every log dir.

src/share/poudriere/include/fs.sh
@@ -171,3 +171,18 @@ destroyfs() {
 		rm -rf ${mnt}
 	fi
 }
+
+# Create/Update a base dir and then hardlink-copy the files into the
+# dest dir. This is used for HTML copying to keep space usage efficient.
+hardlink_base_cp() {
+	[ $# -eq 3 ] || eargs hardlink_base_cp src base dest
+	local src="$1"
+	local base="$2"
+	local dest="$3"
+
+	# Update the base copy
+	cpdup -i0 -x "${src}" "${base}"
+	# Hardlink-copy the base into the destination dir.
+	cp -xal "${base}" "${dest}"
+	return 0
+}

configure
@@ -627,6 +627,7 @@ ac_subst_vars='am__EXEEXT_FALSE
 am__EXEEXT_TRUE
 LTLIBOBJS
 LIBOBJS
+INSTALL
 EGREP
 GREP
 CPP
@@ -4369,6 +4370,11 @@ done
 
 
 ac_config_files="$ac_config_files Makefile"
+
+
+# Preserve timestamps for install. This is needed to preserve hardlinks
+# for HTML files.
+INSTALL="${ac_cv_path_install% -c} -Cp"
 
 
 ac_config_files="$ac_config_files src/etc/rc.d/poudriered"

configure.ac
@@ -33,4 +33,8 @@ AC_CHECK_HEADERS_ONCE([fcntl.h])
 
 AC_CONFIG_FILES(Makefile)
 
+# Preserve timestamps for install. This is needed to preserve hardlinks
+# for HTML files.
+AC_SUBST([INSTALL], ["${ac_cv_path_install% -c} -Cp"])
+
 AC_OUTPUT([src/etc/rc.d/poudriered])

src/share/poudriere/common.sh
@@ -3786,7 +3786,7 @@ clean_build_queue() {
 
 prepare_ports() {
 	local pkg
-	local log
+	local log log_top
 	local n pn nbq resuming_build
 	local cache_dir sflag
 
@@ -3808,9 +3808,18 @@ prepare_ports() {
 		"${MASTERMNT}/.p/var/cache/pkgname-origin"
 
 	if was_a_bulk_run; then
+		_log_path_top log_top
 		get_cache_dir cache_dir
-		mkdir -p ${log}/../../latest-per-pkg ${log}/../latest-per-pkg \
-		    ${log}/logs ${log}/logs/errors ${cache_dir}
+
+		# Sync in HTML files through a base dir
+		hardlink_base_cp "${HTMLPREFIX}" "${log_top}/.html" "${log}"
+		# Create log dirs
+		mkdir -p ${log}/../../latest-per-pkg \
+		    ${log}/../latest-per-pkg \
+		    ${log}/logs \
+		    ${log}/logs/errors \
+		    ${cache_dir}
+		# Link this build as the /latest
 		ln -sfh ${BUILDNAME} ${log%/*}/latest
 
 		# Record the SVN URL@REV in the build

commit 2a34b2b8b611606dfb172de355b48236ba97681f

Fix creation of log dir for new runs.
Regression from 1b5a26f.
Reported by:	sbruno

src/share/poudriere/include/fs.sh
@@ -182,6 +182,7 @@ hardlink_base_cp() {
 
 	# Update the base copy
 	cpdup -i0 -x "${src}" "${base}"
+	mkdir -p "${dest}"
 	# Hardlink-copy the base into the destination dir.
 	cp -xal "${base}" "${dest}"
 	return 0