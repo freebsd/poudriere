a5ddc28690b840d41d48b32ab93588d75f7b19a4
Sean Bruno <sbruno@freebsd.org>
2015-01-06 22:52:35 +0000

    for string comparisons, use = not -eq

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -314,7 +314,7 @@ build_and_install_world() {
 	    cat ${POUDRIERED}/${SETNAME}-src.conf >> ${JAILMNT}/etc/src.conf
 	[ -f ${POUDRIERED}/${JAILNAME}-src.conf ] && cat ${POUDRIERED}/${JAILNAME}-src.conf >> ${JAILMNT}/etc/src.conf
 
-	if [ ${TARGET} -eq "mips" ]; then
+	if [ ${TARGET} = "mips" ]; then
 		echo "WITH_ELFTOOLCHAIN_TOOLS=y" >> ${JAILMNT}/etc/src.conf
 	fi
 
