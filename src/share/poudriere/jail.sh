74fd7203d6f16710acace04586a48d96cf1da95d
Brad Davis <brd@FreeBSD.org>
2015-02-23 08:09:56 -0700

    Remove the games distribution. This fixes #275.

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -492,7 +492,7 @@ install_from_ftp() {
 		allbsd) URL="https://pub.allbsd.org/FreeBSD-snapshots/${ARCH}-${ARCH}/${V}-JPSNAP/ftp" ;;
 		ftp-archive) URL="ftp://ftp-archive.freebsd.org/pub/FreeBSD-Archive/old-releases/${ARCH}/${V}" ;;
 		esac
-		DISTS="base dict src games"
+		DISTS="base dict src"
 		[ ${ARCH} = "amd64" ] && DISTS="${DISTS} lib32"
 		for dist in ${DISTS}; do
 			fetch_file ${JAILMNT}/fromftp/ ${URL}/$dist/CHECKSUM.${HASH} ||
@@ -544,7 +544,7 @@ install_from_ftp() {
 			ftp-archive) URL="ftp://ftp-archive.freebsd.org/pub/FreeBSD-Archive/old-releases/${ARCH}/${V}" ;;
 			url=*) URL=${METHOD##url=} ;;
 		esac
-		DISTS="base.txz src.txz games.txz"
+		DISTS="base.txz src.txz"
 		[ ${ARCH} = "amd64" ] && DISTS="${DISTS} lib32.txz"
 		for dist in ${DISTS}; do
 			msg "Fetching ${dist} for FreeBSD ${V} ${ARCH}"
