6859b6051b36e381f3f5e41cdc45c8ee4c02b76e
Josh Cepek <josh.cepek@usa.net>
2015-02-26 12:36:53 -0600

    Move .cpignore handling to just the TMPFS_ALL case
    This avoids manipulating .cpignore files in cases where cpdup(1) is not
    used. Only this case in clonefs() results in the requirement to have
    cpdup-supporting files, and it can be assumed such writes will succeed
    when the jail source is on tmpfs.
    
    In particular, this avoids modifying the source jail fs when it is not
    the true source, as is the case for zfs sources (which use @clean.)
    
    Signed-off-by: Josh Cepek <josh.cepek@usa.net>
    
    Conflicts:
    	src/share/poudriere/common.sh

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1555,11 +1555,7 @@ jail_start() {
 
 	[ ${SET_STATUS_ON_START-1} -eq 1 ] && export STATUS=1
 	msg_n "Creating the reference jail..."
-	echo "src" >> ${mnt}/usr/.cpignore
-	echo "debug" >> ${mnt}/usr/lib/.cpignore
 	clonefs ${mnt} ${tomnt} clean
-	echo ".p" >> ${tomnt}/.cpignore
-	rm -f ${mnt}/usr/.cpignore ${mnt}/usr/lib/.cpignore
 	echo " done"
 
 	if [ ${JAIL_OSVERSION} -gt ${HOST_OSVERSION} ]; then
