freebsd/poudriere

commit 1d5ec8d73c539bc77bf61c49b41eee163b68929a

Remove need for subshell

src/share/poudriere/jail.sh
@@ -379,7 +379,7 @@ build_and_install_world() {
 			HLINK_SHELLS="bin/sh bin/csh"
 			for file in ${HLINK_SHELLS}; do
 				rm -f ${JAILMNT}/${file}
-				sh -c "cd ${JAILMNT} && ln ./nxb-bin/${file} ${file}"
+				ln ./nxb-bin/${file} ${JAILMNT}/${file}
 			done
 		fi
 

commit bc19ccaf901977c8608cd59eab49fdc2d42eeabf

jail -cx: Fix incorrect hard linking. This was broken due my mistake in 1d5ec8d73c5.
Fixes #292

src/share/poudriere/jail.sh
@@ -396,7 +396,7 @@ build_and_install_world() {
 		for file in ${HLINK_FILES}; do
 			if [ -f "${JAILMNT}/nxb-bin/${file}" ]; then
 				rm -f ${JAILMNT}/${file}
-				ln ./nxb-bin/${file} ${JAILMNT}/${file}
+				ln ${JAILMNT}/nxb-bin/${file} ${JAILMNT}/${file}
 			fi
 		done
 	fi