40de3325ed7a0d5ce6265893daae597e051019d8
Bryan Drewery <bryan@shatow.net>
2015-04-17 23:12:13 -0500

    mangle_stderr: Read until EOF

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -3080,8 +3080,9 @@ mangle_stderr() {
 					"$@"
 				} 2>&3
 			} 3>&1 1>&2 | {
-				read -r line &&
-				    msg_warn "${extra}: ${line}" 2>&3
+				while read -r line; do
+					msg_warn "${extra}: ${line}" 2>&3
+				done
 			}
 		} 3>&2 2>&1
 	}
