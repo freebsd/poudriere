b5073cfddaf7b1e3e3ca021e7129127381d95b5e
Bryan Drewery <bryan@shatow.net>
2015-04-19 12:49:06 -0500

    Let poudriere(1) handle PATH and LIBEXECPREFIX.

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -4368,12 +4368,9 @@ fi
 include_poudriere_confs "$@"
 
 : ${DISTFILES_CACHE:=/nonexistent}
-: ${LIBEXECPREFIX:=${SCRIPTPREFIX}/../../libexec/poudriere}
-LIBEXECPREFIX=$(realpath ${LIBEXECPREFIX})
 AWKPREFIX=${SCRIPTPREFIX}/awk
 HTMLPREFIX=${SCRIPTPREFIX}/html
 HOOKDIR=${POUDRIERED}/hooks
-PATH="${LIBEXECPREFIX}:${PATH}:/sbin:/usr/sbin"
 
 # If the zfs module is not loaded it means we can't have zfs
 [ -z "${NO_ZFS}" ] && lsvfs zfs >/dev/null 2>&1 || NO_ZFS=yes
