2d420137e581771e7dfa0019bb460e1365340d14
Baptiste Daroussin <baptiste.daroussin@gandi.net>
2015-07-23 11:19:40 +0200

    Add ability to generate regular usb stick

--- src/share/poudriere/image.sh
+++ src/share/poudriere/image.sh
@@ -48,6 +48,8 @@ EOF
 
 delete_image() {
 	[ ! -f "${excludelist}" ] || rm -f ${excludelist}
+	[ ! -z "${md}" ] || /sbin/mdconfig -d -u ${md#md}
+
 	destroyfs ${WRKDIR} image
 }
 
@@ -56,7 +58,7 @@ cleanup_image() {
 	delete_image
 }
 
-while getopts "o:j:p:z:n:t:X:f:c:h:S:" FLAG; do
+while getopts "o:j:p:z:n:t:X:f:c:h:s:" FLAG; do
 	case "${FLAG}" in
 		o)
 			OUTPUTDIR=${OPTARG}
@@ -108,8 +110,9 @@ done
 
 saved_argv="$@"
 shift $((OPTIND-1))
-
 . ${SCRIPTPREFIX}/common.sh
+
+
 : ${MEDIATYPE:=iso+mfs}
 
 [ -n "${JAILNAME}" ] || usage
@@ -128,6 +131,9 @@ mkdir -p ${OUTPUTDIR}
 
 jail_exists ${JAILNAME} || err 1 "The jail ${JAILNAME} does not exists"
 case "${MEDIATYPE}" in
+usb)
+	[ -n "${IMAGESIZE}" ] || err 1 "Please specify the imagesize"
+	;;
 iso*|usb*|raw*)
 	_jget mnt ${JAILNAME} mnt
 	test -f ${mnt}/boot/kernel/kernel || err 1 "The ${MEDIATYPE} media type requires a jail with a kernel"
@@ -135,6 +141,7 @@ iso*|usb*|raw*)
 esac
 
 msg "Preparing the image '${IMAGENAME}'"
+md=""
 CLEANUP_HOOK=cleanup_image
 WRKDIR=$(mktemp -d ${POUDRIERE_DATA}/images/${IMAGENAME}-XXXX)
 _jget mnt ${JAILNAME} mnt
@@ -145,6 +152,15 @@ mkdir -p ${WRKDIR}/out
 cat >> ${excludelist} << EOF
 usr/src
 EOF
+case "${MEDIATYPE}" in
+usb)
+	truncate -s ${IMAGESIZE} ${WRKDIR}/raw.img
+	md=$(/sbin/mdconfig ${WRKDIR}/raw.img)
+	newfs -j -L ${IMAGENAME} /dev/${md}
+	mount /dev/${md} ${WRKDIR}/world
+	;;
+esac
+
 # Use of tar given cpdup has a pretty useless -X option for this case
 tar -C ${mnt} -X ${excludelist} -cf - . | tar -xf - -C ${WRKDIR}/world
 mkdir -p ${WRKDIR}/world/etc/rc.conf.d
@@ -169,7 +185,12 @@ case ${MEDIATYPE} in
 	mfs_load="YES"
 	mfs_type="mfs_root"
 	mfs_name="/mfsroot"
-	vfs.root.mountfrom="ufs:/dev/ufs/${IMAGENAME}
+	vfs.root.mountfrom="ufs:/dev/ufs/${IMAGENAME}"
+	EOF
+	;;
+usb)
+	cat >> ${WRKDIR}/world/etc/fstab <<-EOF
+	/dev/ufs/${IMAGENAME} / ufs rw 1 1
 	EOF
 	;;
 esac
@@ -192,7 +213,18 @@ usb+mfs)
 		-p freebsd-swap::1M \
 		-o ${OUTPUTDIR}/${FINALIMAGE}
 	;;
-
+usb)
+	FINALIMAGE=${IMAGENAME}.img
+	mkimg -s gpt -b ${WRKDIR}/world/boot/pmbr \
+		-p efi:=${WRKDIR}/world/boot/boot1.efifat \
+		-p freebsd-boot:=${WRKDIR}/world/boot/gptboot \
+		-p freebsd-ufs:=${WRKDIR}/raw.img \
+		-p freebsd-swap::1M \
+		-o ${OUTPUTDIR}/${FINALIMAGE}
+	umount ${WRKDIR}/world
+	/sbin/mdconfig -d -u ${md#md}
+	;;
 esac
 
+CLEANUP_HOOK=delete_image
 msg "Image available at: ${OUTPUTDIR}${FINALIMAGE}"
