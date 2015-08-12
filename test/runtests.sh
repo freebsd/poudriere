4d819638b9946910075b7fcf1280cdbfde7a52b4
Bryan Drewery <bryan@shatow.net>
2015-08-12 10:16:15 -0700

    Add basic test support

---
+++ test/runtests.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+export PATH=..:${PATH}
+
+TESTS="$@"
+
+FAILED_TESTS=
+for test in ${TESTS}; do
+	status=0
+	echo -n "Running ${test} ... "
+	timeout 20 sh ${test} > ${test}.stdout.log 2> ${test}.stderr.log || status=$?
+	if [ ${status} -ne 0 ]; then
+		if [ ${status} -eq 124 ]; then
+			status="124 (timeout)"
+		fi
+		echo "failed: ${status}"
+		FAILED_TESTS="${FAILED_TESTS}${FAILED_TESTS:+ }${test}"
+	else
+		echo "pass"
+	fi
+done
+
+if [ -n "${FAILED_TESTS}" ]; then
+	echo "Failed tests:"
+	for test in ${FAILED_TESTS}; do
+		cat ${test}.stderr.log | sed -e "s,^,${test}: ,"
+	done
+	exit 1
+fi
+exit 0
