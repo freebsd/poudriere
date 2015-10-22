3b034b96a5ef624606579a6033085f751e47388a
Bryan Drewery <bryan@shatow.net>
2015-10-22 10:42:47 -0700

    testport: Allow -o to be optional.
    Issue #338

--- src/share/poudriere/testport.sh
+++ src/share/poudriere/testport.sh
@@ -32,7 +32,7 @@ poudriere testport [parameters] [options]
 
 Parameters:
     -j jailname -- Run inside the given jail
-    -o origin   -- Specify an origin in the portstree
+    [-o] origin   -- Specify an origin in the portstree
 
 Options:
     -c          -- Run make config for the given port
@@ -135,7 +135,12 @@ done
 saved_argv="$@"
 shift $((OPTIND-1))
 
-[ -z ${ORIGIN} ] && usage
+if [ -z ${ORIGIN} ]; then
+	if [ $# -ne 1 ]; then
+		usage
+	fi
+	ORIGIN="${1}"
+fi
 
 [ -z "${JAILNAME}" ] && err 1 "Don't know on which jail to run please specify -j"
 _pget portsdir ${PTNAME} mnt
