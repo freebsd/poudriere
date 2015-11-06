7f16131067ed1c05aec273149cc7939c4a112972
Bryan Drewery <bryan@shatow.net>
2015-11-06 11:58:44 -0800

    Go crazy, use -j for installworld targets

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -267,11 +267,11 @@ installworld() {
 
 	msg "Starting make installworld"
 	${MAKE_CMD} -C "${SRC_BASE}" installworld DESTDIR=${destdir} \
-	    DB_FROM_SRC=1 || err 1 "Failed to 'make installworld'"
+	    ${MAKE_JOBS} DB_FROM_SRC=1 || err 1 "Failed to 'make installworld'"
 	${MAKE_CMD} -C "${SRC_BASE}" DESTDIR=${destdir} DB_FROM_SRC=1 \
-	    distrib-dirs || err 1 "Failed to 'make distrib-dirs'"
+	    ${MAKE_JOBS} distrib-dirs || err 1 "Failed to 'make distrib-dirs'"
 	${MAKE_CMD} -C "${SRC_BASE}" DESTDIR=${destdir} distribution ||
-	    err 1 "Failed to 'make distribution'"
+	    ${MAKE_JOBS} err 1 "Failed to 'make distribution'"
 
 	return 0
 }
