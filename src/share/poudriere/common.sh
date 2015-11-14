0c93fb8df9185577474354bcc0d15cd6e7fa86e2
Bryan Drewery <bryan@shatow.net>
2015-11-14 13:30:43 -0800

    Don't transcend mount directories when running rm -rf

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1854,7 +1854,7 @@ jail_stop() {
 	fi
 	msg "Umounting file systems"
 	destroyfs ${MASTERMNT} jail || :
-	rm -rf ${MASTERMNT}/../
+	rm -rfx ${MASTERMNT}/../
 	export STATUS=0
 
 	# Don't override if there is a failure to grab the last status.
