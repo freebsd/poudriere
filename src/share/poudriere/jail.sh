86acc338f4616aa685d191f92ed1586d364d2a73
Bryan Drewery <bryan@shatow.net>
2016-01-29 13:37:56 -0800

    Remove _MAKE_JOBS not present in release-3.1 and noted to break builds by sbruno

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -380,7 +380,6 @@ build_and_install_world() {
 		STRINGS=/nxb-bin/usr/bin/strings
 		AWK=/nxb-bin/usr/bin/awk
 		FLEX=/nxb-bin/usr/bin/flex
-		_MAKE_JOBS=-j1
 		EOF
 
 		# hardlink these files to capture scripts and tools
