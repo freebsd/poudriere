99b3286b9da0f7f0d045db8e58e07246c8dc52e5
Bryan Drewery <bryan@shatow.net>
2016-05-19 10:42:05 -0700

    Hide warnings from initial rm -rf before chflags is used.
    Issue:	#390

--- src/share/poudriere/include/fs.sh
+++ src/share/poudriere/include/fs.sh
@@ -191,7 +191,7 @@ destroyfs() {
 		zfs destroy -rf ${fs}
 		rmdir ${mnt}
 	else
-		rm -rfx ${mnt} || :
+		rm -rfx ${mnt} 2>/dev/null || :
 		if [ -d "${mnt}" ]; then
 			chflags -R 0 ${mnt}
 			rm -rfx ${mnt}
