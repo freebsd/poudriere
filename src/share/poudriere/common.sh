01116e9f3c01daaf5d00de91c3c5e29714cf58f4
Bryan Drewery <bryan@shatow.net>
2016-05-31 09:25:18 -0700

    Fix incorrect handling of mounting null-paths.
    Issue:	#392

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1169,9 +1169,10 @@ do_jail_mounts() {
 		fi
 
 	fi
+	( cd "${mnt}" && mkdir -p ${nullpaths} )
 	for nullpath in ${nullpaths}; do
-		[ -d "${nullpath}" -a "${from}" != "${mnt}" ] && \
-		    ${NULLMOUNT} -o ro "${from}${nullpath}" "${mnt}/${nullpath}"
+		[ -d "${from}${nullpath}" -a "${from}" != "${mnt}" ] && \
+		    ${NULLMOUNT} -o ro "${from}${nullpath}" "${mnt}${nullpath}"
 	done
 
 	# Mount /usr/src into target, no need for anything to write to it
