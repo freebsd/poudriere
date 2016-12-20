ca20b3931c49e86a241c5b28045ce4287c73ff42
Brad Davis <brd@FreeBSD.org>
2016-12-20 10:53:04 -0700

    Add support for building a jail from a given source tree

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -41,6 +41,7 @@ Parameters:
     -r newname    -- Rename a jail
 
 Options:
+    -b            -- Build the OS (for use with -m src)
     -q            -- Quiet (Do not print the header)
     -n            -- Print only jail name (for use with -l)
     -J n          -- Run buildworld in parallel with n jobs.
@@ -433,7 +434,11 @@ install_from_src() {
 	echo " done"
 
 	setup_build_env
-	installworld
+	if [ ${BUILD} -eq 0 ]; then
+		installworld
+	else
+		build_and_install_world
+	fi
 }
 
 install_from_vcs() {
@@ -867,9 +872,13 @@ UPDATE=0
 PTNAME=default
 SETNAME=""
 XDEV=0
+BUILD=0
 
-while getopts "iJ:j:v:a:z:m:nf:M:sdkK:lqcip:r:uU:t:z:P:S:x" FLAG; do
+while getopts "biJ:j:v:a:z:m:nf:M:sdkK:lqcip:r:uU:t:z:P:S:x" FLAG; do
 	case "${FLAG}" in
+		b)
+			BUILD=1
+			;;
 		i)
 			INFO=1
 			;;
