bca8b5b91f2dc39ce976ec7bd0cd818085127272
Bryan Drewery <bryan@shatow.net>
2017-02-15 16:49:35 -0800

    Stop null-mounting /nxb-bin for now.
    It is null-mounted after a copy is done (#444), and creates
    contention in nullfs locking when trying to run the toolchain.

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1208,7 +1208,7 @@ do_jail_mounts() {
 	fi
 
 	# Mount some paths read-only from the ref-jail if possible.
-	nullpaths="/nxb-bin /rescue"
+	nullpaths="/rescue"
 	if [ "${MUTABLE_BASE}" = "no" ]; then
 		# Need to keep /usr/src and /usr/ports on their own.
 		nullpaths="${nullpaths} /usr/bin /usr/include /usr/lib \
