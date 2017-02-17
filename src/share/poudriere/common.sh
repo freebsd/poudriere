b2c05db647208b2f93b1027c06c961538b888993
Bryan Drewery <bryan@shatow.net>
2017-02-17 15:44:31 -0800

    Auto fix PORTBUILD_UID in case it does not match the jail

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -314,7 +314,7 @@ injail_tty() {
 }
 
 jstart() {
-	local name network
+	local name network portbuild_uid
 
 	network="${localipargs}"
 
@@ -337,13 +337,17 @@ jstart() {
 	    jexecd -j ${name}-n -d ${MASTERMNT}/../ ${MAX_MEMORY_BYTES+-m ${MAX_MEMORY_BYTES}}
 	injail id >/dev/null 2>&1 || \
 	    err 1 "Unable to execute id(1) in jail. Emulation or ABI wrong."
-	if ! injail id ${PORTBUILD_USER} >/dev/null 2>&1 ; then
+
+	portbuild_uid=$(injail id -u ${PORTBUILD_USER} 2>&1)
+	if [ -z "${portbuild_uid}" -a $? -ne 0 ]; then
 		msg_n "Creating user/group ${PORTBUILD_USER}"
 		injail pw groupadd ${PORTBUILD_USER} -g ${PORTBUILD_UID} || \
 		err 1 "Unable to create group ${PORTBUILD_USER}"
 		injail pw useradd ${PORTBUILD_USER} -u ${PORTBUILD_UID} -d /nonexistent -c "Package builder" || \
 		err 1 "Unable to create user ${PORTBUILD_USER}"
 		echo " done"
+	else
+		PORTBUILD_UID=${portbuild_uid}
 	fi
 }
 
