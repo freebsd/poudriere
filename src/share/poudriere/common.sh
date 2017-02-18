3906e6c9354c0fdb0abedc502abf5edead420acd
Bryan Drewery <bryan@shatow.net>
2017-02-17 17:04:17 -0800

    testport: Fix limits not being applied

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -3136,6 +3136,14 @@ print_phase_footer() {
 	echo "==========================================================================="
 }
 
+apply_limits() {
+	if [ -n "${MAX_MEMORY_BYTES}" -o -n "${MAX_FILES}" ]; then
+		ulimit \
+		    ${MAX_MEMORY_BYTES:+-v ${MAX_MEMORY_BYTES}} \
+		    ${MAX_FILES:+-n ${MAX_FILES}}
+	fi
+}
+
 build_pkg() {
 	# If this first check fails, the pool will not be cleaned up,
 	# since PKGNAME is not yet set.
@@ -3157,12 +3165,7 @@ build_pkg() {
 	_log_path log
 	clean_rdepends=
 	trap '' SIGTSTP
-	if [ -n "${MAX_MEMORY_BYTES}" -o -n "${MAX_FILES}" ]; then
-		ulimit \
-		    ${MAX_MEMORY_BYTES:+-v ${MAX_MEMORY_BYTES}} \
-		    ${MAX_FILES:+-n ${MAX_FILES}}
-	fi
-
+	apply_limits
 	export PKGNAME="${pkgname}" # set ASAP so jail_cleanup() can use it
 	cache_get_origin port "${pkgname}"
 	portdir="/usr/ports/${port}"
