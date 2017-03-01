a6348b26b21ffea33766537c3cdf8924174e997c
Bryan Drewery <bryan@shatow.net>
2017-02-28 19:19:28 -0800

    Allow test timeout to be configurable and a bit longer

--- test/runtests.sh
+++ test/runtests.sh
@@ -3,6 +3,7 @@
 export PATH=..:${PATH}
 
 SH="$1"
+: ${TIMEOUT:=30}
 shift
 TESTS="$@"
 
@@ -10,7 +11,7 @@ FAILED_TESTS=
 for test in ${TESTS}; do
 	status=0
 	echo -n "Running ${test} ... "
-	/usr/bin/timeout 20 ${SH} ${test} > ${test}.stdout.log 2> ${test}.stderr.log || status=$?
+	/usr/bin/timeout ${TIMEOUT} ${SH} ${test} > ${test}.stdout.log 2> ${test}.stderr.log || status=$?
 	if [ ${status} -ne 0 ]; then
 		if [ ${status} -eq 124 ]; then
 			status="124 (timeout)"
