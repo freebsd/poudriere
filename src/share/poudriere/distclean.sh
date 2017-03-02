b92e8b2993423b44b02e611af9cb04a82d22ec55
Bryan Drewery <bryan@shatow.net>
2017-03-02 15:43:22 -0800

    pkgclean/distclean: Properly support PREPARE_PARALLEL_JOBS from poudriere.conf

--- src/share/poudriere/distclean.sh
+++ src/share/poudriere/distclean.sh
@@ -51,7 +51,7 @@ ALL=1
 while getopts "J:np:vy" FLAG; do
 	case "${FLAG}" in
 		J)
-			PARALLEL_JOBS=${OPTARG}
+			PREPARE_PARALLEL_JOBS=${OPTARG}
 			;;
 		n)
 			DRY_RUN=1
@@ -76,6 +76,9 @@ done
 : ${PTNAMES:=default}
 
 shift $((OPTIND-1))
+
+: ${PREPARE_PARALLEL_JOBS:=${PARALLEL_JOBS}}
+PARALLEL_JOBS=${PREPARE_PARALLEL_JOBS}
 
 distfiles_cleanup() {
 	rm -f ${DISTFILES_LIST} ${DISTFILES_LIST}.expected \
