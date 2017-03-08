freebsd/poudriere

commit 4d0f539d2caf5d09f8ac1fc546dbf7ec4a0c103b

do_clone: Only use relative paths if requested.
This avoids wasting time trying to make gather_distfiles use a
relative path when the result is always '/' for the common
directory.  It also avoids wasting time creating an extra
subshell in gather_distfiles.

src/share/poudriere/include/fs.sh
@@ -45,16 +45,29 @@ createfs() {
 }
 
 do_clone() {
-	local src dst common
-	set -- $(relpath "${1}" "${2}")
-	common="${1}"
-	src="${2}"
-	dst="${3}"
+	[ $# -lt 2 ] && eargs do_clone [-r] src dst
+	[ $# -gt 3 ] && eargs do_clone [-r] src dst
+	local src dst common relative FLAG
 
-	(
+	relative=0
+	while getopts "r" FLAG; do
+		case "${FLAG}" in
+			r) relative=1 ;;
+		esac
+	done
+
+	if [ ${relative} -eq 1 ]; then
+		set -- $(relpath "${1}" "${2}")
+		common="${1}"
+		src="${2}"
+		dst="${3}"
+		(
 		cd "${common}"
 		cpdup -i0 -x "${src}" "${dst}"
-	)
+		)
+	else
+		cpdup -i0 -x "${1}" "${2}"
+	fi
 }
 
 rollbackfs() {
@@ -69,7 +82,7 @@ rollbackfs() {
 		return
 	fi
 
-	do_clone "${MASTERMNT}" "${mnt}"
+	do_clone -r "${MASTERMNT}" "${mnt}"
 }
 
 findmounts() {
@@ -181,7 +194,7 @@ clonefs() {
 			echo "debug" >> "${from}/usr/lib/.cpignore" || :
 			echo "freebsd-update" >> "${from}/var/db/.cpignore" || :
 		fi
-		do_clone "${from}" "${to}"
+		do_clone -r "${from}" "${to}"
 		if [ "${snap}" = "clean" ]; then
 			rm -f "${from}/usr/.cpignore" \
 			    "${from}/usr/lib/.cpignore" \

src/share/poudriere/common.sh
@@ -3359,7 +3359,7 @@ build_pkg() {
 			umount ${UMOUNT_NONBUSY} ${mnt}/${LOCALBASE:-/usr/local}
 		fi
 		mnt_tmpfs localbase ${mnt}/${LOCALBASE:-/usr/local}
-		do_clone "${MASTERMNT}/${LOCALBASE:-/usr/local}" \
+		do_clone -r "${MASTERMNT}/${LOCALBASE:-/usr/local}" \
 		    "${mnt}/${LOCALBASE:-/usr/local}"
 		:> "${mnt}/${LOCALBASE:-/usr/local}/.mounted"
 	fi

commit 284434f8546bc28e19945b120fd5aa0c16af2e1e

do_clone: Fix 'realpath: Illegal option -r' error caused by 4d0f539d2ca

src/share/poudriere/include/fs.sh
@@ -55,6 +55,7 @@ do_clone() {
 			r) relative=1 ;;
 		esac
 	done
+	shift $((OPTIND-1))
 
 	if [ ${relative} -eq 1 ]; then
 		set -- $(relpath "${1}" "${2}")