25a5202ebe6df15c64dd3d47035f79898fa59d07
Bryan Drewery <bryan@shatow.net>
2017-03-08 11:09:38 -0800

    Support VPATH builds

--- external/update_sh.sh
+++ external/update_sh.sh
@@ -43,9 +43,6 @@ cd "${ORIG_PWD}"
 # Fix backwards compat for st_mtim
 sed -i '' -e 's,[[:<:]]st_mtim[[:>:]],st_mtimespec,g' "${DESTDIR}/test.c"
 
-# Move builtins.def away so we can modify it during build with our own list.
-mv "${DESTDIR}/builtins.def" "${DESTDIR}/builtins.def.sh"
-
 git add -A "${DESTDIR}"
 echo "sh_SOURCES= external/sh_compat/compat.c \\"
 find "${DESTDIR}" -name '*.c'|sed -e 's,$, \\,'|sort
