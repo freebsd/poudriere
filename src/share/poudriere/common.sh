02d8639767a45aed0e662b614be9af80595becc6
Bryan Drewery <bryan@shatow.net>
2017-06-09 09:26:47 -0700

    all_origins should be named listed_origins

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -4571,7 +4571,7 @@ gather_port_vars() {
 	parallel_start
 	for origin in $(listed_ports show_moved); do
 		if [ -d "../${PORTSDIR}/${origin}" ]; then
-			echo "${origin}" >> "all_origins"
+			echo "${origin}" >> "listed_origins"
 			if was_a_bulk_run; then
 				echo "${origin} listed" >> \
 				    "${log}/.poudriere.ports.queued"
@@ -4810,8 +4810,8 @@ listed_ports() {
 	local tell_moved="${1}"
 	local portsdir origin file
 
-	if [ -f "${MASTERMNT}/.p/all_origins" ]; then
-		cat "${MASTERMNT}/.p/all_origins"
+	if [ -f "${MASTERMNT}/.p/listed_origins" ]; then
+		cat "${MASTERMNT}/.p/listed_origins"
 		return
 	fi
 	if [ ${ALL} -eq 1 ]; then
