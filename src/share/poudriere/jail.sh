freebsd/poudriere

commit 47ff5fcd9687cc5e4a9118da9ca16b56eeb24276

jail -u: Only run freebsd-install if needed
Fixes #385

src/share/poudriere/jail.sh
@@ -212,8 +212,16 @@ update_jail() {
 		    ${JAILMNT}/usr/sbin/freebsd-update.fixed
 		chmod +x ${JAILMNT}/usr/sbin/freebsd-update.fixed
 		if [ -z "${TORELEASE}" ]; then
-			injail env PAGER=/bin/cat \
-			    /usr/sbin/freebsd-update.fixed fetch install
+			# We're running inside the jail so basedir is /.
+			# If we start using -b this needs to match it.
+			basedir=/
+			bdhash="$(echo "${basedir}" | sha256 -q)"
+			if injail env PAGER=/bin/cat \
+			    /usr/sbin/freebsd-update.fixed fetch && \
+			    [ -L "${JAILMNT}/${bdhash}-install" ]; then
+				injail env PAGER=/bin/cat \
+				    /usr/sbin/freebsd-update.fixed install
+			fi
 		else
 			# Install new kernel
 			injail env PAGER=/bin/cat \

commit c3c1bc2be80f2e25741f3454d44421dbb5f6521e

jail -cu -m ftp: Fix freebsd-update not being properly ran.
This was a regression in 47ff5fcd968.

Based on patch from Vinicius Zavam <egypcio@googlemail.com>

Fixes #510

src/share/poudriere/jail.sh
@@ -220,10 +220,14 @@ update_jail() {
 			# We're running inside the jail so basedir is /.
 			# If we start using -b this needs to match it.
 			basedir=/
-			bdhash="$(echo "${basedir}" | sha256 -q)"
+			fu_workdir=/var/db/freebsd-update
+			fu_bdhash="$(echo "${basedir}" | sha256 -q)"
+			# New updates are identified by a symlink containing
+			# the basedir hash and -install as suffix.  If we
+			# really have new updates to install, then install them.
 			if injail env PAGER=/bin/cat \
 			    /usr/sbin/freebsd-update.fixed fetch && \
-			    [ -L "${JAILMNT}/${bdhash}-install" ]; then
+			    [ -L "${JAILMNT}${fu_workdir}/${fu_bdhash}-install" ]; then
 				injail env PAGER=/bin/cat \
 				    /usr/sbin/freebsd-update.fixed install
 			fi