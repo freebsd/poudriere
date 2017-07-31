freebsd/poudriere

commit 80ed727b9cba933c0f1b9dc1d230ca5cd3da6901

ports -c: Don't try removing a FS that was not created.
1. If the mountpoint already exists then consider it a fatal error.
2. Don't install the cleanup hook until after a filesystem is created,
   which prevents an exit in createfs from trying to destroy a
   filesystem that does not exist, which may end up trying to
   "rm -rf ${PTMNT}"
3. Don't try destroying the filesystem with -m none

Fixes #471

src/share/poudriere/ports.sh
@@ -72,6 +72,7 @@ NAMEONLY=0
 QUIET=0
 VERBOSE=0
 KEEP=0
+CREATED_FS=0
 while getopts "B:cFuU:dklp:qf:nM:m:v" FLAG; do
 	case "${FLAG}" in
 		B)
@@ -216,7 +217,9 @@ fi
 
 cleanup_new_ports() {
 	msg "Error while creating ports tree, cleaning up." >&2
-	TMPFS_ALL=0 destroyfs ${PTMNT} ports || :
+	if [ "${CREATED_FS}" -eq 1 ]; then
+		TMPFS_ALL=0 destroyfs ${PTMNT} ports || :
+	fi
 	rm -rf ${POUDRIERED}/ports/${PTNAME} || :
 }
 
@@ -233,18 +236,28 @@ if [ ${CREATE} -eq 1 ]; then
 	: ${PTMNT="${BASEFS:=/usr/local${ZROOTFS}}/ports/${PTNAME}"}
 	: ${PTFS="${ZPOOL}${ZROOTFS}/ports/${PTNAME}"}
 
-	# Wrap the ports creation in a special cleanup hook that will remove it
-	# if any error is encountered
-	CLEANUP_HOOK=cleanup_new_ports
-
 	[ "${PTNAME#*.*}" = "${PTNAME}" ] ||
 		err 1 "The ports name cannot contain a period (.). See jail(8)"
 
+	[ -d "${PTMNT}" ] && \
+	    err 1 "Directory ${PTMNT} already exists"
+
 	if [ ${METHOD} != "none" ]; then
+		# This will exit if it fails to zfs create...
 		createfs ${PTNAME} ${PTMNT} ${PTFS}
+		# Ports runs without -e, but even if it did let's not
+		# short-circuit all of -e support in createfs.  It
+		# should have exited on error with err(), but be sure.
+		if [ $? -eq 0 ]; then
+			CREATED_FS=1
+		fi
 	else
 		echo "Not creating fs for method=none"
 	fi
+
+	# Wrap the ports creation in a special cleanup hook that will remove it
+	# if any error is encountered
+	CLEANUP_HOOK=cleanup_new_ports
 
 	pset ${PTNAME} mnt ${PTMNT}
 	if [ $FAKE -eq 0 ]; then

commit 51cb5fdea27617e72c173cb8a74a42ed5d1d9b34

ports -cF: Allow importing existing tree with -F
This was a regression after 80ed727b9cba933c0f1b9dc1d230ca5cd3da6901.

src/share/poudriere/ports.sh
@@ -240,7 +240,7 @@ if [ ${CREATE} -eq 1 ]; then
 	[ "${PTNAME#*.*}" = "${PTNAME}" ] ||
 		err 1 "The ports name cannot contain a period (.). See jail(8)"
 
-	[ -d "${PTMNT}" ] && \
+	[ "${PTFS}" != "none" ] && [ -d "${PTMNT}" ] && \
 	    err 1 "Directory ${PTMNT} already exists"
 
 	# This will exit if it fails to zfs create...