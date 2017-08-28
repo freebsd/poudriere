freebsd/poudriere

commit d480361bc2be93183bf1cccb864e5e4be50bbc9c

Add list_remove() to remove items from a string

src/share/poudriere/include/hash.sh
@@ -139,3 +139,21 @@ hash_unset() {
 	_hash_var_name "${var}" "${key}"
 	unset "${_hash_var_name}"
 }
+
+list_remove() {
+	[ $# -eq 2 ] || eargs list_remove var item
+	local var="$1"
+	local item="$2"
+	local values
+
+	eval "values=\" \${${var}:-} \""
+	# There's probably a better way to do this...
+	values_L=${values%% ${item} *}
+	values_L=${values_L% }
+	values_L=${values_L# }
+	values_R=${values##* ${item} }
+	values_R=${values_R% }
+	values_R=${values_R# }
+	values="${values_L}${values_L:+ }${values_R}"
+	setvar "${var}" "${values}"
+}

src/share/poudriere/include/parallel.sh
@@ -159,15 +159,7 @@ _reap_children() {
 		if ! kill -0 ${pid} 2>/dev/null; then
 			# This will error out if the return status is non-zero
 			_wait ${pid} || ret=$?
-			# Remove pid from PARALLEL_PIDS and strip away all
-			# spaces
-			PARALLEL_PIDS_L=${PARALLEL_PIDS%% ${pid} *}
-			PARALLEL_PIDS_L=${PARALLEL_PIDS_L% }
-			PARALLEL_PIDS_L=${PARALLEL_PIDS_L# }
-			PARALLEL_PIDS_R=${PARALLEL_PIDS##* ${pid} }
-			PARALLEL_PIDS_R=${PARALLEL_PIDS_R% }
-			PARALLEL_PIDS_R=${PARALLEL_PIDS_R# }
-			PARALLEL_PIDS=" ${PARALLEL_PIDS_L} ${PARALLEL_PIDS_R} "
+			list_remove PARALLEL_PIDS "${pid}"
 		fi
 	done
 

commit 6218a55f18a6dfba57061ea76114eeca954ddfd5

Fix kern.randompid / parallel pid reuse resulting in 100% CPU.
This fixes a 100% CPU problem with kern.randompid set.  Consider if process
A spawns, then process B, then A finishes but before it is removed from
PARALLEL_PIDS with _reap_children, it is spawned again as A and duplicated
into PARALLEL_PIDS.  When _reap_children finally runs it would remove
A, then B, then again try to remove A.  The 2nd removal of A (non-existent now)
would incorrectly duplicate the entire remaining list and basically lead
to a very long `kill -0` loop.

This seems to be a regression from d480361bc2be93183bf1cccb864e5e4be50bbc9c.

src/share/poudriere/include/hash.sh
@@ -155,20 +155,19 @@ hash_unset() {
 	unset "${_hash_var_name}"
 }
 
+list_add() {
+	[ $# -eq 2 ] || eargs list_add var item
+	local var="$1"
+	local item="$2"
+	eval "case \"\${${var}}\" in *\ ${item}\ *) return ;; esac;"
+	eval "${var}=\"\${${var}} ${item} \""
+}
+
 list_remove() {
 	[ $# -eq 2 ] || eargs list_remove var item
 	local var="$1"
 	local item="$2"
-	local values
 
-	eval "values=\" \${${var}:-} \""
-	# There's probably a better way to do this...
-	values_L=${values%% ${item} *}
-	values_L=${values_L% }
-	values_L=${values_L# }
-	values_R=${values##* ${item} }
-	values_R=${values_R% }
-	values_R=${values_R# }
-	values="${values_L}${values_L:+ }${values_R}"
-	setvar "${var}" "${values}"
+	eval "case \"\${${var}}\" in *\ ${item}\ *) ;; *) return 0 ;; esac;"
+	eval "${var}=\"\${${var}%* ${item} *}\${${var}#* ${item} *}\""
 }

src/share/poudriere/include/parallel.sh
@@ -209,7 +209,7 @@ parallel_run() {
 
 	[ ${NBPARALLEL} -lt ${PARALLEL_JOBS} ] && NBPARALLEL=$((NBPARALLEL + 1))
 	PARALLEL_CHILD=1 spawn parallel_exec "$@"
-	PARALLEL_PIDS="${PARALLEL_PIDS} $! "
+	list_add PARALLEL_PIDS "$!"
 
 	return ${ret}
 }