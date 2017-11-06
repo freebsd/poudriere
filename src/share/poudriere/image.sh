6353744bca24525223bd01f165f8492c7289ce04
Steve Wills <steve@mouf.net>
2017-11-06 13:19:43 -0500

    While here, avoid logging to syslog
    Without this, you get log messages which make it look like the packages were
    installed locally on the host.

--- src/share/poudriere/image.sh
+++ src/share/poudriere/image.sh
@@ -279,8 +279,8 @@ if [ -n "${PACKAGELIST}" ]; then
 	FreeBSD: { enabled: false }
 	local: { url: file:///${WRKDIR}/world/tmp/packages }
 	EOF
-		env ASSUME_ALWAYS_YES=yes REPOS_DIR=/${WRKDIR}/world/tmp/ ABI_FILE=/${WRKDIR}/world/bin/sh pkg -r ${WRKDIR}/world/ install pkg
-		cat ${PACKAGELIST} | xargs env ASSUME_ALWAYS_YES=yes REPOS_DIR=/${WRKDIR}/world/tmp/ ABI_FILE=/${WRKDIR}/world/bin/sh pkg -r ${WRKDIR}/world/ install
+		env ASSUME_ALWAYS_YES=yes SYSLOG=no REPOS_DIR=/${WRKDIR}/world/tmp/ ABI_FILE=/${WRKDIR}/world/bin/sh pkg -r ${WRKDIR}/world/ install pkg
+		cat ${PACKAGELIST} | xargs env ASSUME_ALWAYS_YES=yes SYSLOG=no REPOS_DIR=/${WRKDIR}/world/tmp/ ABI_FILE=/${WRKDIR}/world/bin/sh pkg -r ${WRKDIR}/world/ install
 	fi
 	rm -rf ${WRKDIR}/world/var/cache/pkg
 	umount ${WRKDIR}/world/tmp/packages
