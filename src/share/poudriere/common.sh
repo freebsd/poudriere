freebsd/poudriere

commit e4036cb0c3239cd130f9d320b8a8b627a168f026

Avoid redefining injail for tests.
Fixes #544

src/share/poudriere/common.sh
@@ -500,10 +500,11 @@ do_confirm_delete() {
 	return ${ret}
 }
 
-# It may be defined as a NOP for tests
-if ! type injail >/dev/null 2>&1; then
 injail() {
-	if [ "${USE_JEXECD}" = "no" ]; then
+	if [ ${STATUS:-0} -eq 0 ]; then
+		# For test/
+		"$@"
+	elif [ "${USE_JEXECD}" = "no" ]; then
 		injail_tty "$@"
 	else
 		local name
@@ -514,7 +515,6 @@ injail() {
 			-u ${JUSER:-root} "$@"
 	fi
 }
-fi
 
 injail_tty() {
 	local name
@@ -5231,13 +5231,6 @@ port_var_fetch() {
 	# Use invalid shell var character '!' to ensure we
 	# don't setvar it later.
 	local assign_var="!"
-	local injail
-
-	if [ ${STATUS:-0} -eq 1 ]; then
-		injail=injail
-	else
-		injail=
-	fi
 
 	if [ -n "${origin}" ]; then
 		_make_origin="-C${sep}${PORTSDIR}/${origin}"
@@ -5297,7 +5290,7 @@ port_var_fetch() {
 			shiftcnt=$((shiftcnt + 1))
 		fi
 	done <<-EOF
-	$(IFS="${sep}"; ${injail} /usr/bin/make ${_make_origin} ${_makeflags} || echo "${_errexit} $?")
+	$(IFS="${sep}"; injail /usr/bin/make ${_make_origin} ${_makeflags} || echo "${_errexit} $?")
 	EOF
 
 	# If the entire output was blank, then $() ate all of the excess

test/common.sh
@@ -104,7 +104,3 @@ assert_ret() {
 aecho() {
 	echo "$@" >&2
 }
-
-injail() {
-	"$@"
-}

commit dac4b3696c032f3aab84c8681182a97f718b1fa1

Fix updating of jail.
Broken by e4036cb0c32 for issue #544.

Fixes #555

src/share/poudriere/common.sh
@@ -501,7 +501,7 @@ do_confirm_delete() {
 }
 
 injail() {
-	if [ ${STATUS:-0} -eq 0 ]; then
+	if [ ${INJAIL_HOST:-0} -eq 1 ]; then
 		# For test/
 		"$@"
 	elif [ "${USE_JEXECD}" = "no" ]; then

test/common.bulk.sh
@@ -205,6 +205,8 @@ assert_queued() {
 export __MAKE_CONF=/dev/null
 export SRCCONF=/dev/null
 export SRC_ENV_CONF=/dev/null
+# Avoid injail() for port_var_fetch
+INJAIL_HOST=1
 
 . common.sh
 

test/port_var_fetch.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 . common.sh
+INJAIL_HOST=1
 . ${SCRIPTPREFIX}/common.sh
 PORTSDIR=${THISDIR}/ports
 export PORTSDIR