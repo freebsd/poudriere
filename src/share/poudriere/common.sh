6e208379152ba1412e951bbfab9fc832d6a852e3
Bryan Drewery <bryan@shatow.net>
2018-01-24 13:21:19 -0800

    Export PACKAGE_BUILDING to environment as well as some ports check there (like postfix pkg-install)

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -2475,6 +2475,7 @@ jail_start() {
 	    echo "FORCE_PACKAGE=yes" >> "${tomnt}/etc/make.conf"
 	if [ -z "${NO_PACKAGE_BUILDING}" ]; then
 		echo "PACKAGE_BUILDING=yes" >> "${tomnt}/etc/make.conf"
+		export PACKAGE_BUILDING=yes
 		echo "PACKAGE_BUILDING_FLAVORS=yes" >> "${tomnt}/etc/make.conf"
 	fi
 
