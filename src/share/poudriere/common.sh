1f5551278445e847c81ba0576b1c74a7863e7ff5
Bryan Drewery <bryan@shatow.net>
2018-02-01 16:59:33 -0800

    Store shared locks in /var/run/poudriere

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -2103,7 +2103,7 @@ need_cross_build() {
 }
 
 _jlock() {
-	setvar "$1" "/var/run/poudriere/poudriere.${MASTERNAME}.lock"
+	setvar "$1" "${SHARED_LOCK_DIR}/poudriere.${MASTERNAME}.lock"
 }
 
 lock_jail() {
@@ -2111,7 +2111,7 @@ lock_jail() {
 
 	_jlock jlock
 	jlockf="${jlock}/pid"
-	mkdir -p /var/run/poudriere >/dev/null 2>&1 || :
+	mkdir -p "${SHARED_LOCK_DIR}" >/dev/null 2>&1 || :
 	# Ensure no other processes are trying to start this jail
 	if ! mkdir "${jlock}" 2>/dev/null; then
 		if [ -d "${jlock}" ]; then
@@ -5473,7 +5473,8 @@ pkgqueue_dir() {
 slock_acquire() {
 	[ $# -ge 1 ] || eargs slock_acquire lockname [waittime]
 
-	POUDRIERE_TMPDIR=/tmp MASTERNAME=poudriere-shared \
+	mkdir -p "${SHARED_LOCK_DIR}" >/dev/null 2>&1 || :
+	POUDRIERE_TMPDIR="${SHARED_LOCK_DIR}" MASTERNAME=poudriere-shared \
 	    lock_acquire "$@"
 }
 
@@ -5499,7 +5500,7 @@ lock_acquire() {
 
 slock_release() {
 	[ $# -ne 1 ] && eargs slock_release lockname
-	POUDRIERE_TMPDIR=/tmp MASTERNAME=poudriere-shared \
+	POUDRIERE_TMPDIR="${SHARED_LOCK_DIR}" MASTERNAME=poudriere-shared \
 	    lock_release "$@"
 }
 
@@ -7717,6 +7718,7 @@ fi
 : ${WATCHDIR:=${POUDRIERE_DATA}/queue}
 : ${PIDFILE:=${POUDRIERE_DATA}/daemon.pid}
 : ${QUEUE_SOCKET:=/var/run/poudriered.sock}
+: ${SHARED_LOCK_DIR:=/var/run/poudriere}
 : ${PORTBUILD_UID:=65532}
 : ${PORTBUILD_GID:=${PORTBUILD_UID}}
 : ${PORTBUILD_USER:=nobody}
