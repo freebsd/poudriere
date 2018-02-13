6267bd0dd4222e350965d187030c5259ce29e517
Bryan Drewery <bryan@shatow.net>
2018-02-12 19:15:48 -0800

    Add read_file tests

---
+++ test/read_file.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+. $(realpath $(dirname $0))/common.sh
+. ${SCRIPTPREFIX}/include/util.sh
+
+TMP=$(mktemp -u)
+data=blah
+read_file data "${TMP}" 2>/dev/null
+assert_not 0 $? "read_file on missing file should not return 0"
+assert '' "${data}" "read_file on missing file should be blank"
+assert 0 "${_read_file_lines_read}" "_read_file_lines_read should be 0 on missing file"
+
+echo "first" >> "${TMP}"
+read_file data "${TMP}"
+assert 'first' "${data}" "read_file on 1 line file should match"
+assert 1 "${_read_file_lines_read}" "_read_file_lines_read should be 1"
+
+data=blah
+echo "second" >> "${TMP}"
+read_file data "${TMP}"
+assert $'first\nsecond' "${data}" "read_file on 2 line file should match"
+assert 2 "${_read_file_lines_read}" "_read_file_lines_read should be 1"
+
+data=blah
+echo "third" >> "${TMP}"
+read_file data "${TMP}"
+assert $'first\nsecond\nthird' "${data}" "read_file on 3 line file should match"
+assert 3 "${_read_file_lines_read}" "_read_file_lines_read should be 1"
+
+rm -f "${TMP}"
+exit 0
