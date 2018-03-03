07499a3ee8936fe3088ef67cd5b9f7985656d9ee
Bryan Drewery <bryan@shatow.net>
2018-03-03 11:42:37 -0800

    common_mtree: Open mtreefile once

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -1451,7 +1451,8 @@ common_mtree() {
 	local mtreefile="$1"
 	local exclude
 
-	cat > "${mtreefile}" <<EOF
+	{
+	cat <<EOF
 ./.npkg
 ./.p
 ./.poudriere-snap-*
@@ -1469,8 +1470,9 @@ common_mtree() {
 ./wrkdirs
 EOF
 	for exclude in ${LOCAL_MTREE_EXCLUDES}; do
-		echo ".${exclude#.}" >> "${mtreefile}"
+		echo ".${exclude#.}"
 	done
+	} > "${mtreefile}"
 }
 
 markfs() {
