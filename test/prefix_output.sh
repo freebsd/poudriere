e283e68741c0c72f0868f03f6b7251b34be05270
Bryan Drewery <bryan@shatow.net>
2019-02-27 17:08:37 -0800

    prefix_output tests: Add timestamp/non-timestamp testing.

--- test/prefix_output.sh
+++ test/prefix_output.sh
@@ -22,217 +22,229 @@ test_output() {
 
 OUTPUT=$(mktemp -ut poudriere)
 ret=0
-# Basic output test with prefix_stderr_quick
-(
-	prefix_stderr_quick "STDERR" test_output 0 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr_quick test_output 0 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	test stdout 1
-	test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stderr_quick stdout output should match"
+# Test twice. With and Without timestamp util.
+USE_TIMESTAMP=0
+until [ "${USE_TIMESTAMP}" -eq 2 ]; do
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	STDERR: test stderr 1
-	STDERR: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr_quick stderr output should match"
-) || ret=1
+	if [ "${USE_TIMESTAMP}" -eq 1 ]; then
+		TS="[00:00:00] "
+	fi
 
-# Basic output test with prefix_stdout
-(
-	prefix_stdout "STDOUT" test_output 0 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stdout test_output 0 wrong exit status"
+	# Basic output test with prefix_stderr_quick
+	(
+		prefix_stderr_quick "STDERR" test_output 0 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr_quick test_output 0 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDOUT: test stdout 1
-	[00:00:00] STDOUT: test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stdout stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		test stdout 1
+		test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr_quick stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	test stderr 1
-	test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stdout stderr output should match"
-) || ret=2
+		cat > "${OUTPUT}.expected" <<-EOF
+		STDERR: test stderr 1
+		STDERR: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr_quick stderr output should match"
+	) || ret=1
 
-# Basic output test with prefix_stderr
-(
-	prefix_stderr "STDERR" test_output 0 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr test_output 0 wrong exit status"
+	# Basic output test with prefix_stdout
+	(
+		prefix_stdout "STDOUT" test_output 0 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stdout test_output 0 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	test stdout 1
-	test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stderr stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDOUT: test stdout 1
+		${TS}STDOUT: test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stdout stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDERR: test stderr 1
-	[00:00:00] STDERR: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr stderr output should match"
-) || ret=3
+		cat > "${OUTPUT}.expected" <<-EOF
+		test stderr 1
+		test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stdout stderr output should match"
+	) || ret=2
 
-# Basic output test with prefix_output
-(
-	prefix_output "OUTPUT" test_output 0 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 0 $? "prefix_output test_output 0 wrong exit status"
+	# Basic output test with prefix_stderr
+	(
+		prefix_stderr "STDERR" test_output 0 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr test_output 0 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] OUTPUT: test stdout 1
-	[00:00:00] OUTPUT: test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_output stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		test stdout 1
+		test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] OUTPUT: test stderr 1
-	[00:00:00] OUTPUT: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_output stderr output should match"
-) || ret=4
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDERR: test stderr 1
+		${TS}STDERR: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr stderr output should match"
+	) || ret=3
 
-# Basic output test with chaining prefix_stderr and prefix_stdout
-(
-	prefix_stderr "STDERR" prefix_stdout "STDOUT" test_output 0 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr+prefix_stdout test_output 0 wrong exit status"
+	# Basic output test with prefix_output
+	(
+		prefix_output "OUTPUT" test_output 0 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_output test_output 0 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDOUT: test stdout 1
-	[00:00:00] STDOUT: test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stderr+prefix_stdout stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}OUTPUT: test stdout 1
+		${TS}OUTPUT: test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_output stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDERR: test stderr 1
-	[00:00:00] STDERR: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr+prefix_stdout stderr output should match"
-) || ret=5
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}OUTPUT: test stderr 1
+		${TS}OUTPUT: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_output stderr output should match"
+	) || ret=4
 
-# Now test exit statuses (pipefail and such)
+	# Basic output test with chaining prefix_stderr and prefix_stdout
+	(
+		prefix_stderr "STDERR" prefix_stdout "STDOUT" test_output 0 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr+prefix_stdout test_output 0 wrong exit status"
 
-# Pipefail test with prefix_stderr_quick
-(
-	prefix_stderr_quick "STDERR" test_output 5 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 5 $? "prefix_stderr_quick test_output 5 wrong exit status"
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDOUT: test stdout 1
+		${TS}STDOUT: test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr+prefix_stdout stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	test stdout 1
-	test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stderr_quick/5 stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDERR: test stderr 1
+		${TS}STDERR: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr+prefix_stdout stderr output should match"
+	) || ret=5
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	STDERR: test stderr 1
-	STDERR: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr_quick/5 stderr output should match"
-) || ret=6
+	# Now test exit statuses (pipefail and such)
 
-# pipefail test with prefix_stdout
-(
-	prefix_stdout "STDOUT" test_output 5 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 5 $? "prefix_stdout test_output 5 wrong exit status"
+	# Pipefail test with prefix_stderr_quick
+	(
+		prefix_stderr_quick "STDERR" test_output 5 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 5 $? "ts=${USE_TIMESTAMP} prefix_stderr_quick test_output 5 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDOUT: test stdout 1
-	[00:00:00] STDOUT: test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stdout/5 stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		test stdout 1
+		test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr_quick/5 stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	test stderr 1
-	test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stdout/5 stderr output should match"
-) || ret=7
+		cat > "${OUTPUT}.expected" <<-EOF
+		STDERR: test stderr 1
+		STDERR: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr_quick/5 stderr output should match"
+	) || ret=6
 
-# pipefail test with prefix_stderr
-(
-	prefix_stderr "STDERR" test_output 5 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 5 $? "prefix_stderr test_output 5 wrong exit status"
+	# pipefail test with prefix_stdout
+	(
+		prefix_stdout "STDOUT" test_output 5 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 5 $? "ts=${USE_TIMESTAMP} prefix_stdout test_output 5 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	test stdout 1
-	test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stderr/5 stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDOUT: test stdout 1
+		${TS}STDOUT: test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stdout/5 stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDERR: test stderr 1
-	[00:00:00] STDERR: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr/5 stderr output should match"
-) || ret=8
+		cat > "${OUTPUT}.expected" <<-EOF
+		test stderr 1
+		test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stdout/5 stderr output should match"
+	) || ret=7
 
-# pipefail test with prefix_output
-(
-	prefix_output "OUTPUT" test_output 5 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 5 $? "prefix_output test_output 5 wrong exit status"
+	# pipefail test with prefix_stderr
+	(
+		prefix_stderr "STDERR" test_output 5 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 5 $? "ts=${USE_TIMESTAMP} prefix_stderr test_output 5 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] OUTPUT: test stdout 1
-	[00:00:00] OUTPUT: test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_output/5 stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		test stdout 1
+		test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr/5 stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] OUTPUT: test stderr 1
-	[00:00:00] OUTPUT: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_output/5 stderr output should match"
-) || ret=9
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDERR: test stderr 1
+		${TS}STDERR: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr/5 stderr output should match"
+	) || ret=8
 
-# pipefail test with chaining prefix_stderr and prefix_stdout
-(
-	prefix_stderr "STDERR" prefix_stdout "STDOUT" test_output 5 \
-	    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
-	assert 5 $? "prefix_stderr+prefix_stdout test_output 5 wrong exit status"
+	# pipefail test with prefix_output
+	(
+		prefix_output "OUTPUT" test_output 5 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 5 $? "ts=${USE_TIMESTAMP} prefix_output test_output 5 wrong exit status"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDOUT: test stdout 1
-	[00:00:00] STDOUT: test stdout 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}"
-	assert 0 $? "prefix_stderr+prefix_stdout/5 stdout output should match"
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}OUTPUT: test stdout 1
+		${TS}OUTPUT: test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_output/5 stdout output should match"
 
-	cat > "${OUTPUT}.expected" <<-EOF
-	[00:00:00] STDERR: test stderr 1
-	[00:00:00] STDERR: test stderr 2
-	EOF
-	diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
-	assert 0 $? "prefix_stderr+prefix_stdout/5 stderr output should match"
-) || ret=10
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}OUTPUT: test stderr 1
+		${TS}OUTPUT: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_output/5 stderr output should match"
+	) || ret=9
+
+	# pipefail test with chaining prefix_stderr and prefix_stdout
+	(
+		prefix_stderr "STDERR" prefix_stdout "STDOUT" test_output 5 \
+		    > "${OUTPUT}" 2> "${OUTPUT}.stderr"
+		assert 5 $? "ts=${USE_TIMESTAMP} prefix_stderr+prefix_stdout test_output 5 wrong exit status"
+
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDOUT: test stdout 1
+		${TS}STDOUT: test stdout 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr+prefix_stdout/5 stdout output should match"
+
+		cat > "${OUTPUT}.expected" <<-EOF
+		${TS}STDERR: test stderr 1
+		${TS}STDERR: test stderr 2
+		EOF
+		diff -u "${OUTPUT}.expected" "${OUTPUT}.stderr"
+		assert 0 $? "ts=${USE_TIMESTAMP} prefix_stderr+prefix_stdout/5 stderr output should match"
+	) || ret=10
+
+	USE_TIMESTAMP=$((USE_TIMESTAMP + 1))
+done
 
 rm "${OUTPUT}" "${OUTPUT}.stderr" "${OUTPUT}.expected"
 exit "${ret}"
