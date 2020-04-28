c74216b285b785cefc5a3b552cadaa10e0602c20
Bryan Drewery <bryan@shatow.net>
2020-04-28 12:35:08 -0700

    timespecsub is only in 12.0+

--- src/libexec/poudriere/timestamp/timestamp.c
+++ src/libexec/poudriere/timestamp/timestamp.c
@@ -43,6 +43,17 @@
 #include <unistd.h>
 
 #define min(a, b) ((a) > (b) ? (b) : (a))
+#ifndef timespecsub
+#define	timespecsub(tsp, usp, vsp)					\
+	do {								\
+		(vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;		\
+		(vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;	\
+		if ((vsp)->tv_nsec < 0) {				\
+			(vsp)->tv_sec--;				\
+			(vsp)->tv_nsec += 1000000000L;			\
+		}							\
+	} while (0)
+#endif
 
 static struct timespec start;
 
