1519d2f2e2e413729ceb11da385582e9c0385018
Christoph Schönweiler <public2020@hauptsignal.at>
2020-11-02 22:58:17 +0100

    Define regular expression more precisely
    Issue #796
    
    Conflicts:
    	src/share/poudriere/jail.sh

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -170,7 +170,7 @@ cleanup_new_jail() {
 update_version() {
 	local version_extra="$1"
 
-	eval `grep "^[RB][A-Z]*=" ${SRC_BASE}/sys/conf/newvers.sh `
+	eval `grep "^REVISION=\|^BRANCH=" ${SRC_BASE}/sys/conf/newvers.sh `
 	RELEASE=${REVISION}-${BRANCH}
 	[ -n "${version_extra}" ] &&
 	    RELEASE="${RELEASE} ${version_extra}"
