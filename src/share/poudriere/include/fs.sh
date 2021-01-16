0c7d7ff957fc16e61d2be7823685be808424ec0b
Dries Michiels <32487486+Duffyx@users.noreply.github.com>
2021-01-16 19:00:01 +0100

    Drop -p
    Can be dropped as we created parent datasets in common.sh

--- src/share/poudriere/include/fs.sh
+++ src/share/poudriere/include/fs.sh
@@ -34,7 +34,7 @@ createfs() {
 
 	if [ -n "${fs}" -a "${fs}" != "none" ]; then
 		msg_n "Creating ${name} fs at ${mnt}..."
-		if ! zfs create -p ${fs}; then
+		if ! zfs create ${fs}; then
 			echo " fail"
 			err 1 "Failed to create FS ${fs}"
 		fi
