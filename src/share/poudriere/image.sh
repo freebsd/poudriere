freebsd/poudriere

commit 44c2fcb7548c1764776929d6ed3ac4e61ddf8b57

image: Don't install pmbr and gptboot on !x86
Those two bootloaders aren't needed nor generated for !x86.

src/share/poudriere/image.sh
@@ -808,11 +808,15 @@ usb+*mfs)
 	espfilename=$(mktemp /tmp/efiboot.XXXXXX)
 	make_esp_file ${espfilename} 10 ${WRKDIR}/world/boot/loader.efi
 	makefs -B little ${WRKDIR}/img.part ${WRKDIR}/out
-	mkimg -s gpt -b ${mnt}/boot/pmbr \
-		-p efi:=${espfilename} \
-		-p freebsd-boot:=${mnt}/boot/gptboot \
-		-p freebsd-ufs:=${WRKDIR}/img.part \
-		-o "${OUTPUTDIR}/${FINALIMAGE}"
+	if [ "${arch}" == "amd64" ] || [ "${arch}" == "i386" ]; then
+		pmbr="-b ${mnt}/boot/pmbr"
+		gptboot="-p freebsd-boot:=${mnt}/boot/gptboot"
+	fi
+	mkimg -s gpt ${pmbr} \
+	      -p efi:=${espfilename} \
+	      ${gptboot}
+	      -p freebsd-ufs:=${WRKDIR}/img.part \
+	      -o "${OUTPUTDIR}/${FINALIMAGE}"
 	rm -rf ${espfilename}
 	;;
 usb)
@@ -827,9 +831,13 @@ usb)
 	fi
 	espfilename=$(mktemp /tmp/efiboot.XXXXXX)
 	make_esp_file ${espfilename} 10 ${WRKDIR}/world/boot/loader.efi
-	mkimg -s gpt -b ${mnt}/boot/pmbr \
+	if [ "${arch}" == "amd64" ] || [ "${arch}" == "i386" ]; then
+		pmbr="-b ${mnt}/boot/pmbr"
+		gptboot="-p freebsd-boot:=${mnt}/boot/gptboot"
+	fi
+	mkimg -s gpt ${pmbr} \
 		-p efi:=${espfilename} \
-		-p freebsd-boot:=${mnt}/boot/gptboot \
+		${gptboot} \
 		${SWAPFIRST} \
 		-p freebsd-ufs:=${WRKDIR}/raw.img \
 		${SWAPLAST} \

commit f3af93529a19aa0b105c96570c6593a594171a57

image: Fix usb_mfs
Forgot a \ at the end of the line

Fixes: 44c2fcb75

src/share/poudriere/image.sh
@@ -795,7 +795,7 @@ usb+*mfs)
 	fi
 	mkimg -s gpt ${pmbr} \
 	      -p efi:=${espfilename} \
-	      ${gptboot}
+	      ${gptboot} \
 	      -p freebsd-ufs:=${WRKDIR}/img.part \
 	      -o "${OUTPUTDIR}/${FINALIMAGE}"
 	rm -rf ${espfilename}