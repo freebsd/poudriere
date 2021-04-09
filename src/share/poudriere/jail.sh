52fc39e6c9aab61f7e667adf825e81673cfe1942
Bryan Drewery <bryan@shatow.net>
2021-04-09 12:46:22 -0700

    ports/jail: Update usage and manpages for svn/git
    - Fixes method lists
    - Note use of -U and other relevant flags
    - Expand out the ports methods like the jail manpage has
    - Add some more examples to each
    - Sort git before svn
    - Note support for -D, -v, and -t where appropriate
    
    Issue #851

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -44,7 +44,7 @@ Parameters:
 
 Options:
     -b            -- Build the OS (for use with -m src)
-    -B            -- Build the pkgbase set (for use with -b or -m svn/git/...)
+    -B            -- Build the pkgbase set (for use with -b or -m git/svn/...)
     -q            -- Quiet (Do not print the header)
     -n            -- Print only jail name (for use with -l)
     -J n          -- Run buildworld in parallel with n jobs.
@@ -60,9 +60,9 @@ Options:
     -m method     -- When used with -c, overrides the default method for
                      obtaining and building the jail. See poudriere(8) for more
                      details. Can be one of:
-                       allbsd, ftp-archive, ftp, freebsdci, git, http, null, src=PATH,
-                       svn, svn+file, svn+http, svn+https, svn+ssh, tar=PATH
-                       url=SOMEURL.
+                       'allbsd', 'ftp-archive', 'ftp', 'freebsdci', 'http',
+		       'null', 'src=PATH', 'tar=PATH', 'url=URL', or
+		       '{git,svn}{,+http,+https,+file,+ssh}' (e.g., 'git+https').
                      The default is '${METHOD_DEF}'.
     -P patch      -- Specify a patch to apply to the source before building.
     -S srcpath    -- Specify a path to the source tree to be used.
