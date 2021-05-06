freebsd/poudriere

commit 7469c714975c74e911fb6faf5805500355d0ee91

In interactive mode mount the ports tree in read-write mode, allowing to patch it.

src/share/poudriere/common.sh
@@ -1876,7 +1876,8 @@ do_portbuild_mounts() {
 	_pget portsdir ${ptname} mnt || err 1 "Missing mnt metadata for portstree"
 	[ -d ${portsdir}/ports ] && portsdir=${portsdir}/ports
 	${msgmount} "Mounting ports from: ${portsdir}"
-	${NULLMOUNT} -o ro ${portsdir} ${mnt}${PORTSDIR} ||
+	[ ${INTERACTIVE_MODE} -gt 0 ] && OPT=rw || OPT=ro
+	${NULLMOUNT} -o ${OPT} ${portsdir} ${mnt}${PORTSDIR} ||
 		err 1 "Failed to mount the ports directory "
 	for o in ${OVERLAYS}; do
 		_pget odir "${o}" mnt || err 1 "Missing mnt metadata for overlay ${o}"

commit 76477684dd5215e2b48a86d88b98cd9bd94731ca

jail -s: Fix parsing error
Fixes: 7469c71497

src/share/poudriere/common.sh
@@ -8219,6 +8219,7 @@ fi
 : ${HTML_TRACK_REMAINING:=no}
 : ${FORCE_MOUNT_HASH:=no}
 DRY_RUN=0
+INTERACTIVE_MODE=0
 
 # Be sure to update poudriere.conf to document the default when changing these
 : ${RESOLV_CONF="/etc/resolv.conf"}