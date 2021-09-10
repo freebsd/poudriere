freebsd/poudriere

commit ec1e7444f622dfeb33bf3079e18d98adea9be569

jail: Allow poudriere -v to override -q for git/svn
This was already done for ports.

src/share/poudriere/jail.sh
@@ -589,11 +589,14 @@ install_from_vcs() {
 		case ${METHOD} in
 		svn*)
 			msg_n "Checking out the sources with ${METHOD}..."
-			${SVN_CMD} -q co ${SVN_FULLURL}/${VERSION} ${SRC_BASE} || err 1 " fail"
+			${SVN_CMD} ${quiet} checkout \
+			    ${SVN_FULLURL}/${VERSION} ${SRC_BASE} || \
+			    err 1 " fail"
 			echo " done"
 			if [ -n "${SRCPATCHFILE}" ]; then
 				msg_n "Patching the sources with ${SRCPATCHFILE}"
-				${SVN_CMD} -q patch ${SRCPATCHFILE} ${SRC_BASE} || err 1 " fail"
+				${SVN_CMD} ${quiet} patch ${SRCPATCHFILE} \
+				    ${SRC_BASE} || err 1 " fail"
 				echo done
 			fi
 			;;
@@ -602,7 +605,9 @@ install_from_vcs() {
 				err 1 "Patch files not supported with git, please use feature branches"
 			fi
 			msg_n "Checking out the sources with ${METHOD}..."
-			${GIT_CMD} clone ${GIT_DEPTH} -q -b ${VERSION} ${GIT_FULLURL} ${SRC_BASE} || err 1 " fail"
+			${GIT_CMD} clone ${GIT_DEPTH} ${quiet} \
+			    -b ${VERSION} ${GIT_FULLURL} ${SRC_BASE} || \
+			    err 1 " fail"
 			echo " done"
 			# No support for patches, using feature branches is recommanded"
 			;;
@@ -612,13 +617,15 @@ install_from_vcs() {
 		svn*)
 			msg_n "Updating the sources with ${METHOD}..."
 			${SVN_CMD} upgrade ${SRC_BASE} 2>/dev/null || :
-			${SVN_CMD} -q update -r ${TORELEASE:-head} ${SRC_BASE} || err 1 " fail"
+			${SVN_CMD} ${quiet} update -r ${TORELEASE:-head} ${SRC_BASE} || err 1 " fail"
 			echo " done"
 			;;
 		git*)
-			${GIT_CMD} -C ${SRC_BASE} pull --rebase -q || err 1 " fail"
+			${GIT_CMD} -C ${SRC_BASE} pull --rebase ${quiet} || \
+			    err 1 " fail"
 			if [ -n "${TORELEASE}" ]; then
-				${GIT_CMD} -C ${SRC_BASE} checkout -q "${TORELEASE}" || err 1 " fail"
+				${GIT_CMD} -C ${SRC_BASE} checkout \
+				    ${quiet} "${TORELEASE}" || err 1 " fail"
 			fi
 			echo " done"
 			;;
@@ -1182,6 +1189,7 @@ done
 saved_argv="$@"
 shift $((OPTIND-1))
 post_getopts
+[ ${VERBOSE} -gt 0 ] || quiet="-q"
 
 METHOD=${METHOD:-ftp}
 CLEANJAIL=${CLEAN:-none}

src/share/poudriere/ports.sh
@@ -132,6 +132,7 @@ done
 saved_argv="$@"
 shift $((OPTIND-1))
 post_getopts
+[ ${VERBOSE} -gt 0 ] || quiet="-q"
 
 [ ${FAKE} -eq 0 ] && METHOD=${METHOD:-${METHOD_DEF}}
 PTNAME=${PTNAME:-default}
@@ -268,7 +269,6 @@ create)
 			fi
 
 			msg_n "Checking out the ports tree..."
-			[ ${VERBOSE} -gt 0 ] || quiet="-q"
 			${SVN_CMD} ${quiet} co \
 				${SVN_PRESERVE_TIMESTAMP} \
 				${SVN_FULLURL}/${BRANCH} \
@@ -277,7 +277,6 @@ create)
 			;;
 		git*)
 			msg_n "Cloning the ports tree..."
-			[ ${VERBOSE} -gt 0 ] || quiet="-q"
 			${GIT_CMD} clone --depth=1 --single-branch ${quiet} \
 			    ${BRANCH:+-b ${BRANCH}} ${GIT_FULLURL} ${PTMNT} || \
 			    err 1 " fail"
@@ -354,7 +353,6 @@ update)
 		;;
 	svn*)
 		msg_n "Updating portstree \"${PTNAME}\" with ${METHOD}..."
-		[ ${VERBOSE} -gt 0 ] || quiet="-q"
 		${SVN_CMD} upgrade ${PORTSMNT:-${PTMNT}} 2>/dev/null || :
 		${SVN_CMD} ${quiet} update \
 			${SVN_PRESERVE_TIMESTAMP} \
@@ -363,7 +361,6 @@ update)
 		;;
 	git*)
 		msg_n "Updating portstree \"${PTNAME}\" with ${METHOD}..."
-		[ ${VERBOSE} -gt 0 ] || quiet="-q"
 		${GIT_CMD} -C ${PORTSMNT:-${PTMNT}} pull --rebase ${quiet}
 		echo " done"
 		;;

commit 67b4289bd7da0a1154412e808cf2bf8900c60ef1

ports/jail: Only force -q for create/upate modes [fix -l headers]
Fixes: ec1e7444f62 ("jail: Allow poudriere -v to override -q for git/svn")
(cherry picked from commit bbf05bd505ec4676fc8800d61fa2af4b8e3ad241)

Conflicts:
	src/share/poudriere/jail.sh
	src/share/poudriere/ports.sh

src/share/poudriere/jail.sh
@@ -1165,7 +1165,6 @@ done
 saved_argv="$@"
 shift $((OPTIND-1))
 post_getopts
-[ ${VERBOSE} -gt 0 ] || quiet="-q"
 
 METHOD=${METHOD:-${METHOD_DEF}}
 CLEANJAIL=${CLEAN:-none}
@@ -1205,6 +1204,7 @@ fi
 
 case "${CREATE}${INFO}${LIST}${STOP}${START}${DELETE}${UPDATE}${RENAME}" in
 	10000000)
+		[ ${VERBOSE} -gt 0 ] || quiet="-q"
 		[ -z "${JAILNAME}" ] && usage JAILNAME
 		case ${METHOD} in
 			src=*|null|git*) ;;
@@ -1256,6 +1256,7 @@ case "${CREATE}${INFO}${LIST}${STOP}${START}${DELETE}${UPDATE}${RENAME}" in
 		delete_jail
 		;;
 	00000010)
+		[ ${VERBOSE} -gt 0 ] || quiet="-q"
 		[ -z "${JAILNAME}" ] && usage JAILNAME
 		jail_exists ${JAILNAME} || err 1 "No such jail: ${JAILNAME}"
 		maybe_run_queued "${saved_argv}"

src/share/poudriere/ports.sh
@@ -139,7 +139,6 @@ done
 saved_argv="$@"
 shift $((OPTIND-1))
 post_getopts
-[ ${VERBOSE} -gt 0 ] || quiet="-q"
 
 [ ${FAKE} -eq 0 ] && METHOD=${METHOD:-${METHOD_DEF}}
 PTNAME=${PTNAME:-default}
@@ -219,6 +218,7 @@ check_portsnap_interactive() {
 }
 
 if [ ${CREATE} -eq 1 ]; then
+	[ ${VERBOSE} -gt 0 ] || quiet="-q"
 	# test if it already exists
 	porttree_exists ${PTNAME} && err 2 "The ports tree, ${PTNAME}, already exists"
 	maybe_run_queued "${saved_argv}"
@@ -342,6 +342,7 @@ if [ ${DELETE} -eq 1 ]; then
 fi
 
 if [ ${UPDATE} -eq 1 ]; then
+	[ ${VERBOSE} -gt 0 ] || quiet="-q"
 	porttree_exists ${PTNAME} || err 2 "No such ports tree ${PTNAME}"
 	METHOD=$(pget ${PTNAME} method)
 	PTMNT=$(pget ${PTNAME} mnt)