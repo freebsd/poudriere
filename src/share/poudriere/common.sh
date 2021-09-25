freebsd/poudriere

commit b999f9fd3a3c59a1451bb2eaed317cabf7569703

Rework c25726ffc: deletions are not all mutually exclusion.
Fixes c25726ffc8 ("bulk missing pkg: Delete packages before fetching from remote")

src/share/poudriere/common.sh
@@ -7510,7 +7510,7 @@ trim_ignored_pkg() {
 prepare_ports() {
 	local pkg
 	local log log_top
-	local n resuming_build delete_all
+	local n resuming_build
 	local cache_dir sflag delete_pkg_list shash_bucket
 
 	pkgqueue_init
@@ -7606,15 +7606,17 @@ prepare_ports() {
 		if [ -f "${PACKAGES}/.jailversion" ] &&
 		    [ "$(cat ${PACKAGES}/.jailversion)" != \
 		    "$(jget ${JAILNAME} version)" ]; then
-			delete_all="newer version of jail"
-		elif [ ${CLEAN} -eq 1 ]; then
+			delete_all_pkgs "newer version of jail"
+		fi
+		if [ ${CLEAN} -eq 1 ]; then
 			if [ "${ATOMIC_PACKAGE_REPOSITORY}" != "yes" ] && \
 			    package_dir_exists_and_has_packages; then
 				confirm_if_tty "Are you sure you want to clean all packages?" || \
 				    err 1 "Not cleaning all packages"
 			fi
-			delete_all="-c specified"
-		elif [ ${CLEAN_LISTED} -eq 1 ]; then
+			delete_all_pkgs "-c specified"
+		fi
+		if [ ${CLEAN_LISTED} -eq 1 ]; then
 			msg "-C specified, cleaning listed packages"
 			delete_pkg_list=$(mktemp -t poudriere.cleanC)
 			clear_dep_fatal_error
@@ -7642,13 +7644,9 @@ prepare_ports() {
 			cat "${delete_pkg_list}" | tr '\n' '\000' | \
 			    xargs -0 rm -rf
 			unlink "${delete_pkg_list}" || :
-		elif ! ensure_pkg_installed; then
-			delete_all="pkg package missing"
 		fi
-		if [ -n "${delete_all}" ]; then
-			msg_n "${delete_all}, cleaning all packages..."
-			rm -rf ${PACKAGES:?}/* ${cache_dir}
-			echo " done"
+		if ! ensure_pkg_installed; then
+			delete_all_pkgs "pkg package missing"
 		fi
 
 		# If the build is being resumed then packages already

src/share/poudriere/include/pkg.sh
@@ -303,3 +303,12 @@ delete_stale_pkg_cache() {
 
 	return 0
 }
+
+delete_all_pkgs() {
+	[ $# -eq 1 ] || eargs delete_all_pkgs reason
+	local reason="$1"
+
+	msg_n "${reason}, cleaning all packages..."
+	rm -rf ${PACKAGES:?}/* ${cache_dir}
+	echo " done"
+}

commit eff0a5468dda3b01c8fa63685a6ebb010cfbcbf3

delete_all_pkgs: Need to lookup cache_dir.
Fixes: b999f9fd

src/share/poudriere/common.sh
@@ -7691,7 +7691,6 @@ prepare_ports() {
 	if was_a_bulk_run; then
 		_log_path log
 		_log_path_top log_top
-		get_cache_dir cache_dir
 
 		if [ -e "${log}/.poudriere.ports.built" ]; then
 			resuming_build=1
@@ -7707,6 +7706,7 @@ prepare_ports() {
 		fi
 
 		if [ ${resuming_build} -eq 0 ] || ! [ -d "${log}" ]; then
+			get_cache_dir cache_dir
 			# Sync in HTML files through a base dir
 			install_html_files "${HTMLPREFIX}" "${log_top}/.html" \
 			    "${log}"

src/share/poudriere/include/pkg.sh
@@ -307,7 +307,9 @@ delete_stale_pkg_cache() {
 delete_all_pkgs() {
 	[ $# -eq 1 ] || eargs delete_all_pkgs reason
 	local reason="$1"
+	local cache_dir
 
+	get_cache_dir cache_dir
 	msg_n "${reason}, cleaning all packages..."
 	rm -rf ${PACKAGES:?}/* ${cache_dir}
 	echo " done"