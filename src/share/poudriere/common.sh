freebsd/poudriere

commit a679ac0777f2528239a103449a7bfc0bf25ad314

Only show logs for bulk runs
Fixes 3d248385ba ("Show loginfo on error")

src/share/poudriere/common.sh
@@ -110,8 +110,10 @@ _err() {
 	# Avoid recursive err()->exit_handler()->err()... Just let
 	# exit_handler() cleanup.
 	if [ ${ERRORS_ARE_FATAL:-1} -eq 1 ]; then
-		show_build_summary
-		show_log_info
+		if was_a_bulk_run && [ -n "${POUDRIERE_BUILD_TYPE-}" ]; then
+			show_build_summary
+			show_log_info
+		fi
 		exit $1
 	else
 		return 0

commit d455c6be1b62d75fd02094d06b98649723fc62e8

err: Print logs to stderr to avoid reading into err'd pipes
Fixes: a679ac0777 ("Only show logs for bulk runs")

src/share/poudriere/common.sh
@@ -111,8 +111,8 @@ _err() {
 	# exit_handler() cleanup.
 	if [ ${ERRORS_ARE_FATAL:-1} -eq 1 ]; then
 		if was_a_bulk_run && [ -n "${POUDRIERE_BUILD_TYPE-}" ]; then
-			show_build_summary
-			show_log_info
+			show_build_summary >&2
+			show_log_info >&2
 		fi
 		exit $1
 	else