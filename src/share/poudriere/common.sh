freebsd/poudriere

commit 855b17c59b7ae344cd2022d35e09d646bb66766e

Fix empty dir checking for builder pid wait
Add dirempty() and globmatch() for other uses.

Fixes: 255994508c4b

src/share/poudriere/common.sh
@@ -4593,13 +4593,11 @@ stop_builder() {
 }
 
 stop_builders() {
-	local PARALLEL_JOBS real_parallel_jobs
+	local PARALLEL_JOBS real_parallel_jobs pid
 
 	# wait for the last running processes
-	case ${MASTER_DATADIR}/var/run/*.pid in
-	"${MASTER_DATADIR}/var/run/*.pid") ;;
-	*) cat ${MASTER_DATADIR}/var/run/*.pid | xargs pwait 2>/dev/null ;;
-	esac
+	find "${MASTER_DATADIR}/var/run/" -name "*.pid" -print0 |
+	    xargs -0 pwait 2>/dev/null
 
 	if [ ${PARALLEL_JOBS} -ne 0 ]; then
 		msg "Stopping ${PARALLEL_JOBS} builders"

src/share/poudriere/include/util.sh
@@ -1291,3 +1291,28 @@ _mktemp() {
 	setvar "${_mktemp_var_return}" "${_mktemp_tmpfile}"
 	return "${ret}"
 }
+
+dirempty() {
+	[ $# -eq 1 ] || eargs dirempty
+	local dir="$1"
+
+	! globmatch "${dir}/*"
+}
+
+globmatch() {
+	[ $# -eq 1 ] || eargs globmatch glob
+	local glob="$1"
+	local match
+
+	case "${glob}" in
+	*"*"*|*"?"*|*"["*) ;;
+	*) err ${EX_DATAERR} "globmatch: '${glob}' is not a glob" ;;
+	esac
+
+	for match in ${glob}; do
+		case "${match}" in
+		"${glob}") return 1 ;;
+		esac
+		return 0
+	done
+}

test/Makefile.am
@@ -13,6 +13,7 @@ TESTS=\
 	getpid.sh \
 	getvar.sh \
 	gsub.sh \
+	globmatch.sh \
 	hash_basic.sh \
 	list.sh \
 	locks.sh \

test/Makefile.in
@@ -458,7 +458,7 @@ EXTRA_DIST = $(TESTS) \
 
 # Depend bulk tests on jail setup
 TESTS = builtins.sh cache.sh do_clone.sh encode_args.sh getpid.sh \
-	getvar.sh gsub.sh hash_basic.sh list.sh locks.sh \
+	getvar.sh gsub.sh globmatch.sh hash_basic.sh list.sh locks.sh \
 	locks_critical_section.sh locks_critical_section_nested.sh \
 	locked_mkdir.sh locked_mkdir_waiters.sh \
 	locked_mkdir_waiters_kill.sh locked_mkdir_waiters_all_lose.sh \
@@ -741,6 +741,13 @@ getvar.sh.log: getvar.sh
 gsub.sh.log: gsub.sh
 	@p='gsub.sh'; \
 	b='gsub.sh'; \
+	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+	--log-file $$b.log --trs-file $$b.trs \
+	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+	"$$tst" $(AM_TESTS_FD_REDIRECT)
+globmatch.sh.log: globmatch.sh
+	@p='globmatch.sh'; \
+	b='globmatch.sh'; \
 	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
 	--log-file $$b.log --trs-file $$b.trs \
 	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \

test/globmatch.sh
@@ -1,0 +1,15 @@
+set -e
+. common.sh
+. ${SCRIPTPREFIX}/common.sh
+set +e
+
+tdir=$(mktemp -d)
+
+assert_ret 1 globmatch "${tdir}/*"
+assert_ret 0 dirempty "${tdir}"
+touch "${tdir}/blah"
+echo ${tdir}/*
+assert_ret 0 globmatch "${tdir}/*"
+assert_ret 1 dirempty "${tdir}"
+
+rm -rf "${tdir}"

commit d4d40746ff8f51daef65dac5c89aea6e71ece504

Actually fix pid pwaiting.
This probably _never_ worked so add a low timeout too.

Fixes: 855b17c59b7

src/share/poudriere/common.sh
@@ -4585,8 +4585,10 @@ stop_builders() {
 	local PARALLEL_JOBS real_parallel_jobs pid
 
 	# wait for the last running processes
-	find "${MASTER_DATADIR}/var/run/" -name "*.pid" -print0 |
-	    xargs -0 pwait 2>/dev/null
+	# the pidfiles may not contain an EOF newline so awk fixes that
+	find "${MASTER_DATADIR}/var/run/" -name "*.pid" -exec \
+	    awk '{print}' {} + |
+	    xargs pwait -t 20 2>/dev/null || :
 
 	if [ ${PARALLEL_JOBS} -ne 0 ]; then
 		msg "Stopping ${PARALLEL_JOBS} builders"