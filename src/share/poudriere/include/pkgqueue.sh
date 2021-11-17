edfaf7ac03ac0cdb72018ab3d352299d3f223e1f
Bryan Drewery <bryan@shatow.net>
2021-11-16 23:22:11 -0800

    Revert "pkgqueue_remaining: Unhide errors"
    This reverts commit 6d3fe23c9e43b9469536f330ede5c0015fd9c89f.
    
    This is too racy with clean_pool() to not hide the errors from find.

--- src/share/poudriere/include/pkgqueue.sh
+++ src/share/poudriere/include/pkgqueue.sh
@@ -310,7 +310,7 @@ pkgqueue_remaining() {
 		# Find items in queue not ready-to-build.
 		( cd "${MASTER_DATADIR}"; pkgqueue_list ) | \
 		    sed -e 's,$, waiting-on-dependency,'
-	} | sed -e 's,.*/,,'
+	} 2>/dev/null | sed -e 's,.*/,,'
 }
 
 # Return directory name for given job
