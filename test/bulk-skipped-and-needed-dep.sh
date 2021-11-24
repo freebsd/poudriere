33bf0c7f94f0ad75bbcad469fecfc0c09d9de758
Bryan Drewery <bryan@shatow.net>
2021-11-24 08:03:14 -0800

    Add another IGNORE/listed list

---
+++ test/bulk-skipped-and-needed-dep.sh
@@ -0,0 +1,23 @@
+LISTPORTS="ports-mgmt/poudriere-devel-dep-IGNORED misc/freebsd-release-manifests"
+# porta -> portb -> portc
+# list -> ignore -> skip(and listed)
+# ports-mgmt/poudriere-devel-dep-IGNORED -> ports-mgmt/poudriere-devel-IGNORED -> misc/freebsd-release-manifests
+# list also portc
+
+# ports-mgmt/poudriere-devel-dep-IGNORED depends on ports-mgmt/poudriere-devel-IGNORED
+# which is IGNORED.
+# ports-mgmt/poudriere-devel-dep-IGNORED should be skipped.
+# ports-mgmt/poudriere-devel-IGNORED depends on misc/freebsd-release-manifests which is skipped
+# but misc/freebsd-release-manifests is listed so should be queued.
+OVERLAYS="omnibus"
+. common.bulk.sh
+
+do_bulk -n ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+
+EXPECTED_IGNORED="ports-mgmt/poudriere-devel-IGNORED"
+EXPECTED_SKIPPED="ports-mgmt/poudriere-devel-dep-IGNORED"
+
+assert_bulk_queue_and_stats
+
+
