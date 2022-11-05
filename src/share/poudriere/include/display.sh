freebsd/poudriere

commit 17066c65f67818f674204784d2102b123d656cfd

display_setup: columns are not used for anything

src/share/poudriere/include/display.sh
@@ -25,10 +25,9 @@
 # SUCH DAMAGE.
 
 display_setup() {
-	[ $# -ge 2 ] || eargs display_setup format columns [column_sort]
+	[ $# -ge 1 ] || eargs display_setup format [column_sort]
 	_DISPLAY_DATA=
 	_DISPLAY_FORMAT="$1"
-	_DISPLAY_COLUMNS="$2"
 	_DISPLAY_COLUMN_SORT="${3-}"
 }
 
@@ -126,6 +125,6 @@ display_output() {
 		printf "${format}\n" "$@"
 	done
 
-	unset _DISPLAY_DATA _DISPLAY_FORMAT _DISPLAY_COLUMNS \
+	unset _DISPLAY_DATA _DISPLAY_FORMAT \
 	    _DISPLAY_COLUMN_SORT
 }

src/share/poudriere/jail.sh
@@ -94,12 +94,12 @@ list_jail() {
 
 	if [ ${NAMEONLY} -eq 0 ]; then
 		format='%%-%ds %%-%ds %%-%ds %%-%ds %%-%ds %%s'
-		display_setup "${format}" 6 "-d -k2,2 -k3,3 -k1,1"
+		display_setup "${format}" "-d -k2,2 -k3,3 -k1,1"
 		display_add "JAILNAME" "VERSION" "ARCH" "METHOD" \
 		    "TIMESTAMP" "PATH"
 	else
 		format='%s'
-		display_setup "${format}" 1 "-d"
+		display_setup "${format}" "-d"
 		display_add JAILNAME
 	fi
 	[ -d ${POUDRIERED}/jails ] || return 0

src/share/poudriere/ports.sh
@@ -194,11 +194,11 @@ case $COMMAND in
 list)
 	if [ ${NAMEONLY} -eq 0 ]; then
 		format='%%-%ds %%-%ds %%-%ds %%s\n'
-		display_setup "${format}" 4 "-d"
+		display_setup "${format}" "-d"
 		display_add "PORTSTREE" "METHOD" "TIMESTAMP" "PATH"
 	else
 		format='%s'
-		display_setup "${format}" 1 "-d"
+		display_setup "${format}" "-d"
 		display_add "PORTSTREE"
 	fi
 	while read ptname ptmethod ptpath; do

src/share/poudriere/status.sh
@@ -191,7 +191,7 @@ show_summary() {
 		[ ${COMPACT} -eq 0 ] && format="${format}\t%s"
 	fi
 
-	display_setup "${format}" "${columns}" "-d -k1,1 -k2,2 -k3,3n -k4,4n"
+	display_setup "${format}" "-d -k1,1 -k2,2 -k3,3n -k4,4n"
 
 	if [ ${COMPACT} -eq 0 ]; then
 		if [ -n "${URL_BASE}" ] && [ ${URL} -eq 1 ]; then

commit 5b82d94022bbec93b5bb7a51a5b30eb4179ce6ac

display: Fix sorting of columns
Fixes: 17066c65f67818

src/share/poudriere/include/display.sh
@@ -28,7 +28,7 @@ display_setup() {
 	[ $# -ge 1 ] || eargs display_setup format [column_sort]
 	_DISPLAY_DATA=
 	_DISPLAY_FORMAT="$1"
-	_DISPLAY_COLUMN_SORT="${3-}"
+	_DISPLAY_COLUMN_SORT="${2-}"
 }
 
 display_add() {