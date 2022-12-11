368fc8eebef993d53cab7a474ea2eddd8cfede61
Bryan Drewery <bryan@shatow.net>
2022-12-11 12:52:43 -0800

    tests: Add a bulk-build-self-immutable-base-no.sh to capture all cases

---
+++ test/bulk-build-self-immutable-base-no.sh
@@ -0,0 +1,17 @@
+LISTPORTS="ports-mgmt/poudriere-devel"
+OVERLAYS="omnibus"
+IMMUTABLE_BASE=no
+. common.bulk.sh
+
+do_bulk -t -c ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+
+EXPECTED_QUEUED="ports-mgmt/pkg misc/freebsd-release-manifests@default ports-mgmt/poudriere-devel"
+EXPECTED_LISTED="ports-mgmt/poudriere-devel"
+EXPECTED_TOBUILD="${EXPECTED_QUEUED}"
+EXPECTED_BUILT="${EXPECTED_TOBUILD}"
+
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+
+do_pkgclean_smoke
