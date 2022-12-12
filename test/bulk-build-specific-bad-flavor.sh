f343e51a73cd99759499ff655dc5ddddca16cc41
Bryan Drewery <bryan@shatow.net>
2022-12-12 07:57:53 -0800

    Add bulk test for a bad FLAVOR

---
+++ test/bulk-build-specific-bad-flavor.sh
@@ -0,0 +1,18 @@
+OVERLAYS=omnibus
+FLAVOR_ALL=all
+LISTPORTS="misc/freebsd-release-manifests@badflavor"
+. common.bulk.sh
+
+EXPECTED_QUEUED=
+EXPECTED_LISTED=
+EXPECTED_TOBUILD=
+EXPECTED_BUILT=
+do_bulk -cn ${LISTPORTS}
+assert 1 "$?" "bulk dry-run for bad flavor should fail"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+
+do_bulk -c ${LISTPORTS}
+assert 1 "$?" "bulk dry-run for bad flavor should fail"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
