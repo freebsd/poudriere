5c0c0f8122b2d688c68406a026959b821b320138
Bryan Drewery <bryan@shatow.net>
2022-12-15 10:11:14 -0800

    Add test for BAD_PKGNAME_DEPS_ARE_FATAL

---
+++ test/bulk-bad-dep-pkgname.sh
@@ -0,0 +1,31 @@
+LISTPORTS="ports-mgmt/yyyy"
+OVERLAYS="overlay omnibus"
+. common.bulk.sh
+
+set_poudriere_conf <<-EOF
+BAD_PKGNAME_DEPS_ARE_FATAL=no
+EOF
+
+do_bulk -n ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+
+EXPECTED_IGNORED=
+EXPECTED_TOBUILD="${LISTPORTS} ports-mgmt/pkg misc/foo@flav"
+EXPECTED_QUEUED="${EXPECTED_TOBUILD}"
+EXPECTED_LISTED="${LISTPORTS}"
+
+assert_bulk_queue_and_stats
+
+set_poudriere_conf <<-EOF
+BAD_PKGNAME_DEPS_ARE_FATAL=yes
+EOF
+
+do_bulk -n ${LISTPORTS}
+assert 1 $? "Bulk should fail"
+
+EXPECTED_IGNORED=
+EXPECTED_TOBUILD=
+EXPECTED_QUEUED=
+EXPECTED_LISTED="${LISTPORTS}"
+
+assert_bulk_queue_and_stats
