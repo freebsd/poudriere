2b376832ce879effe6d24072e8a16106207488e1
Bryan Drewery <bryan@shatow.net>
2023-01-01 12:32:26 -0800

    runtest: Bump build timeouts due to increased parallelism

--- test/runtest.sh
+++ test/runtest.sh
@@ -82,8 +82,8 @@ cd "${THISDIR}"
 
 case "${1##*/}" in
 prep.sh) : ${TIMEOUT:=1800} ;;
-bulk*build*.sh) : ${TIMEOUT:=900} ;;
-bulk*.sh) : ${TIMEOUT:=300} ;;
+bulk*build*.sh) : ${TIMEOUT:=1800} ;;
+bulk*.sh) : ${TIMEOUT:=500} ;;
 locked_mkdir.sh) : ${TIMEOUT:=120} ;;
 esac
 case "${1##*/}" in
