772bd889011c72f6b8ee4cb556f1dcf049468775
Guido Falsi <mad@madpilot.net>
2023-10-31 16:26:39 +0100

    Add option to force rebuilding repo and signing.

--- src/share/poudriere/distclean.sh
+++ src/share/poudriere/distclean.sh
@@ -191,7 +191,7 @@ comm -1 -3 ${DISTFILES_LIST}.expected ${DISTFILES_LIST}.actual \
 
 ret=0
 do_confirm_delete "${DISTFILES_LIST}.unexpected" "stale distfiles" \
-    "${answer}" "${DRY_RUN}" || ret=$?
+    "${answer}" "${DRY_RUN}" "0" || ret=$?
 if [ ${ret} -eq 2 ]; then
 	exit 0
 fi
