freebsd/poudriere

commit 397973f1fb41eede01ad154059b8efd9e4c32e87

jexecd: remove feature
Jexecd has never really been used and it is now actually broken

aclocal.m4
@@ -20,185 +20,6 @@ You have another version of autoconf.  It may work, but is not guaranteed to.
 If you have problems, you may need to regenerate the build system entirely.
 To do so, use the procedure documented by the package, typically 'autoreconf'.])])
 
-# ===========================================================================
-#  https://www.gnu.org/software/autoconf-archive/ax_check_compile_flag.html
-# ===========================================================================
-#
-# SYNOPSIS
-#
-#   AX_CHECK_COMPILE_FLAG(FLAG, [ACTION-SUCCESS], [ACTION-FAILURE], [EXTRA-FLAGS], [INPUT])
-#
-# DESCRIPTION
-#
-#   Check whether the given FLAG works with the current language's compiler
-#   or gives an error.  (Warnings, however, are ignored)
-#
-#   ACTION-SUCCESS/ACTION-FAILURE are shell commands to execute on
-#   success/failure.
-#
-#   If EXTRA-FLAGS is defined, it is added to the current language's default
-#   flags (e.g. CFLAGS) when the check is done.  The check is thus made with
-#   the flags: "CFLAGS EXTRA-FLAGS FLAG".  This can for example be used to
-#   force the compiler to issue an error when a bad flag is given.
-#
-#   INPUT gives an alternative input source to AC_COMPILE_IFELSE.
-#
-#   NOTE: Implementation based on AX_CFLAGS_GCC_OPTION. Please keep this
-#   macro in sync with AX_CHECK_{PREPROC,LINK}_FLAG.
-#
-# LICENSE
-#
-#   Copyright (c) 2008 Guido U. Draheim <guidod@gmx.de>
-#   Copyright (c) 2011 Maarten Bosmans <mkbosmans@gmail.com>
-#
-#   Copying and distribution of this file, with or without modification, are
-#   permitted in any medium without royalty provided the copyright notice
-#   and this notice are preserved.  This file is offered as-is, without any
-#   warranty.
-
-#serial 6
-
-AC_DEFUN([AX_CHECK_COMPILE_FLAG],
-[AC_PREREQ(2.64)dnl for _AC_LANG_PREFIX and AS_VAR_IF
-AS_VAR_PUSHDEF([CACHEVAR],[ax_cv_check_[]_AC_LANG_ABBREV[]flags_$4_$1])dnl
-AC_CACHE_CHECK([whether _AC_LANG compiler accepts $1], CACHEVAR, [
-  ax_check_save_flags=$[]_AC_LANG_PREFIX[]FLAGS
-  _AC_LANG_PREFIX[]FLAGS="$[]_AC_LANG_PREFIX[]FLAGS $4 $1"
-  AC_COMPILE_IFELSE([m4_default([$5],[AC_LANG_PROGRAM()])],
-    [AS_VAR_SET(CACHEVAR,[yes])],
-    [AS_VAR_SET(CACHEVAR,[no])])
-  _AC_LANG_PREFIX[]FLAGS=$ax_check_save_flags])
-AS_VAR_IF(CACHEVAR,yes,
-  [m4_default([$2], :)],
-  [m4_default([$3], :)])
-AS_VAR_POPDEF([CACHEVAR])dnl
-])dnl AX_CHECK_COMPILE_FLAGS
-
-# ===========================================================================
-#  https://www.gnu.org/software/autoconf-archive/ax_check_enable_debug.html
-# ===========================================================================
-#
-# SYNOPSIS
-#
-#   AX_CHECK_ENABLE_DEBUG([enable by default=yes/info/profile/no], [ENABLE DEBUG VARIABLES ...], [DISABLE DEBUG VARIABLES NDEBUG ...], [IS-RELEASE])
-#
-# DESCRIPTION
-#
-#   Check for the presence of an --enable-debug option to configure, with
-#   the specified default value used when the option is not present.  Return
-#   the value in the variable $ax_enable_debug.
-#
-#   Specifying 'yes' adds '-g -O0' to the compilation flags for all
-#   languages. Specifying 'info' adds '-g' to the compilation flags.
-#   Specifying 'profile' adds '-g -pg' to the compilation flags and '-pg' to
-#   the linking flags. Otherwise, nothing is added.
-#
-#   Define the variables listed in the second argument if debug is enabled,
-#   defaulting to no variables.  Defines the variables listed in the third
-#   argument if debug is disabled, defaulting to NDEBUG.  All lists of
-#   variables should be space-separated.
-#
-#   If debug is not enabled, ensure AC_PROG_* will not add debugging flags.
-#   Should be invoked prior to any AC_PROG_* compiler checks.
-#
-#   IS-RELEASE can be used to change the default to 'no' when making a
-#   release.  Set IS-RELEASE to 'yes' or 'no' as appropriate. By default, it
-#   uses the value of $ax_is_release, so if you are using the AX_IS_RELEASE
-#   macro, there is no need to pass this parameter.
-#
-#     AX_IS_RELEASE([git-directory])
-#     AX_CHECK_ENABLE_DEBUG()
-#
-# LICENSE
-#
-#   Copyright (c) 2011 Rhys Ulerich <rhys.ulerich@gmail.com>
-#   Copyright (c) 2014, 2015 Philip Withnall <philip@tecnocode.co.uk>
-#
-#   Copying and distribution of this file, with or without modification, are
-#   permitted in any medium without royalty provided the copyright notice
-#   and this notice are preserved.
-
-#serial 9
-
-AC_DEFUN([AX_CHECK_ENABLE_DEBUG],[
-    AC_BEFORE([$0],[AC_PROG_CC])dnl
-    AC_BEFORE([$0],[AC_PROG_CXX])dnl
-    AC_BEFORE([$0],[AC_PROG_F77])dnl
-    AC_BEFORE([$0],[AC_PROG_FC])dnl
-
-    AC_MSG_CHECKING(whether to enable debugging)
-
-    ax_enable_debug_default=m4_tolower(m4_normalize(ifelse([$1],,[no],[$1])))
-    ax_enable_debug_is_release=m4_tolower(m4_normalize(ifelse([$4],,
-                                                              [$ax_is_release],
-                                                              [$4])))
-
-    # If this is a release, override the default.
-    AS_IF([test "$ax_enable_debug_is_release" = "yes"],
-      [ax_enable_debug_default="no"])
-
-    m4_define(ax_enable_debug_vars,[m4_normalize(ifelse([$2],,,[$2]))])
-    m4_define(ax_disable_debug_vars,[m4_normalize(ifelse([$3],,[NDEBUG],[$3]))])
-
-    AC_ARG_ENABLE(debug,
-        [AS_HELP_STRING([--enable-debug=]@<:@yes/info/profile/no@:>@,[compile with debugging])],
-        [],enable_debug=$ax_enable_debug_default)
-
-    # empty mean debug yes
-    AS_IF([test "x$enable_debug" = "x"],
-      [enable_debug="yes"])
-
-    # case of debug
-    AS_CASE([$enable_debug],
-      [yes],[
-        AC_MSG_RESULT(yes)
-        CFLAGS="${CFLAGS} -g -O0"
-        CXXFLAGS="${CXXFLAGS} -g -O0"
-        FFLAGS="${FFLAGS} -g -O0"
-        FCFLAGS="${FCFLAGS} -g -O0"
-        OBJCFLAGS="${OBJCFLAGS} -g -O0"
-      ],
-      [info],[
-        AC_MSG_RESULT(info)
-        CFLAGS="${CFLAGS} -g"
-        CXXFLAGS="${CXXFLAGS} -g"
-        FFLAGS="${FFLAGS} -g"
-        FCFLAGS="${FCFLAGS} -g"
-        OBJCFLAGS="${OBJCFLAGS} -g"
-      ],
-      [profile],[
-        AC_MSG_RESULT(profile)
-        CFLAGS="${CFLAGS} -g -pg"
-        CXXFLAGS="${CXXFLAGS} -g -pg"
-        FFLAGS="${FFLAGS} -g -pg"
-        FCFLAGS="${FCFLAGS} -g -pg"
-        OBJCFLAGS="${OBJCFLAGS} -g -pg"
-        LDFLAGS="${LDFLAGS} -pg"
-      ],
-      [
-        AC_MSG_RESULT(no)
-        dnl Ensure AC_PROG_CC/CXX/F77/FC/OBJC will not enable debug flags
-        dnl by setting any unset environment flag variables
-        AS_IF([test "x${CFLAGS+set}" != "xset"],
-          [CFLAGS=""])
-        AS_IF([test "x${CXXFLAGS+set}" != "xset"],
-          [CXXFLAGS=""])
-        AS_IF([test "x${FFLAGS+set}" != "xset"],
-          [FFLAGS=""])
-        AS_IF([test "x${FCFLAGS+set}" != "xset"],
-          [FCFLAGS=""])
-        AS_IF([test "x${OBJCFLAGS+set}" != "xset"],
-          [OBJCFLAGS=""])
-      ])
-
-    dnl Define various variables if debugging is disabled.
-    dnl assert.h is a NOP if NDEBUG is defined, so define it by default.
-    AS_IF([test "x$enable_debug" = "xyes"],
-      [m4_map_args_w(ax_enable_debug_vars, [AC_DEFINE(], [,[1],[Define if debugging is enabled])])],
-      [m4_map_args_w(ax_disable_debug_vars, [AC_DEFINE(], [,[1],[Define if debugging is disabled])])])
-    ax_enable_debug=$enable_debug
-])
-
 # Copyright (C) 2002-2021 Free Software Foundation, Inc.
 #
 # This file is free software; the Free Software Foundation

configure
@@ -792,7 +792,6 @@ ac_user_opts='
 enable_option_checking
 enable_silent_rules
 enable_maintainer_mode
-enable_debug
 enable_dependency_tracking
 enable_shared
 enable_static
@@ -1444,8 +1443,6 @@ Optional Features:
   --enable-maintainer-mode
                           enable make rules and dependencies not useful (and
                           sometimes confusing) to the casual installer
-  --enable-debug=[yes/info/profile/no]
-                          compile with debugging
   --enable-dependency-tracking
                           do not reject slow dependency extractors
   --disable-dependency-tracking
@@ -3280,108 +3277,6 @@ else $as_nop
 fi
 
 
-    { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether to enable debugging" >&5
-printf %s "checking whether to enable debugging... " >&6; }
-
-    ax_enable_debug_default=yes
-    ax_enable_debug_is_release=$ax_is_release
-
-    # If this is a release, override the default.
-    if test "$ax_enable_debug_is_release" = "yes"
-then :
-  ax_enable_debug_default="no"
-fi
-
-
-
-
-    # Check whether --enable-debug was given.
-if test ${enable_debug+y}
-then :
-  enableval=$enable_debug;
-else $as_nop
-  enable_debug=$ax_enable_debug_default
-fi
-
-
-    # empty mean debug yes
-    if test "x$enable_debug" = "x"
-then :
-  enable_debug="yes"
-fi
-
-    # case of debug
-    case $enable_debug in #(
-  yes) :
-
-        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: yes" >&5
-printf "%s\n" "yes" >&6; }
-        CFLAGS="${CFLAGS} -g -O0"
-        CXXFLAGS="${CXXFLAGS} -g -O0"
-        FFLAGS="${FFLAGS} -g -O0"
-        FCFLAGS="${FCFLAGS} -g -O0"
-        OBJCFLAGS="${OBJCFLAGS} -g -O0"
-       ;; #(
-  info) :
-
-        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: info" >&5
-printf "%s\n" "info" >&6; }
-        CFLAGS="${CFLAGS} -g"
-        CXXFLAGS="${CXXFLAGS} -g"
-        FFLAGS="${FFLAGS} -g"
-        FCFLAGS="${FCFLAGS} -g"
-        OBJCFLAGS="${OBJCFLAGS} -g"
-       ;; #(
-  profile) :
-
-        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: profile" >&5
-printf "%s\n" "profile" >&6; }
-        CFLAGS="${CFLAGS} -g -pg"
-        CXXFLAGS="${CXXFLAGS} -g -pg"
-        FFLAGS="${FFLAGS} -g -pg"
-        FCFLAGS="${FCFLAGS} -g -pg"
-        OBJCFLAGS="${OBJCFLAGS} -g -pg"
-        LDFLAGS="${LDFLAGS} -pg"
-       ;; #(
-  *) :
-
-        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no" >&5
-printf "%s\n" "no" >&6; }
-                        if test "x${CFLAGS+set}" != "xset"
-then :
-  CFLAGS=""
-fi
-        if test "x${CXXFLAGS+set}" != "xset"
-then :
-  CXXFLAGS=""
-fi
-        if test "x${FFLAGS+set}" != "xset"
-then :
-  FFLAGS=""
-fi
-        if test "x${FCFLAGS+set}" != "xset"
-then :
-  FCFLAGS=""
-fi
-        if test "x${OBJCFLAGS+set}" != "xset"
-then :
-  OBJCFLAGS=""
-fi
-       ;;
-esac
-
-            if test "x$enable_debug" = "xyes"
-then :
-
-else $as_nop
-
-printf "%s\n" "#define NDEBUG 1" >>confdefs.h
-
-fi
-    ax_enable_debug=$enable_debug
-
-
-
 
 
 
@@ -4315,45 +4210,6 @@ fi
 
 
 AM_CFLAGS=
-
-{ printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether C compiler accepts -Wno-format-truncation" >&5
-printf %s "checking whether C compiler accepts -Wno-format-truncation... " >&6; }
-if test ${ax_cv_check_cflags__Werror__Wno_format_truncation+y}
-then :
-  printf %s "(cached) " >&6
-else $as_nop
-
-  ax_check_save_flags=$CFLAGS
-  CFLAGS="$CFLAGS -Werror -Wno-format-truncation"
-  cat confdefs.h - <<_ACEOF >conftest.$ac_ext
-/* end confdefs.h.  */
-
-int
-main (void)
-{
-
-  ;
-  return 0;
-}
-_ACEOF
-if ac_fn_c_try_compile "$LINENO"
-then :
-  ax_cv_check_cflags__Werror__Wno_format_truncation=yes
-else $as_nop
-  ax_cv_check_cflags__Werror__Wno_format_truncation=no
-fi
-rm -f core conftest.err conftest.$ac_objext conftest.beam conftest.$ac_ext
-  CFLAGS=$ax_check_save_flags
-fi
-{ printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: $ax_cv_check_cflags__Werror__Wno_format_truncation" >&5
-printf "%s\n" "$ax_cv_check_cflags__Werror__Wno_format_truncation" >&6; }
-if test "x$ax_cv_check_cflags__Werror__Wno_format_truncation" = xyes
-then :
-  AM_CFLAGS="$AM_CFLAGS -Wno-format-truncation"
-else $as_nop
-  :
-fi
-
 
 
 case `pwd` in
@@ -6097,6 +5953,7 @@ fi
 printf "%s\n" "$lt_cv_sharedlib_from_linklib_cmd" >&6; }
 sharedlib_from_linklib_cmd=$lt_cv_sharedlib_from_linklib_cmd
 test -z "$sharedlib_from_linklib_cmd" && sharedlib_from_linklib_cmd=$ECHO
+
 
 
 

configure.ac
@@ -9,13 +9,9 @@ AC_CONFIG_HEADER(src/config.h)
 AM_MAINTAINER_MODE([disable])
 AS_IF([test "$USE_MAINTAINER_MODE" != "no"],
       [ax_is_release=no],[ax_is_release=yes])
-AX_CHECK_ENABLE_DEBUG([yes])
 
 AC_PROG_CC([cc clang gcc])
 AM_CFLAGS=
-AX_CHECK_COMPILE_FLAG([-Wno-format-truncation],
-                      [AM_CFLAGS="$AM_CFLAGS -Wno-format-truncation"],
-                      [], [-Werror])
 AC_SUBST([AM_CFLAGS])
 
 LT_INIT

src/config.h.in
@@ -88,9 +88,6 @@
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
 #undef LT_OBJDIR
 
-/* Define if debugging is disabled */
-#undef NDEBUG
-
 /* Name of package */
 #undef PACKAGE
 

.gitignore
@@ -19,7 +19,6 @@ libtool
 /cpdup
 /dirempty
 /dirwatch
-/jexecd
 /locked_mkdir
 /make_index
 /nc
@@ -29,7 +28,6 @@ libtool
 /ptsort
 /pwait
 /rename
-/rexec
 /rm
 /setsid
 /sh

Makefile.am
@@ -151,14 +151,12 @@ pkglibexec_PROGRAMS= \
 		     cpdup \
 		     dirempty \
 		     dirwatch \
-		     jexecd \
 		     locked_mkdir \
 		     nc \
 		     poudriered \
 		     ptsort \
 		     pwait \
 		     rename \
-		     rexec \
 		     @USE_RM@ \
 		     setsid \
 		     timeout \
@@ -319,17 +317,6 @@ poudriered_CFLAGS=	$(AM_CFLAGS) \
 poudriered_LDADD=	libucl.la \
 			-lsbuf \
 			-lutil
-
-jexecd_SOURCES=	src/libexec/poudriere/jexecd/jexecd.c
-jexecd_CFLAGS=	$(AM_CFLAGS) -I$(top_srcdir)/external/libnv/sys
-jexecd_LDADD=	libnv.la \
-		-lsbuf \
-		-lutil \
-		-ljail
-
-rexec_SOURCES=	src/libexec/poudriere/rexec/rexec.c
-rexec_CFLAGS=	$(AM_CFLAGS) -I$(top_srcdir)/external/libnv/sys
-rexec_LDADD=	libnv.la
 
 cached_SOURCES=	src/libexec/poudriere/cached/cached.c \
 		external/klib/khash.h

commit 3d1cb9fadabde866d2b3cdc1812147bfa2511958

Restore code improperly deleted in "jexecd: remove feature"
Fixes: 397973f1fb41eede01ad154059b8efd9e4c32e87

aclocal.m4
@@ -20,6 +20,185 @@ You have another version of autoconf.  It may work, but is not guaranteed to.
 If you have problems, you may need to regenerate the build system entirely.
 To do so, use the procedure documented by the package, typically 'autoreconf'.])])
 
+# ===========================================================================
+#  https://www.gnu.org/software/autoconf-archive/ax_check_compile_flag.html
+# ===========================================================================
+#
+# SYNOPSIS
+#
+#   AX_CHECK_COMPILE_FLAG(FLAG, [ACTION-SUCCESS], [ACTION-FAILURE], [EXTRA-FLAGS], [INPUT])
+#
+# DESCRIPTION
+#
+#   Check whether the given FLAG works with the current language's compiler
+#   or gives an error.  (Warnings, however, are ignored)
+#
+#   ACTION-SUCCESS/ACTION-FAILURE are shell commands to execute on
+#   success/failure.
+#
+#   If EXTRA-FLAGS is defined, it is added to the current language's default
+#   flags (e.g. CFLAGS) when the check is done.  The check is thus made with
+#   the flags: "CFLAGS EXTRA-FLAGS FLAG".  This can for example be used to
+#   force the compiler to issue an error when a bad flag is given.
+#
+#   INPUT gives an alternative input source to AC_COMPILE_IFELSE.
+#
+#   NOTE: Implementation based on AX_CFLAGS_GCC_OPTION. Please keep this
+#   macro in sync with AX_CHECK_{PREPROC,LINK}_FLAG.
+#
+# LICENSE
+#
+#   Copyright (c) 2008 Guido U. Draheim <guidod@gmx.de>
+#   Copyright (c) 2011 Maarten Bosmans <mkbosmans@gmail.com>
+#
+#   Copying and distribution of this file, with or without modification, are
+#   permitted in any medium without royalty provided the copyright notice
+#   and this notice are preserved.  This file is offered as-is, without any
+#   warranty.
+
+#serial 6
+
+AC_DEFUN([AX_CHECK_COMPILE_FLAG],
+[AC_PREREQ(2.64)dnl for _AC_LANG_PREFIX and AS_VAR_IF
+AS_VAR_PUSHDEF([CACHEVAR],[ax_cv_check_[]_AC_LANG_ABBREV[]flags_$4_$1])dnl
+AC_CACHE_CHECK([whether _AC_LANG compiler accepts $1], CACHEVAR, [
+  ax_check_save_flags=$[]_AC_LANG_PREFIX[]FLAGS
+  _AC_LANG_PREFIX[]FLAGS="$[]_AC_LANG_PREFIX[]FLAGS $4 $1"
+  AC_COMPILE_IFELSE([m4_default([$5],[AC_LANG_PROGRAM()])],
+    [AS_VAR_SET(CACHEVAR,[yes])],
+    [AS_VAR_SET(CACHEVAR,[no])])
+  _AC_LANG_PREFIX[]FLAGS=$ax_check_save_flags])
+AS_VAR_IF(CACHEVAR,yes,
+  [m4_default([$2], :)],
+  [m4_default([$3], :)])
+AS_VAR_POPDEF([CACHEVAR])dnl
+])dnl AX_CHECK_COMPILE_FLAGS
+
+# ===========================================================================
+#  https://www.gnu.org/software/autoconf-archive/ax_check_enable_debug.html
+# ===========================================================================
+#
+# SYNOPSIS
+#
+#   AX_CHECK_ENABLE_DEBUG([enable by default=yes/info/profile/no], [ENABLE DEBUG VARIABLES ...], [DISABLE DEBUG VARIABLES NDEBUG ...], [IS-RELEASE])
+#
+# DESCRIPTION
+#
+#   Check for the presence of an --enable-debug option to configure, with
+#   the specified default value used when the option is not present.  Return
+#   the value in the variable $ax_enable_debug.
+#
+#   Specifying 'yes' adds '-g -O0' to the compilation flags for all
+#   languages. Specifying 'info' adds '-g' to the compilation flags.
+#   Specifying 'profile' adds '-g -pg' to the compilation flags and '-pg' to
+#   the linking flags. Otherwise, nothing is added.
+#
+#   Define the variables listed in the second argument if debug is enabled,
+#   defaulting to no variables.  Defines the variables listed in the third
+#   argument if debug is disabled, defaulting to NDEBUG.  All lists of
+#   variables should be space-separated.
+#
+#   If debug is not enabled, ensure AC_PROG_* will not add debugging flags.
+#   Should be invoked prior to any AC_PROG_* compiler checks.
+#
+#   IS-RELEASE can be used to change the default to 'no' when making a
+#   release.  Set IS-RELEASE to 'yes' or 'no' as appropriate. By default, it
+#   uses the value of $ax_is_release, so if you are using the AX_IS_RELEASE
+#   macro, there is no need to pass this parameter.
+#
+#     AX_IS_RELEASE([git-directory])
+#     AX_CHECK_ENABLE_DEBUG()
+#
+# LICENSE
+#
+#   Copyright (c) 2011 Rhys Ulerich <rhys.ulerich@gmail.com>
+#   Copyright (c) 2014, 2015 Philip Withnall <philip@tecnocode.co.uk>
+#
+#   Copying and distribution of this file, with or without modification, are
+#   permitted in any medium without royalty provided the copyright notice
+#   and this notice are preserved.
+
+#serial 9
+
+AC_DEFUN([AX_CHECK_ENABLE_DEBUG],[
+    AC_BEFORE([$0],[AC_PROG_CC])dnl
+    AC_BEFORE([$0],[AC_PROG_CXX])dnl
+    AC_BEFORE([$0],[AC_PROG_F77])dnl
+    AC_BEFORE([$0],[AC_PROG_FC])dnl
+
+    AC_MSG_CHECKING(whether to enable debugging)
+
+    ax_enable_debug_default=m4_tolower(m4_normalize(ifelse([$1],,[no],[$1])))
+    ax_enable_debug_is_release=m4_tolower(m4_normalize(ifelse([$4],,
+                                                              [$ax_is_release],
+                                                              [$4])))
+
+    # If this is a release, override the default.
+    AS_IF([test "$ax_enable_debug_is_release" = "yes"],
+      [ax_enable_debug_default="no"])
+
+    m4_define(ax_enable_debug_vars,[m4_normalize(ifelse([$2],,,[$2]))])
+    m4_define(ax_disable_debug_vars,[m4_normalize(ifelse([$3],,[NDEBUG],[$3]))])
+
+    AC_ARG_ENABLE(debug,
+        [AS_HELP_STRING([--enable-debug=]@<:@yes/info/profile/no@:>@,[compile with debugging])],
+        [],enable_debug=$ax_enable_debug_default)
+
+    # empty mean debug yes
+    AS_IF([test "x$enable_debug" = "x"],
+      [enable_debug="yes"])
+
+    # case of debug
+    AS_CASE([$enable_debug],
+      [yes],[
+        AC_MSG_RESULT(yes)
+        CFLAGS="${CFLAGS} -g -O0"
+        CXXFLAGS="${CXXFLAGS} -g -O0"
+        FFLAGS="${FFLAGS} -g -O0"
+        FCFLAGS="${FCFLAGS} -g -O0"
+        OBJCFLAGS="${OBJCFLAGS} -g -O0"
+      ],
+      [info],[
+        AC_MSG_RESULT(info)
+        CFLAGS="${CFLAGS} -g"
+        CXXFLAGS="${CXXFLAGS} -g"
+        FFLAGS="${FFLAGS} -g"
+        FCFLAGS="${FCFLAGS} -g"
+        OBJCFLAGS="${OBJCFLAGS} -g"
+      ],
+      [profile],[
+        AC_MSG_RESULT(profile)
+        CFLAGS="${CFLAGS} -g -pg"
+        CXXFLAGS="${CXXFLAGS} -g -pg"
+        FFLAGS="${FFLAGS} -g -pg"
+        FCFLAGS="${FCFLAGS} -g -pg"
+        OBJCFLAGS="${OBJCFLAGS} -g -pg"
+        LDFLAGS="${LDFLAGS} -pg"
+      ],
+      [
+        AC_MSG_RESULT(no)
+        dnl Ensure AC_PROG_CC/CXX/F77/FC/OBJC will not enable debug flags
+        dnl by setting any unset environment flag variables
+        AS_IF([test "x${CFLAGS+set}" != "xset"],
+          [CFLAGS=""])
+        AS_IF([test "x${CXXFLAGS+set}" != "xset"],
+          [CXXFLAGS=""])
+        AS_IF([test "x${FFLAGS+set}" != "xset"],
+          [FFLAGS=""])
+        AS_IF([test "x${FCFLAGS+set}" != "xset"],
+          [FCFLAGS=""])
+        AS_IF([test "x${OBJCFLAGS+set}" != "xset"],
+          [OBJCFLAGS=""])
+      ])
+
+    dnl Define various variables if debugging is disabled.
+    dnl assert.h is a NOP if NDEBUG is defined, so define it by default.
+    AS_IF([test "x$enable_debug" = "xyes"],
+      [m4_map_args_w(ax_enable_debug_vars, [AC_DEFINE(], [,[1],[Define if debugging is enabled])])],
+      [m4_map_args_w(ax_disable_debug_vars, [AC_DEFINE(], [,[1],[Define if debugging is disabled])])])
+    ax_enable_debug=$enable_debug
+])
+
 # Copyright (C) 2002-2021 Free Software Foundation, Inc.
 #
 # This file is free software; the Free Software Foundation

configure
@@ -792,6 +792,7 @@ ac_user_opts='
 enable_option_checking
 enable_silent_rules
 enable_maintainer_mode
+enable_debug
 enable_dependency_tracking
 enable_shared
 enable_static
@@ -1443,6 +1444,8 @@ Optional Features:
   --enable-maintainer-mode
                           enable make rules and dependencies not useful (and
                           sometimes confusing) to the casual installer
+  --enable-debug=[yes/info/profile/no]
+                          compile with debugging
   --enable-dependency-tracking
                           do not reject slow dependency extractors
   --disable-dependency-tracking
@@ -3277,6 +3280,108 @@ else $as_nop
 fi
 
 
+    { printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether to enable debugging" >&5
+printf %s "checking whether to enable debugging... " >&6; }
+
+    ax_enable_debug_default=yes
+    ax_enable_debug_is_release=$ax_is_release
+
+    # If this is a release, override the default.
+    if test "$ax_enable_debug_is_release" = "yes"
+then :
+  ax_enable_debug_default="no"
+fi
+
+
+
+
+    # Check whether --enable-debug was given.
+if test ${enable_debug+y}
+then :
+  enableval=$enable_debug;
+else $as_nop
+  enable_debug=$ax_enable_debug_default
+fi
+
+
+    # empty mean debug yes
+    if test "x$enable_debug" = "x"
+then :
+  enable_debug="yes"
+fi
+
+    # case of debug
+    case $enable_debug in #(
+  yes) :
+
+        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+printf "%s\n" "yes" >&6; }
+        CFLAGS="${CFLAGS} -g -O0"
+        CXXFLAGS="${CXXFLAGS} -g -O0"
+        FFLAGS="${FFLAGS} -g -O0"
+        FCFLAGS="${FCFLAGS} -g -O0"
+        OBJCFLAGS="${OBJCFLAGS} -g -O0"
+       ;; #(
+  info) :
+
+        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: info" >&5
+printf "%s\n" "info" >&6; }
+        CFLAGS="${CFLAGS} -g"
+        CXXFLAGS="${CXXFLAGS} -g"
+        FFLAGS="${FFLAGS} -g"
+        FCFLAGS="${FCFLAGS} -g"
+        OBJCFLAGS="${OBJCFLAGS} -g"
+       ;; #(
+  profile) :
+
+        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: profile" >&5
+printf "%s\n" "profile" >&6; }
+        CFLAGS="${CFLAGS} -g -pg"
+        CXXFLAGS="${CXXFLAGS} -g -pg"
+        FFLAGS="${FFLAGS} -g -pg"
+        FCFLAGS="${FCFLAGS} -g -pg"
+        OBJCFLAGS="${OBJCFLAGS} -g -pg"
+        LDFLAGS="${LDFLAGS} -pg"
+       ;; #(
+  *) :
+
+        { printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: no" >&5
+printf "%s\n" "no" >&6; }
+                        if test "x${CFLAGS+set}" != "xset"
+then :
+  CFLAGS=""
+fi
+        if test "x${CXXFLAGS+set}" != "xset"
+then :
+  CXXFLAGS=""
+fi
+        if test "x${FFLAGS+set}" != "xset"
+then :
+  FFLAGS=""
+fi
+        if test "x${FCFLAGS+set}" != "xset"
+then :
+  FCFLAGS=""
+fi
+        if test "x${OBJCFLAGS+set}" != "xset"
+then :
+  OBJCFLAGS=""
+fi
+       ;;
+esac
+
+            if test "x$enable_debug" = "xyes"
+then :
+
+else $as_nop
+
+printf "%s\n" "#define NDEBUG 1" >>confdefs.h
+
+fi
+    ax_enable_debug=$enable_debug
+
+
+
 
 
 
@@ -4210,6 +4315,45 @@ fi
 
 
 AM_CFLAGS=
+
+{ printf "%s\n" "$as_me:${as_lineno-$LINENO}: checking whether C compiler accepts -Wno-format-truncation" >&5
+printf %s "checking whether C compiler accepts -Wno-format-truncation... " >&6; }
+if test ${ax_cv_check_cflags__Werror__Wno_format_truncation+y}
+then :
+  printf %s "(cached) " >&6
+else $as_nop
+
+  ax_check_save_flags=$CFLAGS
+  CFLAGS="$CFLAGS -Werror -Wno-format-truncation"
+  cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+int
+main (void)
+{
+
+  ;
+  return 0;
+}
+_ACEOF
+if ac_fn_c_try_compile "$LINENO"
+then :
+  ax_cv_check_cflags__Werror__Wno_format_truncation=yes
+else $as_nop
+  ax_cv_check_cflags__Werror__Wno_format_truncation=no
+fi
+rm -f core conftest.err conftest.$ac_objext conftest.beam conftest.$ac_ext
+  CFLAGS=$ax_check_save_flags
+fi
+{ printf "%s\n" "$as_me:${as_lineno-$LINENO}: result: $ax_cv_check_cflags__Werror__Wno_format_truncation" >&5
+printf "%s\n" "$ax_cv_check_cflags__Werror__Wno_format_truncation" >&6; }
+if test "x$ax_cv_check_cflags__Werror__Wno_format_truncation" = xyes
+then :
+  AM_CFLAGS="$AM_CFLAGS -Wno-format-truncation"
+else $as_nop
+  :
+fi
+
 
 
 case `pwd` in
@@ -5953,7 +6097,6 @@ fi
 printf "%s\n" "$lt_cv_sharedlib_from_linklib_cmd" >&6; }
 sharedlib_from_linklib_cmd=$lt_cv_sharedlib_from_linklib_cmd
 test -z "$sharedlib_from_linklib_cmd" && sharedlib_from_linklib_cmd=$ECHO
-
 
 
 

configure.ac
@@ -9,9 +9,13 @@ AC_CONFIG_HEADERS([src/config.h])
 AM_MAINTAINER_MODE([disable])
 AS_IF([test "$USE_MAINTAINER_MODE" != "no"],
       [ax_is_release=no],[ax_is_release=yes])
+AX_CHECK_ENABLE_DEBUG([yes])
 
 AC_PROG_CC([cc clang gcc])
 AM_CFLAGS=
+AX_CHECK_COMPILE_FLAG([-Wno-format-truncation],
+                      [AM_CFLAGS="$AM_CFLAGS -Wno-format-truncation"],
+                      [], [-Werror])
 AC_SUBST([AM_CFLAGS])
 
 LT_INIT

src/config.h.in
@@ -88,6 +88,9 @@
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
 #undef LT_OBJDIR
 
+/* Define if debugging is disabled */
+#undef NDEBUG
+
 /* Name of package */
 #undef PACKAGE
 