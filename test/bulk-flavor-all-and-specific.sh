freebsd/poudriere

commit 26bfbed74772659e07f69cb20dae05929019dbbe

gather_port_vars: No need for -p to some mkdir here

src/share/poudriere/common.sh
@@ -6612,7 +6612,8 @@ gather_port_vars() {
 			# Don't worry about duplicates from user list.
 			qdir="fqueue/${originspec%/*}!${originspec#*/}"
 			msg_debug "queueing ${COLOR_PORT}${originspec}${COLOR_RESET} into flavorqueue (rdep=${COLOR_PORT}${rdep}${COLOR_RESET})"
-			mkdir -p "${qdir}"
+			mkdir "${qdir}" ||
+			    err 1 "gather_port_vars: Failed to add ${COLOR_PORT}${originspec}${COLOR_RESET} into flavorqueue (rdep=${COLOR_PORT}${rdep}${COLOR_RESET})"
 			echo "${rdep}" > "${qdir}/rdep"
 
 			# Testport already looked up the main FLAVOR
@@ -6959,8 +6960,8 @@ gather_port_vars_port() {
 			originspec_encode dep_originspec "${origin}" "${dep_flavor}" "${origin_subpkg}"
 			msg_debug "gather_port_vars_port (${COLOR_PORT}${originspec}${COLOR_RESET}): Adding to flavorqueue FLAVOR=${dep_flavor}"
 			qdir="fqueue/${dep_originspec%/*}!${dep_originspec#*/}"
-			mkdir -p "${qdir}" || \
-				err 1 "gather_port_vars_port: Failed to add ${dep_originspec} to flavorqueue"
+			mkdir "${qdir}" ||
+			    err 1 "gather_port_vars_port: Failed to add ${COLOR_PORT}${dep_originspec}${COLOR_RESET} to flavorqueue for ${COLOR_PORT}${originspec}${COLOR_RESET}"
 			# Copy our own reverse dep over.  This should always
 			# just be "listed" in this case ($rdep == listed) but
 			# use the actual value to reduce maintenance.
@@ -7038,6 +7039,7 @@ gather_port_vars_process_depqueue_enqueue() {
 
 	msg_debug "gather_port_vars_process_depqueue_enqueue (${COLOR_PORT}${originspec}${COLOR_RESET}): Adding ${COLOR_PORT}${dep_originspec}${COLOR_RESET} into the ${queue} (rdep=${COLOR_PORT}${rdep}${COLOR_RESET})"
 	# Another worker may have created it
+	# mkdir -p is not used here as it would waste time touching $queue/
 	qdir="${queue}/${dep_originspec%/*}!${dep_originspec#*/}"
 	if mkdir "${qdir}" 2>&${fd_devnull}; then
 		echo "${rdep}" > "${qdir}/rdep"

commit 4ca881e06c33f85870127a57f9457ae6c1b69d74

Fix error in gather_port_vars_port after 26bfbed7477
This happens when a port has port@all and port@flavor both listed to
build.

Reported by:	manu
Fixes: 26bfbed7477 (gather_port_vars: No need for -p to some mkdir here)

src/share/poudriere/common.sh
@@ -7011,7 +7011,7 @@ gather_port_vars_port() {
 			originspec_encode dep_originspec "${origin}" "${dep_flavor}" "${origin_subpkg}"
 			msg_debug "gather_port_vars_port (${COLOR_PORT}${originspec}${COLOR_RESET}): Adding to flavorqueue FLAVOR=${dep_flavor}"
 			qdir="fqueue/${dep_originspec%/*}!${dep_originspec#*/}"
-			mkdir "${qdir}" ||
+			mkdir -p "${qdir}" ||
 			    err 1 "gather_port_vars_port: Failed to add ${COLOR_PORT}${dep_originspec}${COLOR_RESET} to flavorqueue for ${COLOR_PORT}${originspec}${COLOR_RESET}"
 			# Copy our own reverse dep over.  This should always
 			# just be "listed" in this case ($rdep == listed) but

test/Makefile.am
@@ -55,6 +55,7 @@ JAIL_TESTS= \
 	bulk-build-self-immutable-base-schg.sh \
 	bulk-build-self-immutable-base-yes.sh \
 	bulk-flavor-FLAVORS-misordered.sh \
+	bulk-flavor-all-and-specific.sh \
 	bulk-flavor-default-dep-and-specific-listed.sh \
 	bulk-flavor-default-dep.sh \
 	bulk-flavor-ignore-all.sh \

test/Makefile.in
@@ -482,6 +482,7 @@ JAIL_TESTS = \
 	bulk-build-self-immutable-base-schg.sh \
 	bulk-build-self-immutable-base-yes.sh \
 	bulk-flavor-FLAVORS-misordered.sh \
+	bulk-flavor-all-and-specific.sh \
 	bulk-flavor-default-dep-and-specific-listed.sh \
 	bulk-flavor-default-dep.sh \
 	bulk-flavor-ignore-all.sh \
@@ -989,6 +990,13 @@ bulk-build-self-immutable-base-yes.sh.log: bulk-build-self-immutable-base-yes.sh
 bulk-flavor-FLAVORS-misordered.sh.log: bulk-flavor-FLAVORS-misordered.sh
 	@p='bulk-flavor-FLAVORS-misordered.sh'; \
 	b='bulk-flavor-FLAVORS-misordered.sh'; \
+	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+	--log-file $$b.log --trs-file $$b.trs \
+	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+	"$$tst" $(AM_TESTS_FD_REDIRECT)
+bulk-flavor-all-and-specific.sh.log: bulk-flavor-all-and-specific.sh
+	@p='bulk-flavor-all-and-specific.sh'; \
+	b='bulk-flavor-all-and-specific.sh'; \
 	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
 	--log-file $$b.log --trs-file $$b.trs \
 	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \

test/bulk-flavor-all-and-specific.sh
@@ -1,0 +1,11 @@
+LISTPORTS="misc/freebsd-release-manifests@all misc/freebsd-release-manifests@foo"
+OVERLAYS="omnibus"
+. common.bulk.sh
+
+do_bulk -n ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+
+EXPECTED_QUEUED="misc/foo misc/freebsd-release-manifests misc/freebsd-release-manifests@bar misc/freebsd-release-manifests@foo ports-mgmt/pkg"
+EXPECTED_LISTED="misc/freebsd-release-manifests misc/freebsd-release-manifests@bar misc/freebsd-release-manifests@foo"
+
+assert_bulk_queue_and_stats