freebsd/poudriere

commit 06b67be81c01b97ff6663bdaf3fddc2c5681b10a

bulk -f: Do not process dependencies of IGNORED ports.
This avoids the need to do the TRIM_ORPHANED_BUILD_DEPS trimming of
ignored dependencies which are no longer needed.  It does not remove
the TRIM_ORPHANED_BUILD_DEPS use later after incremental package deletions.

src/share/poudriere/common.sh
@@ -6619,6 +6619,7 @@ gather_port_vars_port() {
 	fi
 
 	# If there are no deps for this port then there's nothing left to do.
+	[ -n "${ignore}" ] && return 0
 	[ -z "${deps}" ] && return 0
 
 	# Assert some policy before proceeding to process these deps
@@ -6810,6 +6811,10 @@ compute_deps_pkg() {
 	shash_remove pkgname-deps "${pkgname}" deps || \
 	    err 1 "compute_deps_pkg failed to find deps for ${pkgname}"
 
+	if shash_exists pkgname-ignore "${pkgname}"; then
+		msg_debug "compute_deps_pkg: Will not build IGNORED ${COLOR_PORT}${pkgname}${COLOR_RESET} nor queue its deps"
+		return
+	fi
 	msg_debug "compute_deps_pkg: Will build ${pkgname}"
 	pkgqueue_add "${pkgname}" || \
 	    err 1 "compute_deps_pkg: Error creating queue entry for ${pkgname}: There may be a duplicate origin in a category Makefile"
@@ -7467,7 +7472,6 @@ trim_ignored() {
 	    _rdep ignore; do
 		trim_ignored_pkg "${pkgname}" "${originspec}" "${ignore}"
 	done
-	pkgqueue_trim_orphaned_build_deps
 	# Update ignored/skipped stats
 	update_stats 2>/dev/null || :
 	update_stats_queued

test/bulk-ignore-and-skipped.sh
@@ -7,6 +7,9 @@ do_bulk -n ${LISTPORTS}
 assert 0 $? "Bulk should pass"
 
 EXPECTED_LISTPORTS_IGNORED="ports-mgmt/poudriere-devel-IGNORED-and-skipped"
-EXPECTED_IGNORED="ports-mgmt/poudriere-devel-IGNORED ports-mgmt/poudriere-devel-IGNORED-and-skipped"
+# ports-mgmt/poudriere-devel-IGNORED is a dependency which is also ignored but
+# because we are ignoring ports-mgmt/poudriere-devel-IGNORED-and-skipped we
+# should not bother processing ports-mgmt/poudriere-devel-IGNORED at all.
+EXPECTED_IGNORED="ports-mgmt/poudriere-devel-IGNORED-and-skipped"
 EXPECTED_SKIPPED=
 assert_bulk_queue_and_stats

test/common.bulk.sh
@@ -65,6 +65,7 @@ cache_pkgnames() {
 	ALL_ORIGINS="${ALL_ORIGINS}${ALL_ORIGINS:+ }${originspec}"
 	if [ -n "${ignore}" ]; then
 		list_add IGNOREDPORTS "${originspec}"
+		return
 	fi
 	for dep_origin in ${pdeps}; do
 		if cache_pkgnames 1 "${dep_origin}"; then

commit 44f7f1a04184e4575cf78b53e98e6ef3856b6fa0

prepare_ports: Reduce work done when not building which fixes a pkgclean bug.
- pkgclean: Fix crash with IGNORED port handling.
  Commit 06b67be81c01b97ff broke this. Commit d92b679189a has
  more context.

src/share/poudriere/common.sh
@@ -7115,6 +7115,9 @@ gather_port_vars() {
 	# are visited in the gatherqueue for *their default* originspec
 	# before processing any dependencies.
 
+	load_moved
+	load_blacklist "${JAILNAME:?}" "${PTNAME:?}" "${SETNAME-}"
+
 	msg "Gathering ports metadata"
 	bset status "gatheringportvars:"
 	run_hook gather_port_vars start
@@ -8416,7 +8419,7 @@ trim_ignored_pkg() {
 prepare_ports() {
 	local pkg
 	local log log_top log_jail
-	local n resuming_build
+	local resuming_build
 	local cache_dir sflag delete_pkg_list shash_bucket
 
 	cd "${MASTER_DATADIR:?}"
@@ -8491,9 +8494,6 @@ prepare_ports() {
 		fi
 	fi
 
-	load_moved
-	load_blacklist "${JAILNAME}" "${PTNAME}" "${SETNAME}"
-
 	fetch_global_port_vars || \
 	    err 1 "Failed to lookup global ports metadata"
 
@@ -8507,11 +8507,12 @@ prepare_ports() {
 
 	gather_port_vars
 
-	generate_queue
-
-	bset status "sanity:"
-
 	if was_a_bulk_run; then
+		generate_queue
+
+		bset status "sanity:"
+		msg "Sanity checking the repository"
+
 		# Migrate packages to new sufx
 		maybe_migrate_packages
 		# Stash dependency graph
@@ -8613,27 +8614,9 @@ prepare_ports() {
 			delete_all_pkgs "pkg bootstrap missing: unable to inspect existing packages"
 		fi
 		bset status "sanity:"
-	fi
 
-	msg "Sanity checking the repository"
+		delete_bad_pkg_repo_files
 
-	for n in \
-	    meta.${PKG_EXT} meta.txz \
-	    digests.${PKG_EXT} digests.txz \
-	    filesite.${PKG_EXT} filesite.txz \
-	    packagesite.${PKG_EXT} packagesite.txz; do
-		pkg="${PACKAGES:?}/All/${n}"
-		if [ -f "${pkg}" ]; then
-			msg "Removing invalid pkg repo file: ${pkg}"
-			unlink "${pkg}"
-		fi
-
-	done
-
-	delete_stale_pkg_cache
-
-	# Skip incremental build for pkgclean
-	if was_a_bulk_run; then
 		install -lsr "${log:?}" "${PACKAGES:?}/logs"
 
 		if ensure_pkg_installed; then
@@ -8654,6 +8637,7 @@ prepare_ports() {
 		fi
 
 		delete_stale_symlinks_and_empty_dirs
+		delete_stale_pkg_cache
 		download_from_repo_post_delete
 		bset status "sanity:"
 
@@ -8672,17 +8656,15 @@ prepare_ports() {
 				shash_remove_var "${shash_bucket}" || :
 			done
 		)
-	fi
 
-	pkgqueue_unqueue_existing_packages
-	pkgqueue_trim_orphaned_build_deps
+		pkgqueue_unqueue_existing_packages
+		pkgqueue_trim_orphaned_build_deps
 
-	# Call the deadlock code as non-fatal which will check for cycles
-	msg "Sanity checking build queue"
-	bset status "pkgqueue_sanity_check:"
-	pkgqueue_sanity_check 0
+		# Call the deadlock code as non-fatal which will check for cycles
+		msg "Sanity checking build queue"
+		bset status "pkgqueue_sanity_check:"
+		pkgqueue_sanity_check 0
 
-	if was_a_bulk_run; then
 		if [ "${resuming_build}" -eq 0 ]; then
 			# Generate ports.queued list and stats_queued after
 			# the queue was trimmed.

src/share/poudriere/include/pkg.sh
@@ -368,6 +368,26 @@ delete_stale_pkg_cache() {
 	return 0
 }
 
+# If the user ran pkg-repo in the wrong directory we need to undo that.
+delete_bad_pkg_repo_files() {
+	local ext file
+
+	for ext in "${PKG_EXT:?}" "txz"; do
+		for file in \
+		    meta \
+		    digests \
+		    filesite \
+		    packagesite; do
+			pkg="${PACKAGES:?}/All/${file}.${ext}"
+			if [ ! -f "${pkg}" ]; then
+				continue
+			fi
+			msg "Removing invalid pkg repo file: ${pkg}"
+			unlink "${pkg}"
+		done
+	done
+}
+
 delete_all_pkgs() {
 	[ $# -eq 1 ] || eargs delete_all_pkgs reason
 	local reason="$1"