freebsd/poudriere

commit bac8a649e2717554700b7e04f8b226db41d883dc

mapfile sh: mapfile open for read should fail on missing file

src/share/poudriere/include/util.sh
@@ -678,8 +678,20 @@ mapfile() {
 	[ $# -eq 2 -o $# -eq 3 ] || eargs mapfile handle_name file modes
 	local handle_name="$1"
 	local _file="$2"
-	local mypid _hkey
+	local _modes="$3"
+	local mypid _hkey ret
 
+	case " ${_modes} " in
+	*r*w*|*w*r*) ;;
+	*w*|*a*) ;;
+	*r*)
+		if [ ! -r "${_file}" ]; then
+			return 1
+		fi
+		;;
+	esac
+
+	ret=0
 	mypid=$(getpid)
 	case "${_file}" in
 		-|/dev/stdin) _file="/dev/fd/0" ;;
@@ -716,9 +728,21 @@ mapfile() {
 		hash_set mapfile_fd "${_mapfile_handle}" "${_file#/dev/fd/}"
 		;;
 	*)
-		exec 8<> "${_file}" ;;
+		case " ${_modes} " in
+		*r*w*|*w*r*)
+			exec 8<> "${_file}" || ret="$?"
+			;;
+		*r*)
+			exec 8< "${_file}" || ret="$?"
+			;;
+		*w*|*a*)
+			exec 8> "${_file}" || ret="$?"
+			;;
+		esac
+		;;
 	esac
 	hash_set mapfile_file "${_mapfile_handle}" "${_file}"
+	return "${ret}"
 }
 
 mapfile_read() {

test/mapfile.sh
@@ -47,6 +47,13 @@ writer() {
 	fi
 }
 
+{
+	TMP=$(mktemp -u)
+	set -x
+	assert_ret_not 0 mapfile handle "${TMP}" "re"
+	rm -f "${TMP}"
+}
+
 if mapfile_builtin; then
 # Test pipes
 {

commit 6b2cd890982392b4e848c681d29c6dc961588b07

mapfile sh: Fix opening -
Fixes: bac8a649e2

src/share/poudriere/include/util.sh
@@ -919,6 +919,13 @@ mapfile() {
 	local _modes="$3"
 	local mypid _hkey ret
 
+	ret=0
+	mypid=$(getpid)
+	case "${_file}" in
+		-|/dev/stdin) _file="/dev/fd/0" ;;
+	esac
+	_hkey="${_file}.${mypid}"
+
 	case " ${_modes} " in
 	*r*w*|*w*r*|*+*) ;;
 	*w*|*a*) ;;
@@ -928,13 +935,6 @@ mapfile() {
 		fi
 		;;
 	esac
-
-	ret=0
-	mypid=$(getpid)
-	case "${_file}" in
-		-|/dev/stdin) _file="/dev/fd/0" ;;
-	esac
-	_hkey="${_file}.${mypid}"
 
 	case "${_mapfile_handle-}" in
 	""|"${_hkey}") ;;