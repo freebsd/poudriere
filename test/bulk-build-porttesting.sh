15931db4ed6b9824d8362e8c575a617bfb2343ff
Bryan Drewery <bryan@shatow.net>
2024-02-26 17:46:08 -0800

    Add porttesting tests

---
+++ test/bulk-build-porttesting.sh
@@ -0,0 +1,40 @@
+LISTPORTS="ports-mgmt/poudriere-devel-porttesting"
+OVERLAYS="omnibus porttesting"
+. common.bulk.sh
+
+do_pkgclean -y -A
+assert 0 "$?" "Pkgclean should pass"
+
+EXPECTED_IGNORED=""
+EXPECTED_SKIPPED=
+EXPECTED_TOBUILD="ports-mgmt/pkg ports-mgmt/poudriere-devel-porttesting"
+EXPECTED_QUEUED="${EXPECTED_TOBUILD}"
+EXPECTED_LISTED="${LISTPORTS}"
+EXPECTED_BUILT=
+do_bulk -c -n ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+assert_bulk_queue_and_stats
+assert_bulk_dry_run
+echo "------" | tee /dev/stderr
+
+EXPECTED_BUILT="${EXPECTED_TOBUILD}"
+do_bulk -c ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+allpackages="$(/bin/ls ${PACKAGES:?}/All/)"
+assert 0 "$?"
+echo "------" | tee /dev/stderr
+
+# Now test with -t which should fail due to bad plist.
+EXPECTED_TOBUILD="ports-mgmt/poudriere-devel-porttesting"
+EXPECTED_QUEUED="${EXPECTED_TOBUILD}"
+EXPECTED_BUILT=""
+EXPECTED_FAILED="ports-mgmt/poudriere-devel-porttesting"
+do_bulk -C -t ${LISTPORTS}
+assert 1 $? "Bulk should fail"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+allpackages="$(/bin/ls ${PACKAGES:?}/All/)"
+assert 0 "$?"
+echo "------" | tee /dev/stderr
---
+++ test/testport-build-porttesting.sh
@@ -0,0 +1,28 @@
+OVERLAYS="omnibus porttesting"
+TESTPORT="ports-mgmt/poudriere-devel-porttesting"
+LISTPORTS="${TESTPORT}"
+. common.bulk.sh
+
+# testport will keep old packages so we need to clean out everything
+# before doing the first run to ensure it all builds.
+do_pkgclean -y -A
+assert 0 $? "Pkgclean should pass"
+echo "-----" | tee /dev/stderr
+
+EXPECTED_QUEUED="ports-mgmt/pkg ${TESTPORT}"
+EXPECTED_LISTED="${TESTPORT}"
+EXPECTED_TOBUILD="ports-mgmt/pkg ${TESTPORT}"
+EXPECTED_BUILT=
+do_testport -n ${TESTPORT}
+assert 0 "$?" "testport dry-run should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+echo "-----" | tee /dev/stderr
+
+EXPECTED_BUILT="ports-mgmt/pkg"
+EXPECTED_FAILED="ports-mgmt/poudriere-devel-porttesting"
+do_testport ${TESTPORT}
+rssert 1 "$?" "testport should fail"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+echo "-----" | tee /dev/stderr
