2f78c0be4e252e40307aafc7cb29a05b5c3a11f8
Bryan Drewery <bryan@shatow.net>
2024-11-11 13:49:58 -0800

    jail -cu: Don't include build-time make.conf files into jail's clean snapshot
    (cherry picked from commit 256f4f17a12097b15cc2c493c15425b1a21131bb)
    
    Conflicts:
    	src/share/poudriere/jail.sh
    
    Fixes #1317

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -335,19 +335,22 @@ update_jail() {
 		rm -f "${fu_bin}"
 		update_version
 		build_native_xtools
+		cleanup_confs
 		markfs clean ${JAILMNT}
 		;;
 	svn*|git*)
 		install_from_vcs version_extra
 		RELEASE=$(update_version "${version_extra}")
-		make -C ${SRC_BASE} delete-old delete-old-libs DESTDIR=${JAILMNT} BATCH_DELETE_OLD_FILES=yes
+		make -C ${SRC_BASE} delete-old delete-old-libs DESTDIR=${JAILMNT:?} BATCH_DELETE_OLD_FILES=yes
+		cleanup_confs
 		markfs clean ${JAILMNT}
 		;;
 	src=*)
 		SRC_BASE="${METHOD#src=}"
 		install_from_src version_extra
 		RELEASE=$(update_version "${version_extra}")
-		make -C ${SRC_BASE} delete-old delete-old-libs DESTDIR=${JAILMNT} BATCH_DELETE_OLD_FILES=yes
+		make -C ${SRC_BASE} delete-old delete-old-libs DESTDIR=${JAILMNT:?} BATCH_DELETE_OLD_FILES=yes
+		cleanup_confs
 		markfs clean ${JAILMNT}
 		;;
 	allbsd|gjb|url=*|freebsdci)
@@ -470,6 +473,7 @@ setup_build_env() {
  	setup_src_conf "make"
 	setup_src_conf "src"
 	setup_src_conf "src-env"
+	SETUP_CONFS=1
 	if [ "${TARGET}" = "mips" ]; then
 		echo "WITH_ELFTOOLCHAIN_TOOLS=y" >> ${JAILMNT}/etc/src.conf
 	fi
@@ -477,6 +481,21 @@ setup_build_env() {
 	export __MAKE_CONF=${JAILMNT}/etc/make.conf
 	export SRCCONF=${JAILMNT}/etc/src.conf
 	export SRC_ENV_CONF=${JAILMNT}/etc/src-env.conf
+}
+
+# Must ensure conf files don't leak into `markfs clean`
+cleanup_confs() {
+	local file
+
+	case "${SETUP_CONFS-}" in
+	1) ;;
+	*) return 0 ;;
+	esac
+
+	for file in /etc/make.conf /etc/src.conf /etc/src-env.conf; do
+		rm -f "${JAILMNT?}/${file}"
+	done
+	unset SETUP_CONFS
 }
 
 setup_src_conf() {
@@ -1130,6 +1149,7 @@ create_jail() {
 	    [ ! -f "${JAILMNT}/etc/login.conf" ] && \
 	    err 1 "Directory ${JAILMNT} must be populated from installworld already."
 
+	cleanup_confs
 	markfs clean ${JAILMNT}
 
 	# Check VERSION before running 'update_jail' on jails created using FreeBSD dists.
