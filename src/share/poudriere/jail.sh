05a460dcafccb99c643ac2cfe71c5ff81aa88cc2
Anton Saietskii <vsasjason@gmail.com>
2025-03-12 15:07:23 +0000

    jail -d: Properly pass -C argument
    The argument for CLEANJAIL via OPTARG is never passed down as it should.
    
    Fixes: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=247448

--- src/share/poudriere/jail.sh
+++ src/share/poudriere/jail.sh
@@ -1270,7 +1270,7 @@ shift $((OPTIND-1))
 post_getopts
 
 METHOD=${METHOD:-${METHOD_DEF}}
-CLEANJAIL=${CLEAN:-none}
+CLEANJAIL=${CLEANJAIL:-none}
 if [ -n "${JAILNAME}" -a "${COMMAND}" != "create" ]; then
 	_jget ARCH ${JAILNAME} arch || :
 	_jget JAILFS ${JAILNAME} fs || :
