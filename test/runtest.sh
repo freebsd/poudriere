freebsd/poudriere

commit 7037c2f534f698b4d188ba58239a144c94bf4354

tests: Let truss finish draining on signals INT/PIPE/TERM

test/runtest.sh
@@ -114,6 +114,9 @@ runtest() {
 	export TEST_NUMS
 	# With truss use --foreground to prevent process reaper and ptrace deadlocking.
 	set -x
+	# Keep timeout/timestamp/truss running until the test exits.
+	# All of these will be fixed in the test.
+	trap '' INT PIPE TERM HUP
 	{
 		TEST_START="$(clock -monotonic)"
 		echo "Test started: $(date)"
@@ -125,7 +128,8 @@ runtest() {
 	    ${SH_DISABLE_VFORK:+SH_DISABLE_VFORK=1} \
 	    THISDIR="${THISDIR}" \
 	    SH="${SH}" \
-	    ${TRUSS:+truss -ae -f -s512 -o"$(get_log_name).truss"} \
+	    lockf -k "$(get_log_name).lock" \
+	    ${TRUSS:+truss -ae -f -s512 -o "$(get_log_name).truss"} \
 	    "${SH}" "${TEST}"
 	{
 		TEST_END="$(clock -monotonic)"

.gitignore
@@ -60,3 +60,4 @@ external/ptsort/.git
 external/ptsort/.gitignore
 test/prep
 test/*.log.truss
+test/*.log.lock

commit 1e40df446d97ff97bc5abd431592e61ae7ea04b8

tests: Fix locks_critical_section*.sh.
Commit 7037c2f5 broke these. Need to keep the signals unignored
for locks_critical_section*.sh to work.

test/runtest.sh
@@ -267,9 +267,15 @@ runtest() {
 	export TEST_NUMS
 	# With truss use --foreground to prevent process reaper and ptrace deadlocking.
 	set -x
-	# Keep timeout/timestamp/truss running until the test exits.
-	# All of these will be fixed in the test.
-	trap '' INT PIPE TERM HUP
+	case "${TRUSS-}" in
+	"") ;;
+	*)
+		# Let truss finish draining when receiving a signal.
+		# Only do this for truss as otherwise some tests will not
+		# be able to modify the signals for their own purposes.
+		trap '' INT PIPE TERM HUP
+		;;
+	esac
 	{
 		TEST_START="$(clock -monotonic)"
 		echo "Test started: $(date)"