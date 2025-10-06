freebsd/poudriere

commit 13216a7d047514a7151183853ff9eb2030d1a8ca

Convert some bgpid handling to use jobs
Issue #977

src/share/poudriere/common.sh
@@ -998,16 +998,19 @@ log_start() {
 				# Otherwise need setbuf -o L here due to
 				# stdout not writing to terminal but to tee.
 				TIME_START="${TIME_START_JOB:-${TIME_START:-0}}" \
+				    _spawn_wrapper \
 				    timestamp -u < ${logfile}.pipe | \
 				    tee ${logfile} &
 			else
-				tee ${logfile} < ${logfile}.pipe &
+				_spawn_wrapper \
+				    tee ${logfile} < ${logfile}.pipe &
 			fi
 		elif [ "${TIMESTAMP_LOGS}" = "yes" ]; then
 			TIME_START="${TIME_START_JOB:-${TIME_START:-0}}" \
+			    _spawn_wrapper \
 			    timestamp > ${logfile} < ${logfile}.pipe &
 		fi
-		tpid=$!
+		get_job_id "$!" log_start_job
 		exec > ${logfile}.pipe 2>&1
 
 		# Remove fifo pipe file right away to avoid orphaning it.
@@ -1016,7 +1019,7 @@ log_start() {
 		unlink ${logfile}.pipe
 	else
 		# Send output directly to file.
-		tpid=
+		unset log_start_job
 		exec > ${logfile} 2>&1
 	fi
 }
@@ -1186,11 +1189,10 @@ log_stop() {
 		unset OUTPUT_REDIRECTED_STDOUT
 		unset OUTPUT_REDIRECTED_STDERR
 	fi
-	case "${tpid:+set}" in
+	case "${log_start_job:+set}" in
 	set)
-		# Give tee a moment to flush buffers
-		timed_wait_and_kill 5 $tpid 2>/dev/null || :
-		unset tpid
+		timed_wait_and_kill_job 5 "%${log_start_job}"
+		unset log_start_job
 		;;
 	esac
 }
@@ -5329,16 +5331,16 @@ stop_builders() {
 job_done() {
 	[ $# -eq 1 ] || eargs job_done j
 	local j="$1"
-	local pkgname status pid ret
+	local pkgname status jobno ret
 
 	# Failure to find this indicates the job is already done.
 	hash_remove builder_pkgnames "${j}" pkgname || return 1
-	hash_remove builder_pids "${j}" pid
+	hash_remove builder_jobs "${j}" jobno || return 1
 	unlink "${MASTER_DATADIR:?}/var/run/${j}.pid"
 	_bget status ${j} status
 	pkgqueue_job_done "${pkgname}"
 	ret=0
-	_wait "${pid}" || ret="$?"
+	_wait "${jobno}" || ret="$?"
 	case "${status}:" in
 	"done:"*)
 		dev_assert 0 "${ret}"
@@ -5359,7 +5361,9 @@ build_queue() {
 	local jname="$1"
 	local ptname="$2"
 	local setname="$3"
-	local j jobid pid pkgname builders_active queue_empty
+	# jobid is analgous to MY_JOB_ID: builder number
+	# jobno is from $(jobs)
+	local j jobid jobno pid pkgname builders_active queue_empty
 	local builders_idle idle_only timeout log porttesting
 
 	_log_path log
@@ -5379,12 +5383,12 @@ build_queue() {
 		builders_idle=0
 		timeout=30
 		for j in ${JOBS}; do
-			# Check if pid is alive. A job will have no PID if it
+			# Check if job is alive. A job will have no PID if it
 			# is idle. idle_only=1 is a quick check for giving
 			# new work only to idle workers.
-			if hash_get builder_pids "${j}" pid; then
+			if hash_get builder_jobs "${j}" jobno; then
 				if [ ${idle_only} -eq 1 ] ||
-				    kill -0 ${pid} 2>/dev/null; then
+				    kill -0 "${jobno}" 2>/dev/null; then
 					# Job still active or skipping busy.
 					builders_active=1
 					continue
@@ -5423,9 +5427,10 @@ build_queue() {
 			    maybe_start_builder "${j}" "${jname}" \
 			        "${ptname}" "${setname}" \
 			    build_pkg "${pkgname}" "${porttesting}"
-			pid=$!
+			pid="$!"
+			jobno="%${spawn_jobid:?}"
 			echo "${pid}" > "${MASTER_DATADIR:?}/var/run/${j}.pid"
-			hash_set builder_pids "${j}" "${pid}"
+			hash_set builder_jobs "${j}" "${jobno}"
 			hash_set builder_pkgnames "${j}" "${pkgname}"
 		done
 

test/common.sh
@@ -210,6 +210,7 @@ capture_output_simple() {
 	local my_stderr_return="$2"
 	local _my_stdout _my_stdout_log
 	local _my_stderr _my_stderr_log
+	local -
 
 	if [ -n "${REDIRECTED_STDERR_FD-}" ]; then
 		err 99 "capture_output_simple called nested"
@@ -222,8 +223,10 @@ capture_output_simple() {
 		echo "Capture stdout logs to ${_my_stdout_log}" >&2
 		exec 6>&1
 		mkfifo "${_my_stdout}"
-		tee "${_my_stdout_log}" >&6 < "${_my_stdout}" &
-		my_stdout_pid=$!
+		set -m
+		_spawn_wrapper tee "${_my_stdout_log}" >&6 < "${_my_stdout}" &
+		get_job_id "$!" my_stdout_job
+		set +m
 		exec > "${_my_stdout}"
 		unlink "${_my_stdout}"
 		setvar "${my_stdout_return}" "${_my_stdout_log}"
@@ -240,8 +243,10 @@ capture_output_simple() {
 		exec 7>&2
 		REDIRECTED_STDERR_FD=7
 		mkfifo "${_my_stderr}"
-		tee "${_my_stderr_log}" >&7 < "${_my_stderr}" &
-		my_stderr_pid=$!
+		set -m
+		_spawn_wrapper tee "${_my_stderr_log}" >&7 < "${_my_stderr}" &
+		get_job_id "$!" my_stderr_job
+		set +m
 		exec 2> "${_my_stderr}"
 		unlink "${_my_stderr}"
 		setvar "${my_stderr_return}" "${_my_stderr_log}"
@@ -257,18 +262,18 @@ capture_output_simple_stop() {
 		return
 	fi
 	unset REDIRECTED_STDERR_FD
-	case "${my_stdout_pid:+set}" in
+	case "${my_stdout_job:+set}" in
 	set)
 		exec 1>&6 6>&-
-		timed_wait_and_kill 1 "${my_stdout_pid}" >/dev/null 2>&1 || :
-		unset my_stdout_pid
+		timed_wait_and_kill_job 1 "%${my_stdout_job:?}" || :
+		unset my_stdout_job
 		;;
 	esac
-	case "${my_stderr_pid:+set}" in
+	case "${my_stderr_job:+set}" in
 	set)
 		exec 2>&7 7>&-
-		timed_wait_and_kill 1 "${my_stderr_pid}" >/dev/null 2>&1 || :
-		unset my_stderr_pid
+		timed_wait_and_kill_job 1 "%${my_stderr_job:?}" || :
+		unset my_stderr_job
 		;;
 	esac
 }

src/share/poudriere/include/util.sh
@@ -908,8 +908,8 @@ _pipe_hold_child() {
 
 # This keeps the given fifos open to avoid EOF in writers.
 pipe_hold() {
-	[ $# -ge 3 ] || eargs pipe_hold var_return_pid watch_pid fifos...
-	local var_return_pid="$1"
+	[ $# -ge 3 ] || eargs pipe_hold var_return_jobid watch_pid fifos...
+	local var_return_jobid="$1"
 	local watch_pid="$2"
 	shift 2
 	local sync_fifo sync ret
@@ -920,7 +920,7 @@ pipe_hold() {
 	mkfifo "${sync_fifo}"
 
 	spawn_job_protected _pipe_hold_child "${sync_fifo}" "${watch_pid}" "$@"
-	setvar "${var_return_pid}" "$!"
+	setvar "${var_return_jobid}" "${spawn_jobid}"
 	read_pipe "${sync_fifo}" sync || ret="$?"
 	case "${sync}" in
 	ready) ;;
@@ -1273,8 +1273,7 @@ pipe_func() {
 		mkfifo "${_mf_fifo}"
 		hash_set pipe_func_fifo "${_mf_key}" "${_mf_fifo}"
 		spawn_job _pipe_func_job "${_mf_fifo}" "$@"
-		_mf_job="$!"
-		hash_set pipe_func_job "${_mf_key}" "${_mf_job}"
+		hash_set pipe_func_job "${_mf_key}" "${spawn_jobid}"
 		mapfile _mf_handle "${_mf_fifo}" "re" ||
 		    err "${EX_SOFTWARE}" "pipe_func: Failed to open ${_mf_fifo}"
 		hash_set pipe_func_handle "${_mf_key}" "${_mf_handle}"
@@ -1295,7 +1294,7 @@ pipe_func() {
 		unlink "${_mf_fifo}"
 		hash_remove pipe_func_job  "${_mf_key}" _mf_job ||
 		    err "${EX_SOFTWARE}" "pipe_func: No stored job for ${_mf_key}"
-		kill_job 1 "${_mf_job}" || _mf_ret="$?"
+		kill_job 1 "%${_mf_job}" || _mf_ret="$?"
 		unset "${_mf_handle_var}"
 		return "${_mf_ret}"
 	fi
@@ -1441,14 +1440,16 @@ prefix_stderr_quick() {
 prefix_stderr() {
 	local extra="$1"
 	shift 1
-	local prefixpipe prefixpid ret
+	local prefixpipe prefix_job ret
 	local prefix MSG_NESTED_STDERR
 	local - errexit
 
 	prefixpipe=$(mktemp -ut prefix_stderr.pipe)
 	mkfifo "${prefixpipe}"
+	set -m
 	(
-		trap - INT
+		_spawn_wrapper :
+
 		if [ "${USE_TIMESTAMP:-1}" -eq 1 ] && \
 		    command -v timestamp >/dev/null; then
 			# Let timestamp handle showing the proper time.
@@ -1465,7 +1466,8 @@ prefix_stderr() {
 			done
 		fi
 	) < "${prefixpipe}" &
-	prefixpid=$!
+	set +m
+	get_job_id "$!" prefix_job
 	exec 4>&2
 	exec 2> "${prefixpipe}"
 	unlink "${prefixpipe}"
@@ -1480,7 +1482,7 @@ prefix_stderr() {
 	fi
 
 	exec 2>&4 4>&-
-	timed_wait_and_kill 5 ${prefixpid} 2>/dev/null || :
+	timed_wait_and_kill_job 5 "%${prefix_job}" || :
 
 	return ${ret}
 }
@@ -1488,14 +1490,15 @@ prefix_stderr() {
 prefix_stdout() {
 	local extra="$1"
 	shift 1
-	local prefixpipe prefixpid ret
+	local prefixpipe prefix_job ret
 	local prefix MSG_NESTED
 	local - errexit
 
 	prefixpipe=$(mktemp -ut prefix_stdout.pipe)
 	mkfifo "${prefixpipe}"
+	set -m
 	(
-		trap - INT
+		_spawn_wrapper :
 		if [ "${USE_TIMESTAMP:-1}" -eq 1 ] && \
 		    command -v timestamp >/dev/null; then
 			# Let timestamp handle showing the proper time.
@@ -1511,7 +1514,8 @@ prefix_stdout() {
 			done
 		fi
 	) < "${prefixpipe}" &
-	prefixpid=$!
+	set +m
+	get_job_id "$!" prefix_job
 	exec 3>&1
 	exec > "${prefixpipe}"
 	unlink "${prefixpipe}"
@@ -1526,7 +1530,7 @@ prefix_stdout() {
 	fi
 
 	exec 1>&3 3>&-
-	timed_wait_and_kill 5 ${prefixpid} 2>/dev/null || :
+	timed_wait_and_kill_job 5 "%${prefix_job}" || :
 
 	return ${ret}
 }
@@ -1534,7 +1538,7 @@ prefix_stdout() {
 prefix_output() {
 	local extra="$1"
 	local prefix_stdout prefix_stderr prefixpipe_stdout prefixpipe_stderr
-	local ret MSG_NESTED MSG_NESTED_STDERR prefixpid
+	local ret MSG_NESTED MSG_NESTED_STDERR prefix_job
 	local - errexit
 	shift 1
 
@@ -1555,13 +1559,12 @@ prefix_output() {
 	prefix_stderr="$(NO_ELAPSED_IN_MSG=1 msg_warn "${extra}:" 2>&1)"
 
 	TIME_START="${TIME_START_JOB:-${TIME_START:-0}}" \
-	    spawn \
+	    spawn_job \
 	    timestamp \
 	    -1 "${prefix_stdout}" -o "${prefixpipe_stdout}" \
 	    -2 "${prefix_stderr}" -e "${prefixpipe_stderr}" \
 	    -P "poudriere: ${PROC_TITLE} (prefix_output)"
-
-	prefixpid=$!
+	prefix_job="${spawn_jobid}"
 	exec 3>&1
 	exec > "${prefixpipe_stdout}"
 	unlink "${prefixpipe_stdout}"
@@ -1580,7 +1583,7 @@ prefix_output() {
 	fi
 
 	exec 1>&3 3>&- 2>&4 4>&-
-	timed_wait_and_kill 5 ${prefixpid} 2>/dev/null || :
+	timed_wait_and_kill_job 5 "%${prefix_job}" || :
 
 	return ${ret}
 }

test/locks_critical_section.sh
@@ -73,17 +73,20 @@ assert 1 ${main_siginfo} "INFO should be trapped"
 	lock_acquire TEST 0
 	assert 0 "$?" "lock_acquire"
 
+	set -m
 	(
-		trap - INT
+		_spawn_wrapper :
 		lock_have TEST
 		assert_not 0 "$?" "child should not have lock TEST"
 		sleep 300
 	) &
-	bgpid="$!"
+	set +m
+	jobs -l
+	get_job_id "$!" bgjob
 
 	sleep 2
-	kill_and_wait 10 "${bgpid}"
-	assert 143 "$?" "kill bgpid - it should exit on INT rather than wait"
+	kill_job 10 "%${bgjob}"
+	assert 0 "$?" "kill bgpid - it should exit on TERM rather than wait"
 
 	lock_release TEST
 	assert 0 "$?" "lock_release"

test/logging.sh
@@ -15,7 +15,7 @@ NO_ELAPSED_IN_MSG=1
 USE_DEBUG=no
 
 logging_setup() {
-	unset tpid
+	unset tpid log_start_job
 	unset expect_pids
 	TIMESTAMP_LOGS=no
 	expect_pids=0
@@ -88,10 +88,10 @@ while get_test_context; do
 	PORT_NAME="port-${TEST_CONTEXT_NUM}"
 	assert_ret 0 log_start ${prefix:+-P "port:"} "${PORT_NAME}" ${PORT_TEE}
 	if [ "${expect_pids_port}" -eq 1 ]; then
-		assert_not "null" "${tpid-null}"
-		assert_ret 0 check_pids "${tpid}"
+		assert_not "null" "${log_start_job-null}"
+		assert_ret 0 check_pids "${log_start_job}"
 	else
-		assert "" "${tpid-null}"
+		assert "null" "${log_start_job-null}"
 	fi
 	_logfile port "${PORT_NAME}"
 	assert_ret 0 [ -r "${port}" ]
@@ -109,6 +109,8 @@ while get_test_context; do
 	redirect_to_bulk echo console-port-test1
 	redirect_to_bulk echo console-port-test1-err >&2
 	if [ "${expect_pids_port}" -eq 1 ]; then
+		tpid="$(jobid "%${log_start_job:?}")"
+		assert 0 "$?"
 		assert_ret 0 check_pids "${tpid}"
 	fi
 	echo "port-test2${colored}"
@@ -121,6 +123,8 @@ while get_test_context; do
 	MY_JOBID=${my_job_id} msg_error "job error-post from port for port stderr and bulk stdout"
 	msg_error "error-post from port only for bulk stderr"
 	if [ "${expect_pids_port}" -eq 1 ]; then
+		tpid="$(jobid "%${log_start_job:?}")"
+		assert 0 "$?"
 		assert_ret 0 check_pids "${tpid}"
 	fi
 	assert_ret 0 log_stop "${PORT_NAME}"

test/pipe_hold.sh
@@ -23,11 +23,12 @@ reader() {
 	spawn_job reader "${FIFO}" "${OUTPUT}"
 	assert 0 "$?"
 	reader_pid="$!"
-	assert_true kill -0 "${reader_pid}"
+	assert_true get_job_id "${reader_pid}" reader_job
+	assert_true kill -0 "%${reader_job}"
 	echo "Blah1" > "${FIFO}"
 	assert 0 "$?"
 	# Having closed the pipe the child will die.
-	assert_true _wait "${reader_pid}"
+	assert_true _wait "%${reader_job}"
 	assert_file - "${OUTPUT}" <<-EOF
 	Blah1
 	EOF
@@ -42,25 +43,32 @@ reader() {
 	spawn_job reader "${FIFO}" "${OUTPUT}"
 	assert 0 "$?"
 	reader_pid="$!"
-	assert_true pipe_hold pipe_hold_pid "${reader_pid}" "${FIFO}"
-	assert_true kill -0 "${reader_pid}"
+	assert_true get_job_id "${reader_pid}" reader_job
+	assert_true pipe_hold pipe_hold_jobid "${reader_pid}" "${FIFO}"
+	assert_true kill -0 "%${reader_job}"
+	assert_true get_job_status "%${reader_job}" status
+	assert "Running" "${status}"
 	echo "Blah1" > "${FIFO}"
 	assert 0 "$?"
-	assert_true kill -0 "${reader_pid}"
+	assert_true kill -0 "%${reader_job}"
+	assert_true get_job_status "%${reader_job}" status
+	assert "Running" "${status}"
 	echo "Blah2" > "${FIFO}"
 	assert 0 "$?"
-	assert_true kill -0 "${reader_pid}"
+	assert_true kill -0 "%${reader_job}"
+	assert_true get_job_status "%${reader_job}" status
+	assert "Running" "${status}"
 	assert_file - "${OUTPUT}" <<-EOF
 	Blah1
 	Blah2
 	EOF
-	kill_job 1 "${reader_pid}"
+	kill_job 1 "%${reader_job}"
 	ret="$?"
 	case "${ret}" in
 	143) ret=0 ;;
 	esac
 	assert 0 "${ret}"
-	kill_job 1 "${pipe_hold_pid}"
+	kill_job 1 "%${pipe_hold_jobid}"
 	ret="$?"
 	case "${ret}" in
 	143) ret=0 ;;

commit 72f2ce92d553a04d2ea8802a945e07dff8c1e410

build_queue: Fix detecting broken builders.
This was broken by commit 13216a7d04 which changed build_queue() to use
a job rather than pid. When the build job exits, its job keeps the pid
responding on kill -0. Check its job status instead.

Fixes #1254

src/share/poudriere/common.sh
@@ -5708,7 +5708,7 @@ build_queue() {
 	# jobid is analgous to MY_JOBID: builder number
 	# jobno is from $(jobs)
 	local j jobid jobno job_name builders_active queue_empty
-	local job_type builders_idle job_finished timeout
+	local job_type builders_idle job_status job_finished timeout
 
 	run_hook build_queue start
 
@@ -5731,11 +5731,20 @@ build_queue() {
 			if hash_get builder_jobs "${j}" jobno; then
 				# If a job just finished we skip checking status of
 				# other jobs. We focus on filling empty slots.
-				if [ ${job_finished} -eq 1 ] ||
-				    kill -0 "${jobno}" 2>/dev/null; then
+				if [ ${job_finished} -eq 1 ]; then
 					builders_active=1
 					continue
 				fi
+				dev_assert_true kill -0 "${jobno}"
+				get_job_status "${jobno}" job_status ||
+				    err "${EX_SOFTWARE:-70}" "build_queue: get_job_status ${jobno}"
+				case "${job_status}" in
+				"Running")
+					builders_active=1
+					continue
+					;;
+				esac
+				# The job is Done or Terminated.
 				job_done "${j}"
 				# Set a 0 timeout to quickly rescan for idle
 				# builders to toss a job at since the queue
@@ -6332,6 +6341,26 @@ build_pkg() {
 		    umount -f "${mnt:?}/wrkdirs"
 		rm -f "${mnt:?}/.tmpfs_blacklist_dir"
 		rm -rf "${tmpfs_blacklist_dir:?}"
+		;;
+	esac
+
+	case "${FP_BUILD_PKG_EXIT_PKGNAMES:+set}" in
+	set)
+		local fp_pkg_glob
+
+		set -o noglob
+		for fp_pkg_glob in ${FP_BUILD_PKG_EXIT_PKGNAMES}; do
+			# shellcheck disable=SC2254
+			case "${pkgbase}" in
+			${fp_pkg_glob})
+				msg_error "FP_BUILD_PKG_EXIT_PKGNAMES failpoint match pkgname='${pkgname}' fp_pkg_glob='${fp_pkg_glob}'"
+				# exit immediately rather than go through
+				# err() cleanup.
+				exit 1
+				;;
+			esac
+		done
+		set +o noglob
 		;;
 	esac
 

test/Makefile.am
@@ -134,6 +134,7 @@ JAIL_TESTS= \
 	bulk-build-self-immutable-base-yes.sh \
 	bulk-build-self-inc-build_as_non_root-no.sh \
 	bulk-build-self-inc-build_as_non_root-yes.sh \
+	bulk-build-skips-crashed-builder.sh \
 	bulk-build-skips.sh \
 	bulk-build-specific-bad-flavor.sh \
 	bulk-flavor-FLAVORS-misordered.sh \

test/Makefile.in
@@ -541,6 +541,7 @@ JAIL_TESTS = \
 	bulk-build-self-immutable-base-yes.sh \
 	bulk-build-self-inc-build_as_non_root-no.sh \
 	bulk-build-self-inc-build_as_non_root-yes.sh \
+	bulk-build-skips-crashed-builder.sh \
 	bulk-build-skips.sh \
 	bulk-build-specific-bad-flavor.sh \
 	bulk-flavor-FLAVORS-misordered.sh \
@@ -1624,6 +1625,13 @@ bulk-build-self-inc-build_as_non_root-no.sh.log: bulk-build-self-inc-build_as_no
 bulk-build-self-inc-build_as_non_root-yes.sh.log: bulk-build-self-inc-build_as_non_root-yes.sh
 	@p='bulk-build-self-inc-build_as_non_root-yes.sh'; \
 	b='bulk-build-self-inc-build_as_non_root-yes.sh'; \
+	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+	--log-file $$b.log --trs-file $$b.trs \
+	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+	"$$tst" $(AM_TESTS_FD_REDIRECT)
+bulk-build-skips-crashed-builder.sh.log: bulk-build-skips-crashed-builder.sh
+	@p='bulk-build-skips-crashed-builder.sh'; \
+	b='bulk-build-skips-crashed-builder.sh'; \
 	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
 	--log-file $$b.log --trs-file $$b.trs \
 	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \

test/bulk-build-skips-crashed-builder.sh
@@ -1,0 +1,51 @@
+FLAVOR_DEFAULT_ALL=no
+FLAVOR_ALL=all
+LISTPORTS="\
+	ports-mgmt/poudriere-devel \
+	misc/freebsd-release-manifests@${FLAVOR_ALL} \
+	misc/foo@${FLAVOR_ALL} \
+	misc/foo-FLAVORS-unsorted@${FLAVOR_ALL} \
+"
+JFLAG=4
+OVERLAYS="omnibus"
+. ./common.bulk.sh
+
+# This is the same as bulk-build-multiple-eligible-in-queue.sh except that
+# misc/foo will fail to build and cause rdeps to skip.
+set_make_conf <<-EOF
+misc_foo_SET=	FAILURE
+EOF
+
+do_pkgclean -y -A
+assert 0 "$?" "Pkgclean should pass"
+
+EXPECTED_IGNORED="misc/foo-FLAVORS-unsorted@ignored misc/foo-FLAVORS-unsorted@depignored misc/foo@ignored misc/foo-dep-FLAVORS-unsorted@depignored"
+EXPECTED_SKIPPED=""
+EXPECTED_TOBUILD="misc/foo@default misc/foo-FLAVORS-unsorted@default misc/foo-FLAVORS-unsorted@flav misc/foo@flav misc/freebsd-release-manifests@default misc/freebsd-release-manifests@bar misc/freebsd-release-manifests@foo ports-mgmt/pkg ports-mgmt/poudriere-devel misc/foo-dep-FLAVORS-unsorted@default misc/foo-dep-FLAVORS-unsorted@flav"
+EXPECTED_QUEUED="${EXPECTED_TOBUILD} ${EXPECTED_IGNORED}"
+EXPECTED_LISTED="${LISTPORTS}"
+EXPECTED_BUILT=
+do_bulk -c -n "${LISTPORTS}"
+assert 0 "$?" "Bulk should pass"
+assert_bulk_queue_and_stats
+assert_bulk_dry_run
+echo "------" | tee /dev/stderr
+
+# Cause foo's builder to crash rather than port failure.
+
+set_poudriere_conf <<-EOF
+FP_BUILD_PKG_EXIT_PKGNAMES="foo"
+EOF
+
+# Remove misc/foo@* and misc/freebsd-release-manifests@foo
+EXPECTED_BUILT="misc/foo-FLAVORS-unsorted@default misc/foo-FLAVORS-unsorted@flav misc/freebsd-release-manifests@default misc/freebsd-release-manifests@bar ports-mgmt/pkg ports-mgmt/poudriere-devel misc/foo-dep-FLAVORS-unsorted@default misc/foo-dep-FLAVORS-unsorted@flav"
+EXPECTED_FAILED="misc/foo@default:stage misc/foo@flav"
+EXPECTED_SKIPPED="misc/freebsd-release-manifests@foo:foo-20161010"
+
+do_bulk -c "${LISTPORTS}"
+# The return status is poorly defined. 2 failed + 1 skipped
+assert 3 "$?" "Bulk should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+
+do_pkgclean_smoke