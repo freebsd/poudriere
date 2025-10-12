57770346f13c7955563e253155a6d172eb2b7dc9
Bryan Drewery <bryan@shatow.net>
2025-10-12 10:02:25 -0700

    build quick tests: Also test PKG_NO_VERSION_FOR_DEPS

--- test/bulk-build-quick-pkg.sh
+++ test/bulk-build-quick-pkg.sh
@@ -1,22 +1,31 @@
 LISTPORTS="ports-mgmt/pkg"
 . ./common.bulk.sh
 
-EXPECTED_IGNORED=
-EXPECTED_INSPECTED=
-EXPECTED_SKIPPED=
-EXPECTED_TOBUILD="${LISTPORTS}"
-EXPECTED_QUEUED="${EXPECTED_TOBUILD}"
-EXPECTED_LISTED="${LISTPORTS}"
-EXPECTED_BUILT=
-do_bulk -c -n ${LISTPORTS}
-assert 0 $? "Bulk should pass"
-assert_bulk_queue_and_stats
-assert_bulk_dry_run
-echo "------" | tee /dev/stderr
+set_test_contexts - '' '' <<-EOF
+PKG_NO_VERSION_FOR_DEPS no yes
+EOF
+while get_test_context; do
+	set_poudriere_conf <<-EOF
+	PKG_NO_VERSION_FOR_DEPS=${PKG_NO_VERSION_FOR_DEPS:?}
+	EOF
 
-EXPECTED_BUILT="${EXPECTED_TOBUILD}"
-do_bulk -c ${LISTPORTS}
-assert 0 $? "Bulk should pass"
-assert_bulk_queue_and_stats
-assert_bulk_build_results
-echo "------" | tee /dev/stderr
+	EXPECTED_IGNORED=
+	EXPECTED_INSPECTED=
+	EXPECTED_SKIPPED=
+	EXPECTED_TOBUILD="${LISTPORTS}"
+	EXPECTED_QUEUED="${EXPECTED_TOBUILD}"
+	EXPECTED_LISTED="${LISTPORTS}"
+	EXPECTED_BUILT=
+	do_bulk -c -n ${LISTPORTS}
+	assert 0 $? "Bulk should pass"
+	assert_bulk_queue_and_stats
+	assert_bulk_dry_run
+	echo "------" | tee /dev/stderr
+
+	EXPECTED_BUILT="${EXPECTED_TOBUILD}"
+	do_bulk -c ${LISTPORTS}
+	assert 0 $? "Bulk should pass"
+	assert_bulk_queue_and_stats
+	assert_bulk_build_results
+	echo "------" | tee /dev/stderr
+done
--- test/testport-build-quick-pkg.sh
+++ test/testport-build-quick-pkg.sh
@@ -3,26 +3,35 @@ TESTPORT="ports-mgmt/pkg"
 LISTPORTS="${TESTPORT}"
 . ./common.bulk.sh
 
-# testport will keep old packages so we need to clean out everything
-# before doing the first run to ensure it all builds.
-do_pkgclean -y -A
-assert 0 $? "Pkgclean should pass"
-echo "-----" | tee /dev/stderr
+set_test_contexts - '' '' <<-EOF
+PKG_NO_VERSION_FOR_DEPS no yes
+EOF
+while get_test_context; do
+	set_poudriere_conf <<-EOF
+	PKG_NO_VERSION_FOR_DEPS=${PKG_NO_VERSION_FOR_DEPS:?}
+	EOF
 
-EXPECTED_QUEUED="${TESTPORT}:listed"
-EXPECTED_LISTED="${TESTPORT}"
-EXPECTED_TOBUILD="${EXPECTED_QUEUED}"
-EXPECTED_BUILT=
-do_testport -n ${TESTPORT}
-assert 0 "$?" "testport dry-run should pass"
-assert_bulk_queue_and_stats
-assert_bulk_build_results
-echo "-----" | tee /dev/stderr
+	# testport will keep old packages so we need to clean out everything
+	# before doing the first run to ensure it all builds.
+	do_pkgclean -y -A
+	assert 0 $? "Pkgclean should pass"
+	echo "-----" | tee /dev/stderr
 
-EXPECTED_BUILT="${EXPECTED_TOBUILD}"
-EXPECTED_FAILED=""
-do_testport ${TESTPORT}
-assert 0 "$?" "testport should pass"
-assert_bulk_queue_and_stats
-assert_bulk_build_results
-echo "-----" | tee /dev/stderr
+	EXPECTED_QUEUED="${TESTPORT}:listed"
+	EXPECTED_LISTED="${TESTPORT}"
+	EXPECTED_TOBUILD="${EXPECTED_QUEUED}"
+	EXPECTED_BUILT=
+	do_testport -n ${TESTPORT}
+	assert 0 "$?" "testport dry-run should pass"
+	assert_bulk_queue_and_stats
+	assert_bulk_build_results
+	echo "-----" | tee /dev/stderr
+
+	EXPECTED_BUILT="${EXPECTED_TOBUILD}"
+	EXPECTED_FAILED=""
+	do_testport ${TESTPORT}
+	assert 0 "$?" "testport should pass"
+	assert_bulk_queue_and_stats
+	assert_bulk_build_results
+	echo "-----" | tee /dev/stderr
+done
