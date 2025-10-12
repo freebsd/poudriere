freebsd/poudriere

commit feb8f9566c8213051a9682f854f4115ace21e8c7

Cleanup some global vars.
- Unset where possible
- Rename to CAPITAL as needed

test/common.sh
@@ -553,3 +553,4 @@ if [ -r "${abs_top_srcdir}/.git" ] &&
     git_get_hash_and_dirty "${abs_top_srcdir}" 0 git_hash git_dirty; then
 	msg "Source git hash: ${git_hash} modified: ${git_dirty}"
 fi >&2
+unset git_hash git_dirty

src/share/poudriere/common.sh
@@ -929,12 +929,12 @@ jstart() {
 		MAX_MEMORY_BYTES="$((MAX_MEMORY * 1024 * 1024 * 1024))"
 		;;
 	esac
-	network="${localipargs}"
+	network="${LOCALIPARGS}"
 
 	case "${RESTRICT_NETWORKING-}" in
 	"yes") ;;
 	*)
-		network="${ipargs} ${JAIL_NET_PARAMS}"
+		network="${IPARGS} ${JAIL_NET_PARAMS}"
 		;;
 	esac
 
@@ -953,7 +953,7 @@ jstart() {
 	jail -c persist name=${name}-n \
 		path=${mpath:?} \
 		host.hostname=${BUILDER_HOSTNAME-${name}} \
-		${ipargs} ${JAIL_PARAMS} ${JAIL_NET_PARAMS}
+		${IPARGS} ${JAIL_PARAMS} ${JAIL_NET_PARAMS}
 	return 0
 }
 
@@ -10351,6 +10351,7 @@ set."")
 		esac
 		msg_warn "MUTABLE_BASE=${val} is deprecated. Change to IMMUTABLE_BASE=${IMMUTABLE_BASE}"
 	done
+	unset val
 	;;
 esac
 
@@ -10361,6 +10362,7 @@ for val in ${IMMUTABLE_BASE-}; do
 		*) err 1 "Unknown value for IMMUTABLE_BASE" ;;
 	esac
 done
+unset val
 
 case ${TMPFS_WRKDIR}${TMPFS_DATA}${TMPFS_LOCALBASE}${TMPFS_ALL} in
 1**1|*1*1|**11)
@@ -10473,16 +10475,16 @@ case "${LOIP6:+set}.${LOIP4:+set}" in
 esac
 case "${IPS:?}" in
 01)
-	localipargs="${LOIP6:+ip6.addr=${LOIP6}}"
-	ipargs="ip6=inherit"
+	LOCALIPARGS="${LOIP6:+ip6.addr=${LOIP6}}"
+	IPARGS="ip6=inherit"
 	;;
 10)
-	localipargs="${LOIP4:+ip4.addr=${LOIP4}}"
-	ipargs="ip4=inherit"
+	LOCALIPARGS="${LOIP4:+ip4.addr=${LOIP4}}"
+	IPARGS="ip4=inherit"
 	;;
 11)
-	localipargs="${LOIP4:+ip4.addr=${LOIP4} }${LOIP6:+ip6.addr=${LOIP6}}"
-	ipargs="ip4=inherit ip6=inherit"
+	LOCALIPARGS="${LOIP4:+ip4.addr=${LOIP4} }${LOIP6:+ip6.addr=${LOIP6}}"
+	IPARGS="ip4=inherit ip6=inherit"
 	;;
 esac
 

src/share/poudriere/include/util.sh
@@ -1108,14 +1108,14 @@ mapfile() {
 		;;
 	esac
 
-	case "${_mapfile_handle-}" in
+	case "${_MAPFILE_HANDLE-}" in
 	""|"${_hkey}") ;;
 	*)
 		# New file or new process
-		case "${_mapfile_handle##*.}" in
+		case "${_MAPFILE_HANDLE##*.}" in
 		"${mypid}")
 			# Same process so far...
-			case "${_mapfile_handle%.*}" in
+			case "${_MAPFILE_HANDLE%.*}" in
 			"${_file}")
 				err 1 "mapfile: earlier case _hkey should cover this"
 				;;
@@ -1128,7 +1128,7 @@ mapfile() {
 						if mapfile_supports_multiple_handles; then
 							err "${EX_SOFTWARE}" "mapfile() needs updated for multiple handle support"
 						fi
-						err "${EX_SOFTWARE}" "mapfile only supports 1 file at a time without builtin for r+w and r. ${_mapfile_handle} already open: tried to open ${_file}"
+						err "${EX_SOFTWARE}" "mapfile only supports 1 file at a time without builtin for r+w and r. ${_MAPFILE_HANDLE} already open: tried to open ${_file}"
 						;;
 					esac
 					;;
@@ -1138,7 +1138,7 @@ mapfile() {
 			;;
 		*)
 			# Different process. Nuke the tracker.
-			unset _mapfile_handle
+			unset _MAPFILE_HANDLE
 			;;
 		esac
 	esac
@@ -1148,8 +1148,8 @@ mapfile() {
 		hash_set mapfile_fd "${_hkey}" "${_file#/dev/fd/}"
 		;;
 	*)
-		: "${_mapfile_handle:="${_hkey}"}"
-		case "${_mapfile_handle}" in
+		: "${_MAPFILE_HANDLE:="${_hkey}"}"
+		case "${_MAPFILE_HANDLE}" in
 		"${_hkey}")
 			case " ${_modes} " in
 			*r*w*|*w*r*|*+*)
@@ -1260,8 +1260,8 @@ mapfile_close() {
 		8)
 			exec 8>&-
 			case "${handle}" in
-			"${_mapfile_handle-}")
-				unset _mapfile_handle
+			"${_MAPFILE_HANDLE-}")
+				unset _MAPFILE_HANDLE
 				;;
 			esac
 			;;

test/Makefile.am
@@ -204,12 +204,12 @@ TESTS+=	prep.sh
 $(JAIL_TESTS:.sh=.sh.log): prep.sh.log
 
 run_env= env \
-	 abs_top_builddir="$(abs_top_builddir)" \
-	 abs_top_srcdir="$(abs_top_srcdir)" \
-	 bindir="$(bindir)" \
-	 pkglibexecdir="$(pkglibexecdir)" \
-	 pkgdatadir="$(pkgdatadir)" \
-	 VPATH="$(VPATH)" \
+	 am_abs_top_builddir="$(abs_top_builddir)" \
+	 am_abs_top_srcdir="$(abs_top_srcdir)" \
+	 am_bindir="$(bindir)" \
+	 am_pkglibexecdir="$(pkglibexecdir)" \
+	 am_pkgdatadir="$(pkgdatadir)" \
+	 am_VPATH="$(VPATH)" \
 	 am_check=1
 LOG_COMPILER=	$(run_env) $(SHELL) $(srcdir)/runtest.sh
 

test/Makefile.in
@@ -608,12 +608,12 @@ JAIL_TESTS = \
 	testport-specific-bad-flavor-failure.sh
 
 run_env = env \
-	 abs_top_builddir="$(abs_top_builddir)" \
-	 abs_top_srcdir="$(abs_top_srcdir)" \
-	 bindir="$(bindir)" \
-	 pkglibexecdir="$(pkglibexecdir)" \
-	 pkgdatadir="$(pkgdatadir)" \
-	 VPATH="$(VPATH)" \
+	 am_abs_top_builddir="$(abs_top_builddir)" \
+	 am_abs_top_srcdir="$(abs_top_srcdir)" \
+	 am_bindir="$(bindir)" \
+	 am_pkglibexecdir="$(pkglibexecdir)" \
+	 am_pkgdatadir="$(pkgdatadir)" \
+	 am_VPATH="$(VPATH)" \
 	 am_check=1
 
 LOG_COMPILER = $(run_env) $(SHELL) $(srcdir)/runtest.sh

test/runtest.sh
@@ -142,13 +142,13 @@ set -u
 # fetching.
 while read var; do
 	case "${var}" in
-	abs_top_builddir|\
-	abs_top_srcdir|\
-	srcdir|\
-	bindir|\
-	pkglibexecdir|\
-	pkgdatadir|\
-	VPATH|\
+	am_abs_top_builddir|\
+	am_abs_top_srcdir|\
+	am_srcdir|\
+	am_bindir|\
+	am_pkglibexecdir|\
+	am_pkgdatadir|\
+	am_VPATH|\
 	am_check|am_installcheck|\
 	MAKEFLAGS|\
 	CCACHE*|\
@@ -182,33 +182,33 @@ PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/usr/local/bin:${PATH}"
 
 if [ "${am_check}" -eq 1 ] &&
 	[ "${am_installcheck}" -eq 0 ]; then
-	LIBEXECPREFIX="${abs_top_builddir}"
-	export SCRIPTPREFIX="${abs_top_srcdir}/src/share/poudriere"
+	LIBEXECPREFIX="${am_abs_top_builddir}"
+	export SCRIPTPREFIX="${am_abs_top_srcdir}/src/share/poudriere"
 	export POUDRIEREPATH="poudriere"
 	export PATH="${LIBEXECPREFIX}:${PATH}"
 elif [ "${am_check}" -eq 1 ] &&
 	[ "${am_installcheck}" -eq 1 ]; then
-	LIBEXECPREFIX="${pkglibexecdir}"
-	export SCRIPTPREFIX="${pkgdatadir}"
-	#export POUDRIEREPATH="${bindir}/poudriere"
+	LIBEXECPREFIX="${am_pkglibexecdir}"
+	export SCRIPTPREFIX="${am_pkgdatadir}"
+	#export POUDRIEREPATH="${am_bindir}/poudriere"
 	export POUDRIEREPATH="poudriere"
-	export PATH="${bindir}:${LIBEXECPREFIX}:${PATH}"
+	export PATH="${am_bindir}:${LIBEXECPREFIX}:${PATH}"
 else
-	if [ -z "${abs_top_srcdir-}" ]; then
-		: ${VPATH:="$(realpath "${0%/*}")"}
-		abs_top_srcdir="$(realpath "${VPATH}/..")"
-		abs_top_builddir="${abs_top_srcdir}"
+	if [ -z "${am_abs_top_srcdir-}" ]; then
+		: ${am_VPATH:="$(realpath "${0%/*}")"}
+		am_abs_top_srcdir="$(realpath "${am_VPATH}/..")"
+		am_abs_top_builddir="${am_abs_top_srcdir}"
 	fi
-	LIBEXECPREFIX="${abs_top_builddir}"
-	export SCRIPTPREFIX="${abs_top_srcdir}/src/share/poudriere"
-	export POUDRIEREPATH="${abs_top_builddir}/poudriere"
+	LIBEXECPREFIX="${am_abs_top_builddir}"
+	export SCRIPTPREFIX="${am_abs_top_srcdir}/src/share/poudriere"
+	export POUDRIEREPATH="${am_abs_top_builddir}/poudriere"
 	export PATH="${LIBEXECPREFIX}:${PATH}"
 fi
 if [ -z "${LIBEXECPREFIX-}" ]; then
 	echo "ERROR: Could not determine POUDRIEREPATH" >&2
 	exit 99
 fi
-: ${VPATH:=.}
+: ${am_VPATH:=.}
 : ${SH:=sh}
 if [ "${SH}" = "sh" ]; then
 	SH="${LIBEXECPREFIX}/sh"
@@ -216,7 +216,7 @@ fi
 
 BUILD_DIR="${PWD}"
 # source dir
-THISDIR=${VPATH}
+THISDIR=${am_VPATH}
 THISDIR="$(realpath "${THISDIR}")"
 cd "${THISDIR}"
 

commit f45bc948ddba4974ee6ffcf8f9f59c68f87ba571

runtest: Fix git hash in log.
Broken since feb8f9566 and 1ed5f63d6.

test/common.sh
@@ -587,9 +587,15 @@ post_getopts
 setup_traps cleanup
 
 msg_debug "getpid: $$"
-
-if [ -r "${abs_top_srcdir}/.git" ] &&
-    git_get_hash_and_dirty "${abs_top_srcdir}" 0 git_hash git_dirty; then
-	msg "Source git hash: ${git_hash} modified: ${git_dirty}"
-fi >&2
-unset git_hash git_dirty
+case "${TEST_CONTEXTS_NUM_CHECK:+set}" in
+set) ;;
+*)
+	if [ -r "${am_abs_top_srcdir:?}/.git" ] &&
+	    git_get_hash_and_dirty "${am_abs_top_srcdir:?}" 0 \
+	    git_hash git_dirty; then
+		msg "Source git hash: ${git_hash} modified: ${git_dirty}"
+	fi >&2
+	shash_remove_var "git_tree_dirty" 2>/dev/null || :
+	unset git_hash git_dirty
+	;;
+esac