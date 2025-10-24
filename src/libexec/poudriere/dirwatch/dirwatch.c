6db4fd3fefc842dbecfb6d3b0373930f11cb09b5
Bryan Drewery <bryan@shatow.net>
2025-10-24 09:36:20 -0700

    dirwatch: can avoid stat(2) with open(O_DIRECTORY)

--- src/libexec/poudriere/dirwatch/dirwatch.c
+++ src/libexec/poudriere/dirwatch/dirwatch.c
@@ -27,7 +27,6 @@
 #include <sys/types.h>
 #include <sys/event.h>
 #include <sys/time.h>
-#include <sys/stat.h>
 
 #include <unistd.h>
 #include <fcntl.h>
@@ -41,21 +40,17 @@ int
 main(int argc, char **argv)
 {
 	struct kevent event, change;
-	struct stat st;
 	int kq, fd;
 
 	if (argc != 2)
 		errx(1, "Missing the directory argument");
 
-	if (!(stat(argv[1], &st) == 0 && S_ISDIR(st.st_mode)))
-		errx(1, "%s: not a directory", argv[1]);
+	fd = open(argv[1], O_RDONLY | O_DIRECTORY);
+	if (fd == -1)
+		err(1, "open()");
 
 	if ((kq = kqueue()) == -1)
 		err(1, "kqueue()");
-
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		err(1, "open()");
 
 	EV_SET(&change, fd, EVFILT_VNODE, EV_ADD | EV_ENABLE | EV_ONESHOT, NOTE_WRITE, 0, 0);
 
