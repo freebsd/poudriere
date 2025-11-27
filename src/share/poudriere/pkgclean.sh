freebsd/poudriere

commit 1724782cc7f7ca41fae0162ebd36450fa7a2d8d8

Add some bulk/testport build quick tests.
These build pkg and a test something tiny.

Add them into `make check` to ensure routine checks do basic bulk build
testing.

This also fixes pkgclean to not require pkg to simply remove all
packages.

src/share/poudriere/pkgclean.sh
@@ -191,9 +191,13 @@ else
 fi
 jail_start "${JAILNAME}" "${PTNAME}" "${SETNAME}"
 prepare_ports
-if ! ensure_pkg_installed; then
-	err 1 "pkg must be built before this command can be used"
-fi
+case "${LISTPORTS+set}" in
+set)
+	if ! ensure_pkg_installed; then
+		err 1 "pkg must be built before this command can be used"
+	fi
+	;;
+esac
 msg "Looking for unneeded packages"
 bset status "pkgclean:"
 

test/Makefile.am
@@ -141,6 +141,7 @@ JAIL_TESTS= \
 	bulk-build-multiple-eligible-in-queue.sh \
 	bulk-build-origin_has_pkgbase.sh \
 	bulk-build-porttesting.sh \
+	bulk-build-quick-pkg.sh \
 	bulk-build-self-immutable-base-no.sh \
 	bulk-build-self-immutable-base-nullfs.sh \
 	bulk-build-self-immutable-base-schg.sh \
@@ -210,6 +211,8 @@ JAIL_TESTS= \
 	testport-build-default-flavor.sh \
 	testport-build-no-deps.sh \
 	testport-build-porttesting.sh \
+	testport-build-quick-pkg.sh \
+	testport-build-quick-1-dep.sh \
 	testport-build-specific-flavor.sh \
 	testport-build-specific-port.sh \
 	testport-default-all-flavors-failure.sh \

test/Makefile.in
@@ -541,6 +541,7 @@ JAIL_TESTS = \
 	bulk-build-multiple-eligible-in-queue.sh \
 	bulk-build-origin_has_pkgbase.sh \
 	bulk-build-porttesting.sh \
+	bulk-build-quick-pkg.sh \
 	bulk-build-self-immutable-base-no.sh \
 	bulk-build-self-immutable-base-nullfs.sh \
 	bulk-build-self-immutable-base-schg.sh \
@@ -610,6 +611,8 @@ JAIL_TESTS = \
 	testport-build-default-flavor.sh \
 	testport-build-no-deps.sh \
 	testport-build-porttesting.sh \
+	testport-build-quick-pkg.sh \
+	testport-build-quick-1-dep.sh \
 	testport-build-specific-flavor.sh \
 	testport-build-specific-port.sh \
 	testport-default-all-flavors-failure.sh \
@@ -1670,6 +1673,13 @@ bulk-build-porttesting.sh.log: bulk-build-porttesting.sh
 	--log-file $$b.log --trs-file $$b.trs \
 	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
 	"$$tst" $(AM_TESTS_FD_REDIRECT)
+bulk-build-quick-pkg.sh.log: bulk-build-quick-pkg.sh
+	@p='bulk-build-quick-pkg.sh'; \
+	b='bulk-build-quick-pkg.sh'; \
+	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+	--log-file $$b.log --trs-file $$b.trs \
+	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+	"$$tst" $(AM_TESTS_FD_REDIRECT)
 bulk-build-self-immutable-base-no.sh.log: bulk-build-self-immutable-base-no.sh
 	@p='bulk-build-self-immutable-base-no.sh'; \
 	b='bulk-build-self-immutable-base-no.sh'; \
@@ -2107,6 +2117,20 @@ testport-build-no-deps.sh.log: testport-build-no-deps.sh
 testport-build-porttesting.sh.log: testport-build-porttesting.sh
 	@p='testport-build-porttesting.sh'; \
 	b='testport-build-porttesting.sh'; \
+	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+	--log-file $$b.log --trs-file $$b.trs \
+	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+	"$$tst" $(AM_TESTS_FD_REDIRECT)
+testport-build-quick-pkg.sh.log: testport-build-quick-pkg.sh
+	@p='testport-build-quick-pkg.sh'; \
+	b='testport-build-quick-pkg.sh'; \
+	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+	--log-file $$b.log --trs-file $$b.trs \
+	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+	"$$tst" $(AM_TESTS_FD_REDIRECT)
+testport-build-quick-1-dep.sh.log: testport-build-quick-1-dep.sh
+	@p='testport-build-quick-1-dep.sh'; \
+	b='testport-build-quick-1-dep.sh'; \
 	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
 	--log-file $$b.log --trs-file $$b.trs \
 	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \

test/bulk-build-quick-pkg.sh
@@ -1,0 +1,22 @@
+LISTPORTS="ports-mgmt/pkg"
+. ./common.bulk.sh
+
+EXPECTED_IGNORED=
+EXPECTED_INSPECTED=
+EXPECTED_SKIPPED=
+EXPECTED_TOBUILD="${LISTPORTS}"
+EXPECTED_QUEUED="${EXPECTED_TOBUILD}"
+EXPECTED_LISTED="${LISTPORTS}"
+EXPECTED_BUILT=
+do_bulk -c -n ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+assert_bulk_queue_and_stats
+assert_bulk_dry_run
+echo "------" | tee /dev/stderr
+
+EXPECTED_BUILT="${EXPECTED_TOBUILD}"
+do_bulk -c ${LISTPORTS}
+assert 0 $? "Bulk should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+echo "------" | tee /dev/stderr

test/common.bulk.sh
@@ -1022,10 +1022,14 @@ do_bulk() {
 	_setup_build
 	case "$@" in
 	*-n*)
-		DRY_RUN_PACKAGES_LIST="$(mktemp -ut packages_for_dry_run)"
-		if [ -d "${PACKAGES:?}/All/" ]; then
-			list_package_files
-		fi > "${DRY_RUN_PACKAGES_LIST:?}"
+		case "$@" in
+		*-c*) ;;
+		*)
+			DRY_RUN_PACKAGES_LIST="$(mktemp -ut packages_for_dry_run)"
+			if [ -d "${PACKAGES:?}/All/" ]; then
+				list_package_files
+			fi > "${DRY_RUN_PACKAGES_LIST:?}"
+		esac
 		;;
 	esac
 	do_poudriere bulk \
@@ -1066,9 +1070,14 @@ do_options() {
 
 do_pkgclean() {
 	_setup_overlays
-	# pkg is needed for pkgclean
-	do_bulk ports-mgmt/pkg
-	assert 0 "$?" "bulk for pkg should pass"
+	case "$@" in
+	*-A*) ;;
+	*)
+		# pkg is needed for pkgclean if not removing all.
+		do_bulk ports-mgmt/pkg
+		assert 0 "$?" "bulk for pkg should pass"
+		;;
+	esac
 	do_poudriere pkgclean \
 	    ${REAL_OVERLAYS:+$(echo "${REAL_OVERLAYS}" | tr ' ' '\n' | sed -e 's,^,-O ,' | paste -d ' ' -s -)} \
 	    ${JFLAG:+-J ${JFLAG}} \
@@ -1193,11 +1202,15 @@ _assert_bulk_dry_run() {
 	    "Logdir '${log:?}/logs/ignored' should be empty"
 
 	# Packages should be untouched.
-	tmp="$(mktemp -u)"
-	if [ -d "${PACKAGES:?}/All/" ]; then
-		list_package_files
-	fi > "${tmp:?}"
-	stack_lineinfo assert_file "${tmp}" "${DRY_RUN_PACKAGES_LIST:?}"
+	case "${DRY_RUN_PACKAGES_LIST:+set}" in
+	set)
+		tmp="$(mktemp -u)"
+		if [ -d "${PACKAGES:?}/All/" ]; then
+			list_package_files
+		fi > "${tmp:?}"
+		stack_lineinfo assert_file "${tmp}" "${DRY_RUN_PACKAGES_LIST:?}"
+		;;
+	esac
 	# No .building dir should be left behind
 	assert_false [ -d "${PACKAGES:?}/.building" ]
 }

test/runtest.sh
@@ -240,6 +240,7 @@ esac
 TIMEOUT_KILL="-k 30"
 if [ -n "${TESTS_SKIP_BUILD-}" ]; then
 	case "${1##*/}" in
+	*-build-quick*.sh) ;;
 	*-build*)
 		exit 77
 		;;
@@ -254,6 +255,7 @@ if [ -n "${TESTS_SKIP_LONG-}" ]; then
 fi
 if [ -n "${TESTS_SKIP_BULK-}" ]; then
 	case "${1##*/}" in
+	*-build-quick*.sh) ;;
 	testport-*.sh|bulk-*.sh)
 		exit 77
 		;;

test/testport-build-quick-1-dep.sh
@@ -1,0 +1,28 @@
+OVERLAYS="omnibus"
+TESTPORT="misc/foo"
+LISTPORTS="${TESTPORT}"
+. ./common.bulk.sh
+
+# testport will keep old packages so we need to clean out everything
+# before doing the first run to ensure it all builds.
+do_pkgclean -y -A
+assert 0 $? "Pkgclean should pass"
+echo "-----" | tee /dev/stderr
+
+EXPECTED_QUEUED="ports-mgmt/pkg ${TESTPORT}:listed"
+EXPECTED_LISTED="${TESTPORT}"
+EXPECTED_TOBUILD="${EXPECTED_QUEUED}"
+EXPECTED_BUILT=
+do_testport -n ${TESTPORT}
+assert 0 "$?" "testport dry-run should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+echo "-----" | tee /dev/stderr
+
+EXPECTED_BUILT="${EXPECTED_TOBUILD}"
+EXPECTED_FAILED=""
+do_testport ${TESTPORT}
+assert 0 "$?" "testport should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+echo "-----" | tee /dev/stderr

test/testport-build-quick-pkg.sh
@@ -1,0 +1,28 @@
+OVERLAYS=""
+TESTPORT="ports-mgmt/pkg"
+LISTPORTS="${TESTPORT}"
+. ./common.bulk.sh
+
+# testport will keep old packages so we need to clean out everything
+# before doing the first run to ensure it all builds.
+do_pkgclean -y -A
+assert 0 $? "Pkgclean should pass"
+echo "-----" | tee /dev/stderr
+
+EXPECTED_QUEUED="${TESTPORT}:listed"
+EXPECTED_LISTED="${TESTPORT}"
+EXPECTED_TOBUILD="${EXPECTED_QUEUED}"
+EXPECTED_BUILT=
+do_testport -n ${TESTPORT}
+assert 0 "$?" "testport dry-run should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+echo "-----" | tee /dev/stderr
+
+EXPECTED_BUILT="${EXPECTED_TOBUILD}"
+EXPECTED_FAILED=""
+do_testport ${TESTPORT}
+assert 0 "$?" "testport should pass"
+assert_bulk_queue_and_stats
+assert_bulk_build_results
+echo "-----" | tee /dev/stderr

commit 0b07bd9a3c90790e98708e335c50e5f2eba0da82

pkgclean -f: Fix not installing pkg into jail.
Fixes: 1724782c ("Add some bulk/testport build quick tests.")

src/share/poudriere/pkgclean.sh
@@ -191,8 +191,8 @@ else
 fi
 jail_start "${JAILNAME}" "${PTNAME}" "${SETNAME}"
 prepare_ports
-case "${LISTPORTS+set}" in
-set)
+case "${DO_ALL}" in
+0)
 	if ! ensure_pkg_installed; then
 		err 1 "pkg must be built before this command can be used"
 	fi
@@ -222,6 +222,10 @@ should_delete() {
 	pkgname="${pkgfile##*/}"
 	pkgname="${pkgname%.*}"
 	ret=0
+
+	case "${DO_ALL}" in
+	1) return 0 ;;
+	esac
 
 	originspec=
 	if ! pkg_get_originspec originspec "${pkgfile}"; then