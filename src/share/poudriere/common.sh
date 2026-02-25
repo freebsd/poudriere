freebsd/poudriere

commit ded8d39bf7ba91c7d224e14563957d593bafb1db

bulk: Fetch DISTFILES info from ports upfront.

src/share/poudriere/common.sh
@@ -5048,11 +5048,9 @@ gather_distfiles() {
 
 	from=$(realpath "$5")
 	to=$(realpath "$6")
-	port_var_fetch_originspec "${originspec}" \
-	    DIST_SUBDIR sub \
-	    ALLFILES dists || \
-	    err 1 "Failed to lookup distfiles for ${COLOR_PORT}${originspec}${COLOR_RESET}"
-
+	# Can't remove the values as other ports may have us as a special.
+	shash_get originspec-dist_subdir "${originspec}" sub || sub=
+	shash_get originspec-dist_allfiles "${originspec}" dists || dists=
 	originspec_decode "${originspec}" origin flavor subpkg
 	case "${pkgname}" in
 	"")
@@ -6616,6 +6614,7 @@ deps_fetch_vars() {
 	local _origin_subpkg
 	local _prefix _pkgname_var _pdeps_var _bdeps_var _rdeps_var
 	local _depend_specials= _build_as_non_root= _need_root=
+	local dist_subdir dist_allfiles
 
 	originspec_decode "${originspec}" origin _origin_flavor _origin_subpkg
 	# If we were passed in a FLAVOR then we better have already looked up
@@ -6677,6 +6676,8 @@ deps_fetch_vars() {
 		FORBIDDEN _forbidden \
 		NO_ARCH:Dyes _no_arch \
 		PREFIX _prefix \
+		DIST_SUBDIR dist_subdir \
+		ALLFILES dist_allfiles \
 		${_build_as_non_root} \
 		${_changed_deps} \
 		${_changed_options:+_PRETTY_OPTS='${SELECTED_OPTIONS:@opt@${opt}+@} ${DESELECTED_OPTIONS:@opt@${opt}-@}'} \
@@ -6802,6 +6803,12 @@ deps_fetch_vars() {
 	esac
 	case "${_prefix:+set}" in
 	set) shash_set pkgname-prefix "${_pkgname}" "${_prefix}" ;;
+	esac
+	case "${dist_subdir:+set}" in
+	set) shash_set originspec-dist_subdir "${originspec}" "${dist_subdir}" ;;
+	esac
+	case "${dist_allfiles:+set}" in
+	set) shash_set originspec-dist_allfiles "${originspec}" "${dist_allfiles}" ;;
 	esac
 	case "${_need_root:+set}" in
 	set) shash_set pkgname-need_root "${_pkgname}" "${_need_root}" ;;

commit b94c44b64393168334730f43573be8af24f63f00

Fix distfile caching for flavored ports.
gather_distfiles() used to lookup `make -V ALLFILES` for the given
originspec.  Then it was changed in commit ded8d39bf7 to cache that
information up-front in gather_port_vars().  There is an existing
problem where deps_fetch_vars() uses the flavorless port origin
for originspec, but gather_distfiles() uses the flavored originspec.
That is, deps_fetch_vars() uses `origin` while gather_distfiles()
uses `origin@default`.  The default flavor gets stripped off in
gather_port_vars_port() for some reason.  This problem is avoided
by using the pkgname.

Similarly the pkgname lookup should use get_pkgname_from_originspec()
which deals with this problem.

Fixes: ded8d39bf7 ("bulk: Fetch DISTFILES info from ports upfront.")
Fixes #1302

src/share/poudriere/common.sh
@@ -5236,17 +5236,17 @@ gather_distfiles() {
 
 	from="$(realpath "$5")"
 	to="$(realpath "$6")"
-	# Can't remove the values as other ports may have us as a special.
-	shash_get originspec-dist_subdir "${originspec}" sub || sub=
-	shash_get originspec-dist_allfiles "${originspec}" dists || dists=
 	originspec_decode "${originspec}" origin flavor subpkg
 	case "${pkgname}" in
 	"")
 		# Recursive gather_distfiles()
-		shash_get originspec-pkgname "${originspec}" pkgname || \
+		get_pkgname_from_originspec "${originspec}" pkgname ||
 		    err 1 "gather_distfiles: Could not find PKGNAME for ${COLOR_PORT}${originspec}${COLOR_RESET}"
 		;;
 	esac
+	# Can't remove the values as other ports may have us as a special.
+	shash_get pkgname-dist_subdir "${pkgname}" sub || sub=
+	shash_get pkgname-dist_allfiles "${pkgname}" dists || dists=
 	shash_get pkgname-depend_specials "${pkgname}" specials || specials=
 
 	job_msg_dev "${COLOR_PORT}${origin}${flavor:+@${flavor}}${subpkg:+~${subpkg}} | ${pkgname_main}${COLOR_RESET}: distfiles ${from} -> ${to}"
@@ -7214,10 +7214,10 @@ deps_fetch_vars() {
 	set) shash_set pkgname-prefix "${_pkgname}" "${_prefix}" ;;
 	esac
 	case "${dist_subdir:+set}" in
-	set) shash_set originspec-dist_subdir "${originspec}" "${dist_subdir}" ;;
+	set) shash_set pkgname-dist_subdir "${_pkgname}" "${dist_subdir}" ;;
 	esac
 	case "${dist_allfiles:+set}" in
-	set) shash_set originspec-dist_allfiles "${originspec}" "${dist_allfiles}" ;;
+	set) shash_set pkgname-dist_allfiles "${_pkgname}" "${dist_allfiles}" ;;
 	esac
 	case "${_need_root:+set}" in
 	set) shash_set pkgname-need_root "${_pkgname}" "${_need_root}" ;;