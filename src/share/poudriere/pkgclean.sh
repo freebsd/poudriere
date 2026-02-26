freebsd/poudriere

commit 7c39d9685ae6e4b3f6ffeef713126d290e7d8a42

pkgclean -u: Rework to avoid layer violation in do_confirm_delete().
Fixes: b691ab8927673a5b36126e991f8c9e1ae5a601bf

src/share/poudriere/pkgclean.sh
@@ -378,14 +378,17 @@ done
 
 ret=0
 do_confirm_delete "${BADFILES_LIST}" "stale packages" \
-    "${answer}" "${DRY_RUN}" "${FORCE_BUILD_REPO}" || ret=$?
-if [ ${ret} -eq 2 ]; then
+    "${answer}" "${DRY_RUN}" || ret=$?
+case "${ret}.${FORCE_BUILD_REPO}" in
+# No files found and not forced, or dry-run, then exit.
+2.0|3)
 	exit 0
-fi
+	;;
+esac
 
 # After deleting stale files, need to remake repo.
 
-if [ $ret -eq 1 ]; then
+if [ "${ret}" -eq 1 ] || [ "${FORCE_BUILD_REPO}" -eq 1 ]; then
 	[ "${NO_RESTRICTED}" != "no" ] && clean_restricted
 	if [ ${BUILD_REPO} -eq 1 ]; then
 		if [ ${DO_ALL} -eq 1 ]; then

src/share/poudriere/common.sh
@@ -580,21 +580,18 @@ stat_humanize() {
 }
 
 do_confirm_delete() {
-	[ $# -eq 5 ] || eargs do_confirm_delete badfiles_list \
-	    reason_plural_object answer DRY_RUN FORCE_BUILD_REPO
+	[ $# -eq 4 ] || eargs do_confirm_delete badfiles_list \
+	    reason_plural_object answer DRY_RUN
 	local filelist="$1"
 	local reason="$2"
 	local answer="$3"
 	local DRY_RUN="$4"
-	local FORCE_BUILD_REPO="$5"
 	local file_cnt count hsize ret
 
-	if [ ${FORCE_BUILD_REPO} -eq 0 ]; then
-		file_cnt=$(wc -l ${filelist} | awk '{print $1}')
-		if [ ${file_cnt} -eq 0 ]; then
-			msg "No ${reason} to cleanup"
-			return 2
-		fi
+	file_cnt=$(wc -l ${filelist} | awk '{print $1}')
+	if [ ${file_cnt} -eq 0 ]; then
+		msg "No ${reason} to cleanup"
+		return 2
 	fi
 
 	msg_n "Calculating size for found files..."
@@ -612,7 +609,7 @@ do_confirm_delete() {
 
 	if [ ${DRY_RUN} -eq 1 ];  then
 		msg "Dry run: not cleaning anything."
-		return 2
+		return 3
 	fi
 
 	if [ -z "${answer}" ]; then

src/share/poudriere/distclean.sh
@@ -192,7 +192,7 @@ comm -1 -3 ${DISTFILES_LIST}.expected ${DISTFILES_LIST}.actual \
 
 ret=0
 do_confirm_delete "${DISTFILES_LIST}.unexpected" "stale distfiles" \
-    "${answer}" "${DRY_RUN}" "0" || ret=$?
+    "${answer}" "${DRY_RUN}" || ret=$?
 if [ ${ret} -eq 2 ]; then
 	exit 0
 fi

src/share/poudriere/logclean.sh
@@ -217,7 +217,7 @@ echo " done"
 ret=0
 do_confirm_delete "${OLDLOGS}" \
     "${reason}" \
-    "${answer}" "${DRY_RUN}" "0" || ret=$?
+    "${answer}" "${DRY_RUN}" || ret=$?
 # ret = 2 means no files were deleted, but let's still
 # cleanup other broken/stale files and links.
 logs_deleted=0
@@ -239,7 +239,7 @@ echo " done"
 ret=0
 do_confirm_delete "${OLDLOGS}" \
     "${reason}" \
-    "${answer}" "${DRY_RUN}" "0" || ret=$?
+    "${answer}" "${DRY_RUN}" || ret=$?
 
 if [ ${DRY_RUN} -eq 0 ]; then
 	msg_n "Removing broken legacy latest-per-pkg symlinks (no filter)..."

commit 79bca07471ffdeec2c6b7784f72f414acdb6a83e

pkgclean: fix dry-run still generating repo
Fixes: 7c39d9685ae6e4b3f6ffeef713126d290e7d8a42
(cherry picked from master)

src/share/poudriere/pkgclean.sh
@@ -394,7 +394,7 @@ do_confirm_delete "${BADFILES_LIST}" "stale packages" \
     "${answer}" "${DRY_RUN}" || ret=$?
 case "${ret}.${FORCE_BUILD_REPO}" in
 # No files found and not forced, or dry-run, then exit.
-2.0|3)
+2.0|3.*)
 	exit 0
 	;;
 esac